b075b948bb0a5be4ab070a6b4a31cd3933ac0f5c
Andreas Mülder <A.Muelder@googlemail.com>
2010-11-17 19:07:55 +0000

    Initial Region and State created for each new diagram

--- org.yakindu.sct.model/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/FactoryUtils.java
+++ org.yakindu.sct.model/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/FactoryUtils.java
@@ -10,29 +10,99 @@
  */
 package org.yakindu.sct.statechart.diagram.factories;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
 import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
+import org.eclipse.gmf.runtime.notation.Bounds;
 import org.eclipse.gmf.runtime.notation.DecorationNode;
+import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.gmf.runtime.notation.NotationFactory;
 import org.eclipse.gmf.runtime.notation.View;
+import org.yakindu.sct.model.statechart.statechart.InitialState;
+import org.yakindu.sct.model.statechart.statechart.Region;
+import org.yakindu.sct.model.statechart.statechart.Statechart;
+import org.yakindu.sct.model.statechart.statechart.StatechartFactory;
+import org.yakindu.sct.statechart.diagram.DiagramActivator;
+import org.yakindu.sct.statechart.diagram.editor.StatechartDiagramEditor;
+import org.yakindu.sct.statechart.diagram.providers.ProviderConstants;
 
 /**
+ * Convenience methods for semantic and notation model element creation.
  * 
  * @author Andreas Muelder <a
  *         href="mailto:andreas.muelder@itemis.de">andreas.muelder@itemis.de</a>
  * 
  */
 public final class FactoryUtils {
-	
+
+	private static final int INITIAL_REGION_WIDTH = 800;
+	private static final int INITIAL_REGION_HEIGHT = 600;
+	private static final String INITIAL_REGION_NAME = "main region";
+
 	private FactoryUtils() {
 	}
-	
+
+	/**
+	 * Creates a Label view
+	 * 
+	 * @param owner
+	 * @param hint
+	 * @return
+	 */
 	public static Node createLabel(View owner, String hint) {
 		DecorationNode nameLabel = NotationFactory.eINSTANCE.createDecorationNode();
 		nameLabel.setType(hint);
 		ViewUtil.insertChildView(owner, nameLabel, ViewUtil.APPEND, true);
-		nameLabel.setLayoutConstraint(NotationFactory.eINSTANCE
-				.createLocation());
+		nameLabel.setLayoutConstraint(NotationFactory.eINSTANCE.createLocation());
 		return nameLabel;
 	}
+
+	/**
+	 * Creates a Statechart with an initial Region and an initial State
+	 * 
+	 * @return instanceof {@link Statechart}
+	 */
+	public static void createStatechartModel(Resource resource) {
+		// Create a statechart
+		Statechart statechart = StatechartFactory.eINSTANCE.createStatechart();
+		Diagram diagram = ViewService.createDiagram(statechart, StatechartDiagramEditor.ID,
+				DiagramActivator.DIAGRAM_PREFERENCES_HINT);
+		diagram.setElement(statechart);
+		// Add to resource
+		resource.getContents().add(statechart);
+		resource.getContents().add(diagram);
+		// Create an initial region
+		Region region = StatechartFactory.eINSTANCE.createRegion();
+		region.setName(INITIAL_REGION_NAME);
+		statechart.getRegions().add(region);
+		Node regionView = ViewService.createNode(diagram, region, ProviderConstants.REGION,
+				DiagramActivator.DIAGRAM_PREFERENCES_HINT);
+		setRegionViewLayoutConstraint(regionView);
+		// // Create an initial state
+		InitialState initialState = StatechartFactory.eINSTANCE.createInitialState();
+		region.getVertices().add(initialState);
+		Node initialStateView = ViewService.createNode(getRegionCompartmentView(regionView),
+				ProviderConstants.INITIALSTATE, DiagramActivator.DIAGRAM_PREFERENCES_HINT);
+		setInitialStateViewLayoutConstraint(initialStateView);
+	}
+
+	private static void setInitialStateViewLayoutConstraint(Node initialStateView) {
+		Bounds bounds = NotationFactory.eINSTANCE.createBounds();
+		bounds.setX(40);
+		bounds.setY(40);
+		initialStateView.setLayoutConstraint(bounds);
+	}
+
+	private static View getRegionCompartmentView(View regionView) {
+		return (View) regionView.getChildren().get(1);
+	}
+
+	private static void setRegionViewLayoutConstraint(Node regionView) {
+		Bounds bounds = NotationFactory.eINSTANCE.createBounds();
+		bounds.setHeight(INITIAL_REGION_HEIGHT);
+		bounds.setWidth(INITIAL_REGION_WIDTH);
+		regionView.setLayoutConstraint(bounds);
+	}
+
 }
