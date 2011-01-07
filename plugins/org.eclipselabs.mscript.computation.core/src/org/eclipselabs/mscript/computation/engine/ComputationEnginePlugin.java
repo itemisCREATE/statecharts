ddb2bb64e16a754f38c07dab34d965085ca271e6
au@andreasunger.net <au@andreasunger.com>
2011-01-07 16:00:51 +0000

    *Plugin classes renamed.

--- plugins/org.eclipselabs.mscript.computation.core/src/org/eclipselabs/mscript/computation/engine/ComputationEnginePlugin.java
+++ plugins/org.eclipselabs.mscript.computation.core/src/org/eclipselabs/mscript/computation/engine/ComputationEnginePlugin.java
@@ -3,7 +3,7 @@ package org.eclipselabs.mscript.computation.engine;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class ComputationPlugin implements BundleActivator {
+public class ComputationEnginePlugin implements BundleActivator {
 
 	private static BundleContext context;
 
@@ -16,7 +16,7 @@ public class ComputationPlugin implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
-		ComputationPlugin.context = bundleContext;
+		ComputationEnginePlugin.context = bundleContext;
 	}
 
 	/*
@@ -24,7 +24,7 @@ public class ComputationPlugin implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
-		ComputationPlugin.context = null;
+		ComputationEnginePlugin.context = null;
 	}
 
 }
