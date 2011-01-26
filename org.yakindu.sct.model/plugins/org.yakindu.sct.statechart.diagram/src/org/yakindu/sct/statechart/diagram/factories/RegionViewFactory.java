61306616bcb0dc7481e180f363dca97dc0eff37f
Andreas Mülder <A.Muelder@googlemail.com>
2011-01-26 10:33:56 +0000

    Shape Node Styles added

--- org.yakindu.sct.model/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/RegionViewFactory.java
+++ org.yakindu.sct.model/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/RegionViewFactory.java
@@ -1,12 +1,22 @@
 package org.yakindu.sct.statechart.diagram.factories;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
 import org.eclipse.gmf.runtime.diagram.ui.view.factories.AbstractShapeViewFactory;
+import org.eclipse.gmf.runtime.draw2d.ui.figures.FigureUtilities;
 import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
+import org.eclipse.gmf.runtime.notation.NotationFactory;
+import org.eclipse.gmf.runtime.notation.Shape;
+import org.eclipse.gmf.runtime.notation.ShapeStyle;
+import org.eclipse.gmf.runtime.notation.Style;
 import org.eclipse.gmf.runtime.notation.View;
 import org.yakindu.sct.statechart.diagram.providers.ProviderConstants;
+
 /**
  * 
  * @author Andreas Muelder <a
@@ -16,10 +26,12 @@ import org.yakindu.sct.statechart.diagram.providers.ProviderConstants;
 public class RegionViewFactory extends AbstractShapeViewFactory {
 
 	@Override
-	protected void decorateView(View containerView, View view, IAdaptable semanticAdapter, String semanticHint,
-			int index, boolean persisted) {
+	protected void decorateView(View containerView, View view,
+			IAdaptable semanticAdapter, String semanticHint, int index,
+			boolean persisted) {
 
-		super.decorateView(containerView, view, semanticAdapter, semanticHint, index, persisted);
+		super.decorateView(containerView, view, semanticAdapter, semanticHint,
+				index, persisted);
 
 		IAdaptable eObjectAdapter = null;
 		EObject eObject = (EObject) semanticAdapter.getAdapter(EObject.class);
@@ -27,10 +39,18 @@ public class RegionViewFactory extends AbstractShapeViewFactory {
 			eObjectAdapter = new EObjectAdapter(eObject);
 		}
 		FactoryUtils.createLabel(view, ProviderConstants.REGION_NAME);
-		
-		
-		getViewService().createNode(eObjectAdapter, view, ProviderConstants.REGION_COMPARTMENT, ViewUtil.APPEND, true,
+
+		getViewService().createNode(eObjectAdapter, view,
+				ProviderConstants.REGION_COMPARTMENT, ViewUtil.APPEND, true,
 				getPreferencesHint());
+	}
+
+	@Override
+	protected List<Style> createStyles(View view) {
+		ShapeStyle style = NotationFactory.eINSTANCE.createShapeStyle();
+		style.setLineColor(FigureUtilities.RGBToInteger(ColorConstants.darkGray
+				.getRGB()));
+		return Collections.singletonList((Style) style);
 	}
 
 }
