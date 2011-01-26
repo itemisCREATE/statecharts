0a69043251e703f1608f10972dbcfdee6ed7e518
A.Muelder@gmail.com <A.Muelder@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-01-26 10:33:56 +0000

    Shape Node Styles added

--- SCT2/trunk/org.yakindu.sct.model/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/StateViewFactory.java
+++ SCT2/trunk/org.yakindu.sct.model/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/StateViewFactory.java
@@ -10,11 +10,19 @@
  */
 package org.yakindu.sct.statechart.diagram.factories;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
 import org.eclipse.gmf.runtime.diagram.ui.view.factories.AbstractShapeViewFactory;
+import org.eclipse.gmf.runtime.draw2d.ui.figures.FigureUtilities;
 import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
+import org.eclipse.gmf.runtime.notation.NotationFactory;
+import org.eclipse.gmf.runtime.notation.ShapeStyle;
+import org.eclipse.gmf.runtime.notation.Style;
 import org.eclipse.gmf.runtime.notation.View;
 import org.yakindu.sct.statechart.diagram.providers.ProviderConstants;
 
@@ -27,24 +35,35 @@ import org.yakindu.sct.statechart.diagram.providers.ProviderConstants;
 public class StateViewFactory extends AbstractShapeViewFactory {
 
 	@Override
-	protected void decorateView(View containerView, View view, IAdaptable semanticAdapter, String semanticHint,
-			int index, boolean persisted) {
-		super.decorateView(containerView, view, semanticAdapter, semanticHint, index, persisted);
+	protected void decorateView(View containerView, View view,
+			IAdaptable semanticAdapter, String semanticHint, int index,
+			boolean persisted) {
+		super.decorateView(containerView, view, semanticAdapter, semanticHint,
+				index, persisted);
 		IAdaptable eObjectAdapter = null;
 		EObject eObject = (EObject) semanticAdapter.getAdapter(EObject.class);
 		if (eObject != null) {
 			eObjectAdapter = new EObjectAdapter(eObject);
 		}
-		
-		FactoryUtils.createLabel(view, ProviderConstants.STATE_NAME);
-		//TODO
-//		getViewService().createNode(eObjectAdapter, view, ProviderConstants.STATE_TEXT_COMPARTMENT, ViewUtil.APPEND,
-//				true, getPreferencesHint());
 
-		getViewService().createNode(eObjectAdapter, view, ProviderConstants.STATE_FIGURE_COMPARTMENT, ViewUtil.APPEND,
+		FactoryUtils.createLabel(view, ProviderConstants.STATE_NAME);
+		// TODO
+		// getViewService().createNode(eObjectAdapter, view,
+		// ProviderConstants.STATE_TEXT_COMPARTMENT, ViewUtil.APPEND,
+		// true, getPreferencesHint());
+
+		getViewService().createNode(eObjectAdapter, view,
+				ProviderConstants.STATE_FIGURE_COMPARTMENT, ViewUtil.APPEND,
 				true, getPreferencesHint());
-		
-	
+
+	}
+
+	@Override
+	protected List<Style> createStyles(View view) {
+		ShapeStyle style = NotationFactory.eINSTANCE.createShapeStyle();
+		style.setLineColor(FigureUtilities.RGBToInteger(ColorConstants.darkGray
+				.getRGB()));
+		return Collections.singletonList((Style) style);
 	}
 
 }
