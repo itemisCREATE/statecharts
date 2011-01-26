f399217a8f0e8b1ef30db4e17a4b5df5ffeef26b
A.Muelder@gmail.com <A.Muelder@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-01-26 10:35:51 +0000

    moved to policy package

---
+++ SCT2/trunk/org.yakindu.sct.model/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/policies/XTextDirectEditPolicy.java
@@ -0,0 +1,52 @@
+package org.yakindu.sct.statechart.diagram.policies;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.editpolicies.DirectEditPolicy;
+import org.eclipse.gef.requests.DirectEditRequest;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
+import org.yakindu.sct.statechart.diagram.xtext.integration.IXTextAwareEditPart;
+import org.yakindu.sct.statechart.diagram.xtext.integration.XTextCellEditor;
+
+/**
+ * The {@link DirectEditPolicy} used for XText Direct Editing.
+ * 
+ * @author muelder
+ * 
+ */
+public class XTextDirectEditPolicy extends DirectEditPolicy {
+
+	protected Command getDirectEditCommand(DirectEditRequest edit) {
+		Assert.isTrue(edit.getCellEditor() instanceof XTextCellEditor);
+		XTextCellEditor cellEditor = (XTextCellEditor) edit.getCellEditor();
+		String value = (String) cellEditor.getValue();
+		if (value == null)
+			return null;
+
+		IGraphicalEditPart editPart = getHost();
+		Assert.isTrue(editPart instanceof IXTextAwareEditPart);
+		IXTextAwareEditPart xTextEditPart = (IXTextAwareEditPart) editPart;
+
+		SetRequest request = new SetRequest(editPart.resolveSemanticElement(),
+				xTextEditPart.getTextFeature(), value);
+		SetValueCommand setCommand = new SetValueCommand(request);
+		return new ICommandProxy(setCommand);
+	}
+
+	/**
+	 * @see DirectEditPolicy#showCurrentEditValue(DirectEditRequest)
+	 */
+	protected void showCurrentEditValue(DirectEditRequest request) {
+		String value = (String) request.getCellEditor().getValue();
+		((IXTextAwareEditPart) getHost()).getLabel().setText(value);
+	}
+
+	@Override
+	public IGraphicalEditPart getHost() {
+		return (IGraphicalEditPart) super.getHost();
+	}
+
+}
