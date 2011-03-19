b6723e08563189ad9cf8f00a55f18605e2d5af85
terfloth@itemis.de <terfloth@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-03-19 23:51:57 +0000

    changed figure visualization: added color gradients and shadows to states, resized pseudostates and changed colors...

--- SCT2/trunk/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/editor/figures/utils/MapModeUtils.java
+++ SCT2/trunk/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/editor/figures/utils/MapModeUtils.java
@@ -9,15 +9,15 @@ public final class MapModeUtils {
 	}
 
 	
-	private static final int DEFAULT_NODE_WIDTH = 80;
-	private static final int DEFAULT_NODE_HEIGHT = 60;
+	private static final int DEFAULT_NODE_WIDTH = 40;
+	private static final int DEFAULT_NODE_HEIGHT = 40;
 
 	public static Dimension getDefaultSizeDimension(IMapMode mode) {
 		return new Dimension(mode.DPtoLP(DEFAULT_NODE_WIDTH), mode.DPtoLP(DEFAULT_NODE_HEIGHT));
 	}
 
 
-	public static final Dimension DEFAULT_SMALL_NODE_DIMENSION = new Dimension(20, 20);
+	public static final Dimension DEFAULT_SMALL_NODE_DIMENSION = new Dimension(15, 15);
 
 	public static Dimension getMappedDimensions(IMapMode mode, Dimension d) {
 		return new Dimension(mode.DPtoLP(d.width), mode.DPtoLP(d.height));
