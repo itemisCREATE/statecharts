83bbb8fd4c03044e0c218c2d6c83ebc5e156bbb2
a.muelder@googlemail.com <a.muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-05-03 08:38:09 +0000

    Migrated to Xtext 2.0 - Target updated

--- SCT2/trunk/plugins/org.yakindu.sct.statechart.expressions/src-gen/org/yakindu/sct/statechart/expressions/util/ExpressionsSwitch.java
+++ SCT2/trunk/plugins/org.yakindu.sct.statechart.expressions/src-gen/org/yakindu/sct/statechart/expressions/util/ExpressionsSwitch.java
@@ -6,10 +6,10 @@
  */
 package org.yakindu.sct.statechart.expressions.util;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
+import java.util.List;
 
-import org.eclipse.emf.ecore.util.Switch;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 
 import org.yakindu.model.sct.statechart.Declaration;
 import org.yakindu.model.sct.statechart.Event;
@@ -31,7 +31,7 @@ import org.yakindu.sct.statechart.expressions.*;
  * @see org.yakindu.sct.statechart.expressions.ExpressionsPackage
  * @generated
  */
-public class ExpressionsSwitch<T> extends Switch<T>
+public class ExpressionsSwitch<T>
 {
   /**
    * The cached model package
@@ -56,17 +56,15 @@ public class ExpressionsSwitch<T> extends Switch<T>
   }
 
   /**
-   * Checks whether this is a switch for the given package.
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @parameter ePackage the package in question.
-   * @return whether this is a switch for the given package.
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
    * @generated
    */
-  @Override
-  protected boolean isSwitchFor(EPackage ePackage)
+  public T doSwitch(EObject theEObject)
   {
-    return ePackage == modelPackage;
+    return doSwitch(theEObject.eClass(), theEObject);
   }
 
   /**
@@ -76,7 +74,29 @@ public class ExpressionsSwitch<T> extends Switch<T>
    * @return the first non-null result returned by a <code>caseXXX</code> call.
    * @generated
    */
-  @Override
+  protected T doSwitch(EClass theEClass, EObject theEObject)
+  {
+    if (theEClass.eContainer() == modelPackage)
+    {
+      return doSwitch(theEClass.getClassifierID(), theEObject);
+    }
+    else
+    {
+      List<EClass> eSuperTypes = theEClass.getESuperTypes();
+      return
+        eSuperTypes.isEmpty() ?
+          defaultCase(theEObject) :
+          doSwitch(eSuperTypes.get(0), theEObject);
+    }
+  }
+
+  /**
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @generated
+   */
   protected T doSwitch(int classifierID, EObject theEObject)
   {
     switch (classifierID)
@@ -1457,7 +1477,6 @@ public class ExpressionsSwitch<T> extends Switch<T>
    * @see #doSwitch(org.eclipse.emf.ecore.EObject)
    * @generated
    */
-  @Override
   public T defaultCase(EObject object)
   {
     return null;
