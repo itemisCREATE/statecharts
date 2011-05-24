2e98a3fe76de0fdd4762d6bf9c083c339e4855a9
Andreas Mülder <A.Muelder@googlemail.com>
2011-05-24 11:25:28 +0000

    Remove generator fragment from trunk

--- plugins/org.yakindu.sct.statechart.expressions/src-gen/org/yakindu/sct/statechart/AbstractExpressionsRuntimeModule.java
+++ plugins/org.yakindu.sct.statechart.expressions/src-gen/org/yakindu/sct/statechart/AbstractExpressionsRuntimeModule.java
@@ -129,11 +129,6 @@ public abstract class AbstractExpressionsRuntimeModule extends DefaultRuntimeMod
 		binder.bind(org.eclipse.xtext.resource.IResourceDescriptions.class).annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.resource.impl.ResourceDescriptionsProvider.NAMED_BUILDER_SCOPE)).to(org.eclipse.xtext.resource.impl.ResourceSetBasedResourceDescriptions.class);
 	}
 
-	// contributed by org.eclipse.xtext.generator.generator.GeneratorFragment
-	public Class<? extends org.eclipse.xtext.generator.IGenerator> bindIGenerator() {
-		return org.yakindu.sct.statechart.generator.ExpressionsGenerator.class;
-	}
-
 	// contributed by org.eclipse.xtext.generator.formatting.FormatterFragment
 	public Class<? extends org.eclipse.xtext.formatting.IFormatter> bindIFormatter() {
 		return org.yakindu.sct.statechart.formatting.ExpressionsFormatter.class;
