e6a273ee5fb9d0765e0f6233c2c0fbda2ba537cc
Andreas Mülder <A.Muelder@googlemail.com>
2011-06-08 20:17:04 +0000

    Share project "org.yakindu.sct.statechart.simulation.ui" into "https://svn.codespot.com/a/eclipselabs.org/yakindu"

---
+++ plugins/org.yakindu.sct.statechart.simulation.ui/src/org/yakindu/sct/statechart/simulation/ui/SimulationActivator.java
@@ -0,0 +1,29 @@
+package org.yakindu.sct.statechart.simulation.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class SimulationActivator extends AbstractUIPlugin {
+
+	public static final String PLUGIN_ID = "org.yakindu.sct.statechart.simulation.ui"; //$NON-NLS-1$
+
+	private static SimulationActivator plugin;
+
+	public SimulationActivator() {
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static SimulationActivator getDefault() {
+		return plugin;
+	}
+
+}
