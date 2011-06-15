74ffe3c7f9eee58f31f6de59783280d293df84b8
Andreas Mülder <A.Muelder@googlemail.com>
2011-06-15 11:42:49 +0000

    Changed FontData for Nodes and Edges

--- plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/TransitionViewFactory.java
+++ plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/factories/TransitionViewFactory.java
@@ -12,8 +12,10 @@ package org.yakindu.sct.statechart.diagram.factories;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.gmf.runtime.diagram.ui.view.factories.ConnectionViewFactory;
+import org.eclipse.gmf.runtime.notation.FontStyle;
 import org.eclipse.gmf.runtime.notation.Location;
 import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.gmf.runtime.notation.NotationFactory;
 import org.eclipse.gmf.runtime.notation.View;
 import org.yakindu.sct.statechart.diagram.providers.SemanticHints;
 
@@ -24,10 +26,15 @@ import org.yakindu.sct.statechart.diagram.providers.SemanticHints;
  */
 public class TransitionViewFactory extends ConnectionViewFactory {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void decorateView(View containerView, View view,
 			IAdaptable element, String semanticHint, int index,
 			boolean persisted) {
+		
+		FontStyle fontStyle = NotationFactory.eINSTANCE.createFontStyle();
+		view.getStyles().add(fontStyle);
+
 		super.decorateView(containerView, view, element, semanticHint, index,
 				persisted);
 
