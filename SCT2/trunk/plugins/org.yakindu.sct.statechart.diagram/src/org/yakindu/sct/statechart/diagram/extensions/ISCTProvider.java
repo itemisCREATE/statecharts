ca2d37ed9325a718c319fb6b7187f8440278f34d
a.muelder@googlemail.com <a.muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-06-28 14:51:44 +0000

    project renaming: remove old projects

--- SCT2/trunk/plugins/org.yakindu.sct.statechart.diagram/src/org/yakindu/sct/statechart/diagram/extensions/ISCTProvider.java
+++
@@ -1,8 +0,0 @@
-package org.yakindu.sct.statechart.diagram.extensions;
-
-import org.eclipse.emf.ecore.EObject;
-
-public interface ISCTProvider {
-
-	public boolean isProviderFor(EObject semanticElement);
-}
