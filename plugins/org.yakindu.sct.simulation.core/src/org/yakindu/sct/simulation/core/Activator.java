4d91e03e0b3fc8739ff4f0ea6088c05daf13f98d
Andreas Mülder <A.Muelder@googlemail.com>
2011-08-01 10:33:30 +0000

    Renamed plugin org.yakindu.sct.ui.simulation to org.yakindu.sct.simulation.ui

---
+++ plugins/org.yakindu.sct.simulation.core/src/org/yakindu/sct/simulation/core/Activator.java
@@ -0,0 +1,32 @@
+package org.yakindu.sct.simulation.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	
+	public static final String PLUGIN_ID = "org.yakindu.sct.simulation.core";
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ plugins/org.yakindu.sct.simulation.core/src/org/yakindu/sct/simulation/core/extensions/IExtensionPoints.java
@@ -0,0 +1,7 @@
+package org.yakindu.sct.simulation.core.extensions;
+
+public interface IExtensionPoints {
+
+	public static final String EXECUTION_BUILDER = "org.yakindu.sct.simulation.core.sgraph.executionbuilder";
+
+}
