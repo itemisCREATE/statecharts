679f3d4e54813192ad65f008af53d7dd0afa2ac2
markus.muehlbrandt@itemis.de <markus.muehlbrandt@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-08-30 13:23:41 +0000

    Added copyright and author headers.

--- SCT2/trunk/plugins/org.yakindu.sct.ui.navigator/src/org/yakindu/sct/ui/navigator/NavigatorLinkHelper.java
+++ SCT2/trunk/plugins/org.yakindu.sct.ui.navigator/src/org/yakindu/sct/ui/navigator/NavigatorLinkHelper.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
 package org.yakindu.sct.ui.navigator;
 
 import java.util.Iterator;
@@ -28,6 +38,11 @@ import org.yakindu.sct.ui.navigator.utils.ComposedAdapterFactoryUtil;
 
 import de.itemis.xtext.utils.jface.viewers.util.ActiveEditorResolver;
 
+/**
+ * 
+ * @author m.muehlbrandt
+ *
+ */
 public class NavigatorLinkHelper implements ILinkHelper {
 
 	private AdapterFactoryContentProvider myAdapterFctoryContentProvier;
--- SCT2/trunk/plugins/org.yakindu.sct.ui.navigator/src/org/yakindu/sct/ui/navigator/StatechartObjectViewerFilter.java
+++ SCT2/trunk/plugins/org.yakindu.sct.ui.navigator/src/org/yakindu/sct/ui/navigator/StatechartObjectViewerFilter.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
 package org.yakindu.sct.ui.navigator;
 
 import org.eclipse.emf.ecore.EObject;
@@ -9,6 +19,11 @@ import org.yakindu.sct.model.sgraph.Trigger;
 import org.yakindu.sct.model.stext.stext.InterfaceScope;
 import org.yakindu.sct.model.stext.stext.InternalScope;
 
+/**
+ * 
+ * @author m.muehlbrandt
+ *
+ */
 public class StatechartObjectViewerFilter extends ViewerFilter {
 
 	@Override
