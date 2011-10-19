a2cc328d872843f84347df7b6c4e3fc0eab3bec4
a.muelder@googlemail.com <a.muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-10-19 13:02:44 +0000

    FIXED  YAKHMI-131 Drop symbol over hidden region text label does not work

--- SCT2/trunk/plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editparts/RegionNameEditPart.java
+++ SCT2/trunk/plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editparts/RegionNameEditPart.java
@@ -1,6 +1,9 @@
 package org.yakindu.sct.ui.editor.editparts;
 
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.requests.CreateRequest;
 import org.eclipse.gmf.runtime.notation.View;
 import org.yakindu.sct.model.sgraph.SGraphPackage;
 import org.yakindu.sct.ui.editor.DiagramActivator;
@@ -21,6 +24,19 @@ public class RegionNameEditPart extends TextAwareLabelEditPart {
 
 	public void setLabel(IFigure label) {
 		setFigure(label);
+	}
+
+	@Override
+	public EditPart getTargetEditPart(Request request) {
+		// Delegate creation requests to the figure compartment edit part
+		if (request instanceof CreateRequest) {
+			for (Object object : getParent().getChildren()) {
+				if (object instanceof RegionCompartmentEditPart) {
+					return (EditPart) object;
+				}
+			}
+		}
+		return super.getTargetEditPart(request);
 	}
 
 	@Override
