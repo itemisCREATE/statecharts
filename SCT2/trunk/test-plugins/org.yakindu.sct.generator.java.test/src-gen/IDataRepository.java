cf18294c34c93ec31cc28b5109dcb5e12246d04c
markus.muehlbrandt@itemis.de <markus.muehlbrandt@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-10-27 13:21:56 +0000

    Share project "org.yakindu.sct.generator.java.test" into "https://svn.codespot.com/a/eclipselabs.org/yakindu"

---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.java.test/src-gen/IDataRepository.java
@@ -0,0 +1,7 @@
+
+package org.yakindu.sct.runtime.java.base;
+
+public interface IDataRepository {
+	
+	public DataType<?> getDataType(String name);
+}
---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.java.test/src-gen/InterfaceA.java
@@ -0,0 +1,30 @@
+
+package org.yakindu.sct.runtime.java.custom;
+
+
+public interface InterfaceA {
+	
+	
+	INEvent
+	
+	INEvent
+	
+	INEvent
+	
+	INEvent
+	
+	
+	public void raiseE1(int value);
+	
+	public void raiseE2();
+	
+	public void raiseE3();
+	
+	public void raiseE4();
+	
+	public void setVarC(int value);
+	
+	public int getVarC();
+
+}
+
