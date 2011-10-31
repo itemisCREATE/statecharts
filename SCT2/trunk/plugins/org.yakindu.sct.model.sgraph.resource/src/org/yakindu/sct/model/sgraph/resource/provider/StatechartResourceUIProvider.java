1e8dae50bcb4bf55c0bf24651032cbe467429ce2
benjamin.schwertfeger@gmail.com <benjamin.schwertfeger@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-10-31 16:24:43 +0000

    UIServiceProvider for Custom naming in 'Open model element'

---
+++ SCT2/trunk/plugins/org.yakindu.sct.model.sgraph.resource/src/org/yakindu/sct/model/sgraph/resource/provider/StatechartResourceUIProvider.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.model.sgraph.resource.provider;
+
+import org.eclipse.xtext.ui.resource.DefaultResourceUIServiceProvider;
+
+import com.google.inject.Inject;
+
+/**
+ * 
+ * @author benjamin.schwertfeger@itemis.de
+ * 
+ */
+public class StatechartResourceUIProvider extends
+		DefaultResourceUIServiceProvider {
+
+	@Inject
+	public StatechartResourceUIProvider(
+			StatechartResourceProvider runtimeDelegate) {
+		super(runtimeDelegate);
+	}
+}
