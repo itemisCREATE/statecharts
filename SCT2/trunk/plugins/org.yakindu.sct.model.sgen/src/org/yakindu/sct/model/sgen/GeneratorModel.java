c79a98c10360bd204802c0b26fdacac32af99db2
a.muelder@googlemail.com <a.muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-11-04 10:03:06 +0000

    Share project "org.yakindu.sct.model.sgen" into "https://svn.codespot.com/a/eclipselabs.org/yakindu"

--- SCT2/trunk/plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/GeneratorModel.java
+++ SCT2/trunk/plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/GeneratorModel.java
@@ -6,11 +6,7 @@
  */
 package org.yakindu.sct.model.sgen;
 
-import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EObject;
-
-import org.yakindu.sct.model.sgraph.Statechart;
 
 /**
  * <!-- begin-user-doc -->
@@ -20,8 +16,8 @@ import org.yakindu.sct.model.sgraph.Statechart;
  * <p>
  * The following features are supported:
  * <ul>
- *   <li>{@link org.yakindu.sct.model.sgen.GeneratorModel#getConfigurations <em>Configurations</em>}</li>
- *   <li>{@link org.yakindu.sct.model.sgen.GeneratorModel#getStatecharts <em>Statecharts</em>}</li>
+ *   <li>{@link org.yakindu.sct.model.sgen.GeneratorModel#getConfiguration <em>Configuration</em>}</li>
+ *   <li>{@link org.yakindu.sct.model.sgen.GeneratorModel#getStatechartReferences <em>Statechart References</em>}</li>
  * </ul>
  * </p>
  *
@@ -31,35 +27,55 @@ import org.yakindu.sct.model.sgraph.Statechart;
  */
 public interface GeneratorModel extends EObject {
 	/**
-	 * Returns the value of the '<em><b>Configurations</b></em>' containment reference list.
-	 * The list contents are of type {@link org.yakindu.sct.model.sgen.Configuration}.
+	 * Returns the value of the '<em><b>Configuration</b></em>' containment reference.
 	 * <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Configurations</em>' containment reference list isn't clear,
+	 * If the meaning of the '<em>Configuration</em>' containment reference isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Configurations</em>' containment reference list.
-	 * @see org.yakindu.sct.model.sgen.SGenPackage#getGeneratorModel_Configurations()
+	 * @return the value of the '<em>Configuration</em>' containment reference.
+	 * @see #setConfiguration(GeneratorConfiguration)
+	 * @see org.yakindu.sct.model.sgen.SGenPackage#getGeneratorModel_Configuration()
 	 * @model containment="true"
 	 * @generated
 	 */
-	EList<Configuration> getConfigurations();
+	GeneratorConfiguration getConfiguration();
 
 	/**
-	 * Returns the value of the '<em><b>Statecharts</b></em>' reference list.
-	 * The list contents are of type {@link org.yakindu.sct.model.sgraph.Statechart}.
+	 * Sets the value of the '{@link org.yakindu.sct.model.sgen.GeneratorModel#getConfiguration <em>Configuration</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Configuration</em>' containment reference.
+	 * @see #getConfiguration()
+	 * @generated
+	 */
+	void setConfiguration(GeneratorConfiguration value);
+
+	/**
+	 * Returns the value of the '<em><b>Statechart References</b></em>' containment reference.
 	 * <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Statecharts</em>' reference list isn't clear,
+	 * If the meaning of the '<em>Statechart References</em>' containment reference isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Statecharts</em>' reference list.
-	 * @see org.yakindu.sct.model.sgen.SGenPackage#getGeneratorModel_Statecharts()
-	 * @model
+	 * @return the value of the '<em>Statechart References</em>' containment reference.
+	 * @see #setStatechartReferences(StatechartReferences)
+	 * @see org.yakindu.sct.model.sgen.SGenPackage#getGeneratorModel_StatechartReferences()
+	 * @model containment="true"
 	 * @generated
 	 */
-	EList<Statechart> getStatecharts();
+	StatechartReferences getStatechartReferences();
+
+	/**
+	 * Sets the value of the '{@link org.yakindu.sct.model.sgen.GeneratorModel#getStatechartReferences <em>Statechart References</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Statechart References</em>' containment reference.
+	 * @see #getStatechartReferences()
+	 * @generated
+	 */
+	void setStatechartReferences(StatechartReferences value);
 
 } // GeneratorModel
