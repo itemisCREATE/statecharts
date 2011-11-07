7926c9a0f880762c868af046f0021cd3832b1191
benjamin.schwertfeger@gmail.com <benjamin.schwertfeger@itemis.de>
2011-11-07 23:05:07 +0000

    StextRuntimeModule wieder auf ResourceSetGlobalScopeProvider umgestellt. Live_Scope gab Probleme in anderen Projekten

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/STextRuntimeModule.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/STextRuntimeModule.java
@@ -4,6 +4,7 @@ import org.eclipse.xtext.linking.ILinker;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.scoping.IScopeProvider;
 import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
+import org.eclipse.xtext.scoping.impl.ResourceSetGlobalScopeProvider;
 import org.yakindu.sct.model.stext.naming.StextNameProvider;
 import org.yakindu.sct.model.stext.scoping.NamespaceLocalScopeResolver;
 
@@ -16,7 +17,12 @@ import de.itemis.xtext.utils.gmf.resource.InjectMembersLazyLinker;
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class STextRuntimeModule extends org.yakindu.sct.model.stext.AbstractSTextRuntimeModule {
-	
+
+	public Class<? extends org.eclipse.xtext.scoping.IGlobalScopeProvider> bindIGlobalScopeProvider() {
+		super.bindIGlobalScopeProvider();
+		return ResourceSetGlobalScopeProvider.class;
+	}
+
 	@Override
 	public Class<? extends ILinker> bindILinker() {
 		return InjectMembersLazyLinker.class;
