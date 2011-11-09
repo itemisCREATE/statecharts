eb749130d15ba822c4a33f2de8aee68466c3075f
Andreas Mülder <A.Muelder@googlemail.com>
2011-11-09 15:31:42 +0000

    Adopted Java, C and Cpp Code Generators to new Code Generator Infrastructure

--- plugins/org.yakindu.sct.generator.java/src/org/yakindu/sct/generator/java/JavaSCTGenerator.java
+++ plugins/org.yakindu.sct.generator.java/src/org/yakindu/sct/generator/java/JavaSCTGenerator.java
@@ -1,17 +1,30 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
 package org.yakindu.sct.generator.java;
 
-import org.yakindu.sct.generator.base.AbstractSCTGenerator;
-import org.yakindu.sct.model.sgen.GeneratorModel;
+import org.yakindu.sct.generator.core.ISCTGenerator;
+import org.yakindu.sct.generator.core.impl.AbstractXpandBasedCodeGenerator;
 
-public class JavaSCTGenerator extends AbstractSCTGenerator<GeneratorModel> {
-
-	private static final String GENMODEL_EXTENSION = "javagen";
+/**
+ * Implementation of {@link ISCTGenerator} interface registered via extension
+ * point
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public class JavaSCTGenerator extends AbstractXpandBasedCodeGenerator {
 
 	@Override
-	public void generate(GeneratorModel genmodel) {
-		System.out.println("------------------------");
-		System.out.println("JavaSCTGenerator.generate() " + genmodel);
-		System.out.println("------------------------");
+	public String getTemplatePath() {
+		return "org::yakindu::sct::generator::java::templates::Main::main";
 	}
 
 }
