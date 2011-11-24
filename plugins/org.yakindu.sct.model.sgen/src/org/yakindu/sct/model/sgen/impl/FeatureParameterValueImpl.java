f6a44ae41c31785741de57cbd8282e04b1adcce2
Andreas Mülder <A.Muelder@googlemail.com>
2011-11-24 15:20:43 +0000

    removed warnings

--- plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/impl/FeatureParameterValueImpl.java
+++ plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/impl/FeatureParameterValueImpl.java
@@ -6,8 +6,6 @@
  */
 package org.yakindu.sct.model.sgen.impl;
 
-import java.nio.channels.IllegalSelectorException;
-
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EClass;
