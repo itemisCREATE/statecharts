e883c35781043f14a6a084b70450ae8e4da959ec
markus.muehlbrandt@itemis.de <markus.muehlbrandt@itemis.de>
2011-11-24 15:16:38 +0000

    Moved general extensions to BaseExtension.ext. Added support for entry- and exit sequences. Added support for StateSwitch and Case.

---
+++ plugins/org.yakindu.sct.generator.java/src/org/yakindu/sct/generator/java/features/JavaFeatureConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.generator.java.features;
+
+/**
+ * 
+ * @author muehlbrandt
+ */
+public final class JavaFeatureConstants implements IJavaFeatureConstants {
+	
+	public static final String getNamingFeature() {
+		return NAMING_FEATURE;
+	}
+	
+	public static final String getBasePackage() {
+		return BASE_PACKAGE;
+	}
+	
+	public static final String getImplementationSuffix() {
+		return IMPLEMENTATION_SUFFIX;
+	}
+}
