42fcb133ccde4b36cbf5886f4febb0023e8a427f
markus.muehlbrandt@itemis.de <markus.muehlbrandt@itemis.de>
2011-11-29 14:29:28 +0000

    Added testcases for Test_Expression model.

--- test-plugins/org.yakindu.sct.generator.java.runtime.test/src/org/yakindu/sct/generator/java/runtime/test/TestExpressionCycleBasedStatemachine.java
+++ test-plugins/org.yakindu.sct.generator.java.runtime.test/src/org/yakindu/sct/generator/java/runtime/test/TestExpressionCycleBasedStatemachine.java
@@ -23,7 +23,7 @@ public class TestExpressionCycleBasedStatemachine {
 	}
 	
 	@Test
-	public void testTransitionState1_State2() {
+	public void testCycles() {
 		assertEquals("Var3 value not set correct: ", 2405.6, statemachine
 				.getInterfaceDefault().getVarVar3(),Math.pow(10, -8));
 		
@@ -35,5 +35,46 @@ public class TestExpressionCycleBasedStatemachine {
 				.getInterfaceDefault().getVarVar2());
 		assertEquals("Default.Var4 value not set correct: ", 22.3, statemachine
 				.getInterfaceDefault().getVarVar4(),Math.pow(10, -8));
+		assertEquals("Other.Var3 value not set correct: ", 481.12, statemachine
+				.getInterfaceDefault().getVarVar3(),Math.pow(10, -8));
+		assertEquals("Default.Var1 value not set correct: ", 7, statemachine
+				.getInterfaceDefault().getVarVar1());
+		
+		statemachine.runCycle();
+		assertEquals("Other.Var1 value not set correct: ", false, statemachine
+				.getInterfaceOther().getVarVar1());
+		assertEquals("Default.Var2 value not set correct: ", 1, statemachine
+				.getInterfaceDefault().getVarVar2());
+		assertEquals("Default.Var4 value not set correct: ", 22.3, statemachine
+				.getInterfaceDefault().getVarVar4(),Math.pow(10, -8));
+		assertEquals("Other.Var3 value not set correct: ", 481.12, statemachine
+				.getInterfaceDefault().getVarVar3(),Math.pow(10, -8));
+		assertEquals("Default.Var1 value not set correct: ", 8, statemachine
+				.getInterfaceDefault().getVarVar1());
+		
+		statemachine.runCycle();
+		assertEquals("Other.Var1 value not set correct: ", false, statemachine
+				.getInterfaceOther().getVarVar1());
+		assertEquals("Default.Var2 value not set correct: ", 1, statemachine
+				.getInterfaceDefault().getVarVar2());
+		assertEquals("Default.Var4 value not set correct: ", 22.3, statemachine
+				.getInterfaceDefault().getVarVar4(),Math.pow(10, -8));
+		assertEquals("Other.Var3 value not set correct: ", 481.12, statemachine
+				.getInterfaceDefault().getVarVar3(),Math.pow(10, -8));
+		assertEquals("Default.Var1 value not set correct: ", 9, statemachine
+				.getInterfaceDefault().getVarVar1());
+		
+		statemachine.getInterfaceDefault().raiseEvent1();
+		statemachine.runCycle();
+		assertEquals("Default.Var5 value not set correct: ", true, statemachine
+				.getInterfaceDefault().getVarVar5());
+		assertEquals("Other.Var3 value not set correct: ", 962.24, statemachine
+				.getInterfaceDefault().getVarVar3(),Math.pow(10, -8));
+		
+		statemachine.getInterfaceDefault().raiseEvent1();
+		statemachine.runCycle();
+		assertEquals("Other.Var3 value not set correct: ", 962.24, statemachine
+				.getInterfaceDefault().getVarVar3(),Math.pow(10, -8));
+		
 	}
 }
