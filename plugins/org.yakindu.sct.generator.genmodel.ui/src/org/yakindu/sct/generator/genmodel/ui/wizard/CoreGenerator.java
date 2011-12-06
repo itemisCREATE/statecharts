f8af168c88db9a6ef1306e0c232fb9801c85e248
holger.willebrandt@gmail.com <holger.willebrandt@gmail.com>
2011-12-06 16:12:08 +0000

    fix inconsistent naming of generators; provide default sorting of generators in GenModel Wizard

--- plugins/org.yakindu.sct.generator.genmodel.ui/src/org/yakindu/sct/generator/genmodel/ui/wizard/CoreGenerator.java
+++ plugins/org.yakindu.sct.generator.genmodel.ui/src/org/yakindu/sct/generator/genmodel/ui/wizard/CoreGenerator.java
@@ -10,12 +10,20 @@
  */
 package org.yakindu.sct.generator.genmodel.ui.wizard;
 
+import java.util.Comparator;
+
+import org.yakindu.sct.generator.core.extensions.GeneratorExtensions.GeneratorDescriptor;
+
 /**
+ * Defines all Generators that are known to be core-generators (provided by
+ * YAKINDU)
+ * 
  * @author holger willebrandt - Initial contribution and API
  */
 public enum CoreGenerator {
 
-	Java("yakindu::java"), C("yakindu::c"), Cpp("yakindu::cpp");
+	Java("yakindu::java"), C("yakindu::c"), Cpp("yakindu::cpp"), GenericJava(
+			"yakindu::generic"), Xpand("yakindu::xpand");
 
 	private String id;
 
@@ -26,5 +34,52 @@ public enum CoreGenerator {
 	public String getId() {
 		return id;
 	}
+
+	static CoreGenerator parseId(String id) {
+		if (Java.id.equals(id)) {
+			return Java;
+		}
+		if (C.id.equals(id)) {
+			return C;
+		}
+		if (Cpp.id.equals(id)) {
+			return Cpp;
+		}
+		if (GenericJava.id.equals(id)) {
+			return GenericJava;
+		}
+		if (Xpand.id.equals(id)) {
+			return Xpand;
+		}
+		throw new IllegalArgumentException(String.format(
+				"No such generator id '%s'", id));
+	}
+
+	/**
+	 * Comparator to sort {@link GeneratorDescriptor}s. YAKINDU
+	 * {@link CoreGenerator}s come first.
+	 */
+	public static final Comparator<GeneratorDescriptor> generatorOrder = new Comparator<GeneratorDescriptor>() {
+
+		public int compare(GeneratorDescriptor o1, GeneratorDescriptor o2) {
+			int a = 0;
+			int b = 0;
+			try {
+				a = CoreGenerator.parseId(o1.getId()).ordinal();
+			} catch (IllegalArgumentException e) {
+				a = Integer.MAX_VALUE;
+			}
+			try {
+				b = CoreGenerator.parseId(o2.getId()).ordinal();
+			} catch (IllegalArgumentException e) {
+				b = Integer.MAX_VALUE;
+			}
+			int result = Integer.valueOf(a).compareTo(b);
+			if (result == 0) {
+				result = o1.getName().compareTo(o2.getName());
+			}
+			return result;
+		}
+	};
 
 }
