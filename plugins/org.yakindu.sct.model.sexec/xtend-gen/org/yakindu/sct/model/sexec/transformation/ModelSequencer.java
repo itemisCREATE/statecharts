f28808bd5c9239b792323f948affa2c47f16fc8a
terfloth@itemis.de <terfloth@itemis.de>
2012-01-02 08:51:20 +0000

    sexec transformation creates StateVector elements for all states. ( YAKHMI-107 )

--- plugins/org.yakindu.sct.model.sexec/xtend-gen/org/yakindu/sct/model/sexec/transformation/ModelSequencer.java
+++ plugins/org.yakindu.sct.model.sexec/xtend-gen/org/yakindu/sct/model/sexec/transformation/ModelSequencer.java
@@ -1512,14 +1512,91 @@ public class ModelSequencer {
   
   public void defineStateVector(final ExecutionFlow flow, final Statechart sc) {
     {
+      int offset = 0;
+      EList<Region> _regions = sc.getRegions();
+      for (Region r : _regions) {
+        int _defineStateVectors = this.defineStateVectors(r, offset);
+        int _operator_plus = IntegerExtensions.operator_plus(((Integer)offset), ((Integer)_defineStateVectors));
+        offset = _operator_plus;
+      }
       SexecFactory _sexecFactory = this.sexecFactory();
       StateVector _createStateVector = _sexecFactory.createStateVector();
       flow.setStateVector(_createStateVector);
       StateVector _stateVector = flow.getStateVector();
       _stateVector.setOffset(0);
       StateVector _stateVector_1 = flow.getStateVector();
-      int _maxOrthogonality = this.sct.maxOrthogonality(sc);
-      _stateVector_1.setSize(_maxOrthogonality);
+      _stateVector_1.setSize(offset);
+    }
+  }
+  
+  public int defineStateVectors(final Statechart sc, final int offset) {
+    EList<Region> _regions = sc.getRegions();
+    final Function2<Integer,Region,Integer> _function = new Function2<Integer,Region,Integer>() {
+        public Integer apply(final Integer o , final Region r) {
+          int _maxOrthogonality = ModelSequencer.this.sct.maxOrthogonality(r);
+          int _operator_plus = IntegerExtensions.operator_plus(((Integer)_maxOrthogonality), o);
+          return ((Integer)_operator_plus);
+        }
+      };
+    Integer _fold = IterableExtensions.<Region, Integer>fold(_regions, ((Integer)0), _function);
+    return _fold;
+  }
+  
+  public int defineStateVectors(final Region r, final int offset) {
+    EList<Vertex> _vertices = r.getVertices();
+    final Function2<Integer,Vertex,Integer> _function = new Function2<Integer,Vertex,Integer>() {
+        public Integer apply(final Integer s , final Vertex v) {
+          int _xblockexpression = (int) 0;
+          {
+            int _defineStateVectors = ModelSequencer.this.defineStateVectors(v, ((Integer)offset));
+            final int mo = _defineStateVectors;
+            int _xifexpression = (int) 0;
+            boolean _operator_greaterThan = ComparableExtensions.<Integer>operator_greaterThan(((Integer)mo), s);
+            if (_operator_greaterThan) {
+              _xifexpression = mo;
+            } else {
+              _xifexpression = s;
+            }
+            _xblockexpression = (_xifexpression);
+          }
+          return ((Integer)_xblockexpression);
+        }
+      };
+    Integer _fold = IterableExtensions.<Vertex, Integer>fold(_vertices, ((Integer)0), _function);
+    return _fold;
+  }
+  
+  protected int _defineStateVectors(final Vertex v, final int offset) {
+    return 0;
+  }
+  
+  protected int _defineStateVectors(final State s, final int offset) {
+    {
+      int maxOrthogonality = 0;
+      EList<Region> _regions = s.getRegions();
+      int _size = _regions.size();
+      boolean _operator_greaterThan = ComparableExtensions.<Integer>operator_greaterThan(((Integer)_size), ((Integer)0));
+      if (_operator_greaterThan) {
+        EList<Region> _regions_1 = s.getRegions();
+        for (Region r : _regions_1) {
+          int _operator_plus = IntegerExtensions.operator_plus(((Integer)offset), ((Integer)maxOrthogonality));
+          int _defineStateVectors = this.defineStateVectors(r, _operator_plus);
+          int _operator_plus_1 = IntegerExtensions.operator_plus(((Integer)maxOrthogonality), ((Integer)_defineStateVectors));
+          maxOrthogonality = _operator_plus_1;
+        }
+      } else {
+        maxOrthogonality = 1;
+      }
+      ExecutionState _create = this.factory.create(s);
+      final ExecutionState es = _create;
+      SexecFactory _sexecFactory = this.sexecFactory();
+      StateVector _createStateVector = _sexecFactory.createStateVector();
+      es.setStateVector(_createStateVector);
+      StateVector _stateVector = es.getStateVector();
+      _stateVector.setOffset(offset);
+      StateVector _stateVector_1 = es.getStateVector();
+      _stateVector_1.setSize(maxOrthogonality);
+      return maxOrthogonality;
     }
   }
   
@@ -2198,6 +2275,19 @@ public class ModelSequencer {
     } else {
       throw new IllegalArgumentException("Unhandled parameter types: " +
         java.util.Arrays.<Object>asList(e).toString());
+    }
+  }
+  
+  public int defineStateVectors(final Vertex s, final Integer offset) {
+    if ((s instanceof State)
+         && (offset instanceof Integer)) {
+      return _defineStateVectors((State)s, (Integer)offset);
+    } else if ((s instanceof Vertex)
+         && (offset instanceof Integer)) {
+      return _defineStateVectors((Vertex)s, (Integer)offset);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        java.util.Arrays.<Object>asList(s, offset).toString());
     }
   }
   
