d5d80cb0acc9f66677221b96322504cbeb67380a
Andreas Mülder <A.Muelder@googlemail.com>
2012-01-05 12:38:13 +0000

    Added a converter that escapes invalid identifier names for States and Regions to allow crossreferencing in active() Statement

--- plugins/org.yakindu.sct.model.sgraph/src/org/yakindu/sct/model/sgraph/naming/SGraphNameProvider.java
+++ plugins/org.yakindu.sct.model.sgraph/src/org/yakindu/sct/model/sgraph/naming/SGraphNameProvider.java
@@ -10,9 +10,6 @@
  */
 package org.yakindu.sct.model.sgraph.naming;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
@@ -20,17 +17,14 @@ import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.util.SimpleAttributeResolver;
 import org.eclipse.xtext.util.Strings;
-import org.yakindu.sct.model.sgraph.Choice;
 import org.yakindu.sct.model.sgraph.Declaration;
 import org.yakindu.sct.model.sgraph.FinalState;
 import org.yakindu.sct.model.sgraph.Region;
 import org.yakindu.sct.model.sgraph.Scope;
 import org.yakindu.sct.model.sgraph.ScopedElement;
+import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
-import org.yakindu.sct.model.sgraph.Vertex;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
 import com.google.inject.Inject;
 
 /**
@@ -43,10 +37,11 @@ import com.google.inject.Inject;
 public class SGraphNameProvider extends DefaultDeclarativeQualifiedNameProvider {
 
 	private static final String _FINAL_STATE_NAME = "_final_";
-	private static final String _CHOICE_NAME = "_choice_";
 
 	@Inject
 	private IQualifiedNameConverter nameConverter;
+	@Inject
+	private IdentifierConverter identifierConverer;
 
 	public QualifiedName qualifiedName(Statechart ele) {
 		String scName = ele.getName();
@@ -76,8 +71,23 @@ public class SGraphNameProvider extends DefaultDeclarativeQualifiedNameProvider 
 	}
 
 	public QualifiedName qualifiedName(FinalState ele) {
+		ele.getName();
+		QualifiedName qfn = QualifiedName.create(_FINAL_STATE_NAME);
 		EObject temp = ele;
-		QualifiedName qualifiedNameFromConverter =QualifiedName.create(_FINAL_STATE_NAME);
+		while (temp.eContainer() != null) {
+			temp = temp.eContainer();
+			QualifiedName parentsQualifiedName = getFullyQualifiedName(temp);
+			if (parentsQualifiedName != null)
+				return parentsQualifiedName.append(qfn);
+		}
+		return qfn;
+	}
+	
+	public QualifiedName qualifiedName(State ele) {
+		EObject temp = ele;
+		ele.getName();
+		QualifiedName qualifiedNameFromConverter = QualifiedName
+				.create(identifierConverer.toIdentifier(ele.getName()));
 		while (temp.eContainer() != null) {
 			temp = temp.eContainer();
 			QualifiedName parentsQualifiedName = getFullyQualifiedName(temp);
@@ -86,18 +96,12 @@ public class SGraphNameProvider extends DefaultDeclarativeQualifiedNameProvider 
 		}
 		return qualifiedNameFromConverter;
 	}
-
-	public QualifiedName qualifiedName(Choice ele) {
-		
-		// first get order number of choice node 
-		List<Vertex> choiceList = new ArrayList<Vertex>();
-		choiceList.addAll(Collections2.filter(((Region) ele.eContainer()).getVertices(), new Predicate<Vertex>() {
-			public boolean apply(Vertex input) { return input instanceof Choice; }
-		}));
-		int index = choiceList.indexOf(ele);
-				
+	
+	public QualifiedName qualifiedName(Region ele) {
 		EObject temp = ele;
-		QualifiedName qualifiedNameFromConverter = QualifiedName.create(_CHOICE_NAME + index);
+		ele.getName();
+		QualifiedName qualifiedNameFromConverter = QualifiedName
+				.create(identifierConverer.toIdentifier(ele.getName()));
 		while (temp.eContainer() != null) {
 			temp = temp.eContainer();
 			QualifiedName parentsQualifiedName = getFullyQualifiedName(temp);
@@ -106,6 +110,7 @@ public class SGraphNameProvider extends DefaultDeclarativeQualifiedNameProvider 
 		}
 		return qualifiedNameFromConverter;
 	}
+	
 
 	public QualifiedName qualifiedName(Declaration ele) {
 		QualifiedName name = null;
