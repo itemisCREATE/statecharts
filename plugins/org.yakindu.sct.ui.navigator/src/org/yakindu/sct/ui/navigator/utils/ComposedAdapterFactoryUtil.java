7e048fe10091fe21c06acd2709818c287cf70856
benjamin.schwertfeger@gmail.com <benjamin.schwertfeger@itemis.de>
2012-01-10 10:38:57 +0000

    edit  plugin for stext

--- plugins/org.yakindu.sct.ui.navigator/src/org/yakindu/sct/ui/navigator/utils/ComposedAdapterFactoryUtil.java
+++ plugins/org.yakindu.sct.ui.navigator/src/org/yakindu/sct/ui/navigator/utils/ComposedAdapterFactoryUtil.java
@@ -1,22 +1,19 @@
 package org.yakindu.sct.ui.navigator.utils;
 
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
-import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
-import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
-import org.yakindu.sct.model.sgraph.provider.SGraphItemProviderAdapterFactory;
-import org.yakindu.sct.model.sgraph.util.SGraphAdapterFactory;
-import org.yakindu.sct.model.stext.stext.util.StextAdapterFactory;
 
 public class ComposedAdapterFactoryUtil {
 
 	public static final ComposedAdapterFactory FACTORY;
 	static {
-		FACTORY = new ComposedAdapterFactory();
-		FACTORY.addAdapterFactory(new ResourceItemProviderAdapterFactory());
-		FACTORY.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
-		FACTORY.addAdapterFactory(new SGraphAdapterFactory());
-		FACTORY.addAdapterFactory(new StextAdapterFactory());
-		FACTORY.addAdapterFactory(new SGraphItemProviderAdapterFactory());
+		FACTORY = new ComposedAdapterFactory(
+				ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+		// FACTORY.addAdapterFactory(new ResourceItemProviderAdapterFactory());
+		// FACTORY.addAdapterFactory(new
+		// ReflectiveItemProviderAdapterFactory());
+
+		// FACTORY.addAdapterFactory(new SGraphItemProviderAdapterFactory());
 
 	}
+
 }
