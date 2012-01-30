42c5fe92c790dc14aaf1547edd38f2eed81d18c5
terfloth@itemis.de <terfloth@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-01-30 12:18:17 +0000

    updated color settings for simulation highlighting

--- SCT2/trunk/plugins/org.yakindu.sct.simulation.ui/src/org/yakindu/sct/simulation/ui/preferences/SimulationPreferenceConstants.java
+++ SCT2/trunk/plugins/org.yakindu.sct.simulation.ui/src/org/yakindu/sct/simulation/ui/preferences/SimulationPreferenceConstants.java
@@ -13,13 +13,16 @@ package org.yakindu.sct.simulation.ui.preferences;
 /**
  * 
  * @author andreas muelder - Initial contribution and API
+ * @author axel terfloth - extensions
  *
  */
 public class SimulationPreferenceConstants {
 
 	public static final String STATE_FOREGROUND_HIGHLIGHTING_COLOR = "stateForegroundHighlightingColor";
-
 	public static final String STATE_BACKGROUND_HIGHLIGHTING_COLOR = "stateBackgroundHighlightingColor";
+
+	public static final String VERTEX_FOREGROUND_TRANSIENT_COLOR = "vertexForegroundTransientColor";
+	public static final String VERTEX_BACKGROUND_TRANSIENT_COLOR = "vertexBackgroundTransientColor";
 
 	public static final String TRANSITION_HIGHLIGHTING_COLOR = "transitionHighlightingColor";
 
