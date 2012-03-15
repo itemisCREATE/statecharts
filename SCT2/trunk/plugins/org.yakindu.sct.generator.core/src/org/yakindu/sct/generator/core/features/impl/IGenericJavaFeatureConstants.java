3d566a0b589a995f94de146e539248a326027983
benjamin.schwertfeger@gmail.com <benjamin.schwertfeger@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-03-15 12:27:46 +0000

    overridingGuiceModule parameter renamed to configurationModule

--- SCT2/trunk/plugins/org.yakindu.sct.generator.core/src/org/yakindu/sct/generator/core/features/impl/IGenericJavaFeatureConstants.java
+++ SCT2/trunk/plugins/org.yakindu.sct.generator.core/src/org/yakindu/sct/generator/core/features/impl/IGenericJavaFeatureConstants.java
@@ -15,5 +15,5 @@ public interface IGenericJavaFeatureConstants {
 	public static final String TEMPLATE_FEATURE = "Generator";
 	public static final String GENERATOR_PROJECT = "generatorProject";
 	public static final String GENERATOR_CLASS = "generatorClass";
-	public static final String OVERRIDING_GUICE_MODULE = "overridingGuiceModule";
+	public static final String CONFIGURATION_MODULE = "configurationModule";
 }
