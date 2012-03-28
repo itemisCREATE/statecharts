739a15aac603f841b3bfd03311c19903d046a44f
Andreas Mülder <A.Muelder@googlemail.com>
2012-03-28 11:07:13 +0000

    suppress warnings for SGenGrammarAccess

--- plugins/org.yakindu.sct.generator.genmodel/src-gen/org/yakindu/sct/generator/genmodel/services/SGenGrammarAccess.java
+++ plugins/org.yakindu.sct.generator.genmodel/src-gen/org/yakindu/sct/generator/genmodel/services/SGenGrammarAccess.java
@@ -14,6 +14,7 @@ import org.eclipse.xtext.service.AbstractElementFinder.*;
 import org.eclipse.xtext.xbase.services.XbaseGrammarAccess;
 import org.eclipse.xtext.xbase.services.XtypeGrammarAccess;
 
+@SuppressWarnings("all")
 @Singleton
 public class SGenGrammarAccess extends AbstractGrammarElementFinder {
 	
