3a08079a34fb3308b121801e6a8912ba30d3d229
Andreas Mülder <A.Muelder@googlemail.com>
2012-04-20 09:55:56 +0000

    Share project "org.yakindu.sct.generator.genmodel.test" into "https://svn.codespot.com/a/eclipselabs.org/yakindu"

---
+++ test-plugins/org.yakindu.sct.generator.genmodel.test/src/org/yakindu/sct/generator/genmodel/test/SGenParserRuleTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.generator.genmodel.test;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.junit.runner.RunWith;
+import org.yakindu.sct.generator.genmodel.test.util.AbstractSGenTest;
+import org.yakindu.sct.generator.genmodel.test.util.SGenInjectorProvider;
+
+/**
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(SGenInjectorProvider.class)
+public class SGenParserRuleTest extends AbstractSGenTest {
+	
+}
