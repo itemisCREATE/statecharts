432bbd9538fa41856e3bb7c3f6fb86884542bdef
Andreas Mülder <A.Muelder@googlemail.com>
2012-04-30 15:15:35 +0000

    Share project "org.yakindu.sct.generator.java.test" into "https://svn.codespot.com/a/eclipselabs.org/yakindu"

---
+++ test-plugins/org.yakindu.sct.generator.java.test/src/org/yakindu/sct/generator/java/JavaSCTGeneratorTest.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.generator.java;
+
+import static util.TestModels.DEEP_HISTORY;
+import static util.TestModels.FEATURE_CALLS;
+import static util.TestModels.GUARD;
+import static util.TestModels.SIMPLE_HIERACHY;
+import static util.TestModels.STATECHART_LOCAL_REACTIONS;
+import static util.TestModels.STATE_ACTIVE;
+import static util.TestModels.VALUED_EVENTS;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.tools.Diagnostic;
+import javax.tools.Diagnostic.Kind;
+import javax.tools.JavaFileObject;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yakindu.sct.generator.java.util.AbstractJavaGeneratorTest;
+import org.yakindu.sct.generator.java.util.JavaGeneratorInjectionProvider;
+import org.yakindu.sct.model.sgraph.Statechart;
+
+import util.TestModels;
+
+import com.google.inject.Inject;
+
+/**
+ * These tests only check for compile errors, they don't check the behaviour of
+ * the generated Statemachine!
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(JavaGeneratorInjectionProvider.class)
+public class JavaSCTGeneratorTest extends AbstractJavaGeneratorTest {
+
+	@Inject
+	private TestModels models;
+
+	@Test
+	public void testGuardModel() throws Exception {
+		Statechart statechart = models.loadStatechartFromResource(GUARD);
+		failOnError(generateAndCompile(statechart));
+	}
+
+	@Test
+	public void testSimpleHierachyModel() throws Exception {
+		Statechart statechart = models
+				.loadStatechartFromResource(SIMPLE_HIERACHY);
+		failOnError(generateAndCompile(statechart));
+
+	}
+
+	@Test
+	public void testDeepHistoryModel() throws Exception {
+		Statechart statechart = models.loadStatechartFromResource(DEEP_HISTORY);
+		failOnError(generateAndCompile(statechart));
+
+	}
+
+	@Test
+	public void testStateActiveModel() throws Exception {
+		Statechart statechart = models.loadStatechartFromResource(STATE_ACTIVE);
+		failOnError(generateAndCompile(statechart));
+
+	}
+
+	@Test
+	public void testValuedEventsModel() throws Exception {
+		Statechart statechart = models
+				.loadStatechartFromResource(VALUED_EVENTS);
+		failOnError(generateAndCompile(statechart));
+
+	}
+
+	@Test
+	public void testFeatureCallsModel() throws Exception {
+		Statechart statechart = models
+				.loadStatechartFromResource(FEATURE_CALLS);
+		failOnError(generateAndCompile(statechart));
+
+	}
+
+	@Test
+	public void testStatechartLocalReactionsModel() throws Exception {
+		Statechart statechart = models
+				.loadStatechartFromResource(STATECHART_LOCAL_REACTIONS);
+		failOnError(generateAndCompile(statechart));
+
+	}
+
+	private void failOnError(
+			List<Diagnostic<? extends JavaFileObject>> testModel) {
+		for (Diagnostic<? extends JavaFileObject> diagnostic : testModel) {
+			if (diagnostic.getKind() == Kind.ERROR) {
+				Assert.fail(diagnostic.getMessage(Locale.getDefault()));
+			}
+		}
+	}
+
+}
---
+++ test-plugins/org.yakindu.sct.generator.java.test/src/org/yakindu/sct/generator/java/util/AbstractJavaGeneratorTest.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.generator.java.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Assert;
+import org.junit.Before;
+import org.yakindu.sct.generator.core.util.GeneratorUtils;
+import org.yakindu.sct.generator.java.JavaSCTGenerator;
+import org.yakindu.sct.model.sgen.FeatureConfiguration;
+import org.yakindu.sct.model.sgen.FeatureParameter;
+import org.yakindu.sct.model.sgen.FeatureParameterValue;
+import org.yakindu.sct.model.sgen.FeatureType;
+import org.yakindu.sct.model.sgen.GeneratorEntry;
+import org.yakindu.sct.model.sgen.SGenFactory;
+import org.yakindu.sct.model.sgraph.Statechart;
+
+import com.google.inject.Inject;
+
+/**
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public abstract class AbstractJavaGeneratorTest {
+
+	private static final String SRC_GEN = "src-gen";
+	private static final String TARGET_PROJECT = "targetProject";
+	private static final String CONTENT_TYPE = "statechart";
+	private static final String OUTLET_FEATURE = "Outlet";
+	private static final String TARGET_FOLDER = "targetFolder";
+
+	@Inject
+	protected JavaSCTGenerator generator;
+	private JavaCompiler compiler;
+
+	@Before
+	public void setup() {
+		compiler = ToolProvider.getSystemJavaCompiler();
+		Assert.assertNotNull("No JDK 1.06 found", compiler);
+	}
+
+	public List<Diagnostic<? extends JavaFileObject>> generateAndCompile(
+			Statechart statechart) throws Exception {
+
+		GeneratorEntry entry = createGeneratorEntry("JavaGeneratorProject",
+				SRC_GEN);
+		entry.setElementRef(statechart);
+		IProject targetProject = GeneratorUtils.getTargetProject(entry);
+		targetProject.delete(true, new NullProgressMonitor());
+		generator.generate(entry);
+		IFolder folder = targetProject.getFolder(SRC_GEN);
+		Iterable<File> javaSourceFiles = getJavaSourceFiles(folder);
+		return compile(javaSourceFiles);
+	}
+
+	private List<Diagnostic<? extends JavaFileObject>> compile(
+			Iterable<File> ioFiles) throws IOException {
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(
+				null, null, null);
+		try {
+			Iterable<? extends JavaFileObject> compilationUnits = fileManager
+					.getJavaFileObjectsFromFiles(ioFiles);
+			DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+			compiler.getTask(null, fileManager, diagnostics, null, null,
+					compilationUnits).call();
+			return diagnostics.getDiagnostics();
+		} finally {
+			fileManager.close();
+		}
+	}
+
+	private GeneratorEntry createGeneratorEntry(String targetProject,
+			String targetFolder) {
+		GeneratorEntry entry = SGenFactory.eINSTANCE.createGeneratorEntry();
+		entry.setContentType(CONTENT_TYPE);
+		FeatureConfiguration config = SGenFactory.eINSTANCE
+				.createFeatureConfiguration();
+		FeatureType type = SGenFactory.eINSTANCE.createFeatureType();
+		type.setName(OUTLET_FEATURE);
+		config.setType(type);
+		FeatureParameterValue project = SGenFactory.eINSTANCE
+				.createFeatureParameterValue();
+		FeatureParameter projectParameter = SGenFactory.eINSTANCE
+				.createFeatureParameter();
+		projectParameter.setName(TARGET_PROJECT);
+		project.setParameter(projectParameter);
+		project.setValue(targetProject);
+		config.getParameterValues().add(project);
+		FeatureParameterValue targetFolderValue = SGenFactory.eINSTANCE
+				.createFeatureParameterValue();
+		FeatureParameter targetParameter = SGenFactory.eINSTANCE
+				.createFeatureParameter();
+		targetParameter.setName(TARGET_FOLDER);
+		targetFolderValue.setParameter(targetParameter);
+		targetFolderValue.setValue(targetFolder);
+		config.getParameterValues().add(targetFolderValue);
+		entry.getFeatures().add(config);
+		return entry;
+	}
+
+	private Iterable<File> getJavaSourceFiles(IResource folder)
+			throws CoreException {
+		List<IFile> files = new ArrayList<IFile>();
+		getFilesToCompile(folder, files);
+		List<File> ioFiles = new ArrayList<File>();
+		for (IFile iFile : files) {
+			ioFiles.add(iFile.getRawLocation().makeAbsolute().toFile());
+		}
+		return ioFiles;
+	}
+
+	private void getFilesToCompile(IResource resource, List<IFile> toAdd)
+			throws CoreException {
+		if (resource instanceof IFolder) {
+			IResource[] members = ((IFolder) resource).members();
+			for (IResource iResource : members) {
+				getFilesToCompile(iResource, toAdd);
+			}
+		} else if (resource instanceof IFile) {
+			if (resource.getFileExtension().equals("java")) {
+				toAdd.add((IFile) resource);
+			}
+		}
+	}
+
+}
