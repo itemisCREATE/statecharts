44c2094664066b30fb5f0fa10fdc4107a85ec233
Andreas Mülder <A.Muelder@googlemail.com>
2012-05-04 11:09:49 +0000

    Bugfix: A state with a self-transition which has a choice is only highlighted when entered the first time. When re-entering the state it is not highlighted again.

--- plugins/org.yakindu.sct.simulation.ui/src/org/yakindu/sct/simulation/ui/model/presenter/ExecutionPathDynamicNotationHandler.java
+++ plugins/org.yakindu.sct.simulation.ui/src/org/yakindu/sct/simulation/ui/model/presenter/ExecutionPathDynamicNotationHandler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.yakindu.sct.model.sexec.ReactionFired;
 import org.yakindu.sct.model.sexec.TraceBeginRunCycle;
 import org.yakindu.sct.model.sexec.TraceEndRunCycle;
@@ -18,63 +19,64 @@ import de.itemis.gmf.runtime.commons.highlighting.IHighlightingSupport;
 import de.itemis.gmf.runtime.commons.highlighting.IHighlightingSupport.Action;
 
 /**
- * This dynamic notation handler highlight the current states, the previous active states and the execution paths 
- * that are the transitions and pseudo states that were taken in the last run to completion step (runCycle).
+ * This dynamic notation handler highlight the current states, the previous
+ * active states and the execution paths that are the transitions and pseudo
+ * states that were taken in the last run to completion step (runCycle).
  * 
  * @author terfloth
  */
 public class ExecutionPathDynamicNotationHandler extends
 		DefaultDynamicNotationHandler {
 
-	
 	/** stores the highlighting actions for the last execution cycle */
 	protected List<Action> cycleUpdates = new ArrayList<Action>();
-	
+
 	protected List<RegularState> active = new ArrayList<RegularState>();
 	protected List<RegularState> previousActive = new ArrayList<RegularState>();
 	protected List<EObject> executionPathElements = new ArrayList<EObject>();
-	
-	
-	
+
 	public void restoreNotationState(IExecutionContext context) {
 		List<Action> actionList = new ArrayList<Action>();
-		
+
 		for (RegularState state : context.getAllActiveStates()) {
 			active.add(state);
-			actionList.add( new IHighlightingSupport.Highlight(state, STATE_HIGHLIGHT_PARAMS) );
+			actionList.add(new IHighlightingSupport.Highlight(state,
+					STATE_HIGHLIGHT_PARAMS));
 		}
-		
-		getHighlightingSupport().executeBatch(actionList);
-		
-	}
 
+		getHighlightingSupport().executeBatch(actionList);
+
+	}
 
 	public void visualizeStep(final TraceBeginRunCycle trace) {
-		
+
 		cycleUpdates.clear();
 
-		for ( EObject obj : previousActive ) { cycleUpdates.add( new IHighlightingSupport.Highlight(obj, null)); }
+		for (EObject obj : previousActive) {
+			cycleUpdates.add(new IHighlightingSupport.Highlight(obj, null));
+		}
 		previousActive.clear();
-		
-		for ( EObject obj : executionPathElements ) { cycleUpdates.add( new IHighlightingSupport.Highlight(obj, null)); }
+
+		for (EObject obj : executionPathElements) {
+			cycleUpdates.add(new IHighlightingSupport.Highlight(obj, null));
+		}
 		executionPathElements.clear();
-				
+
 	}
 
-	
 	public void visualizeStep(final TraceEndRunCycle trace) {
-		
+
 		List<Action> actions = new ArrayList<Action>(cycleUpdates);
 		getHighlightingSupport().executeBatch(actions);
-		
+
 	}
 
-	
 	public void visualizeStep(final TraceStateEntered trace) {
-		
+
 		active.add((RegularState) trace.getState().getSourceElement());
-		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getState().getSourceElement(), HighlightingParameters.DEFAULT) );
-		
+		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getState()
+				.getSourceElement(), HighlightingParameters.DEFAULT));
+
 	}
 
 	public void visualizeStep(final TraceStateExited trace) {
@@ -82,24 +84,28 @@ public class ExecutionPathDynamicNotationHandler extends
 		RegularState state = (RegularState) trace.getState().getSourceElement();
 
 		active.remove(state);
-		previousActive.add(state);
-		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getState().getSourceElement(), VERTEX_TRANSIENT_PARAMS) );
+		// We copy the state here, otherwise the previous active states would be
+		// deleted in visualizeSteps cycleUpdates.clear() for self transitions
+		previousActive.add(EcoreUtil.copy(state));
+		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getState()
+				.getSourceElement(), VERTEX_TRANSIENT_PARAMS));
 
 	}
 
 	public void visualizeStep(final ReactionFired trace) {
-		
+
 		executionPathElements.add(trace.getReaction().getSourceElement());
-		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getReaction().getSourceElement(), TRANSITION_PARAMS) );
+		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getReaction()
+				.getSourceElement(), TRANSITION_PARAMS));
 
 	}
 
 	public void visualizeStep(final TraceNodeExecuted trace) {
-		
+
 		executionPathElements.add(trace.getNode().getSourceElement());
-		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getNode().getSourceElement(), VERTEX_TRANSIENT_PARAMS) );
+		cycleUpdates.add(new IHighlightingSupport.Highlight(trace.getNode()
+				.getSourceElement(), VERTEX_TRANSIENT_PARAMS));
 
 	}
-
 
 }
