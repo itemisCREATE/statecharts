bbe9e4395b3d91b21b7ffd17abff4bd952c48bff
A.Muelder@googlemail.com <A.Muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-05-16 08:17:33 +0000

    Added missing copyright header and GridLayout to StatechartPreferencePage

--- SCT2/trunk/plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/preferences/StatechartAppearancePreferencePage.java
+++ SCT2/trunk/plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/preferences/StatechartAppearancePreferencePage.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
 package org.yakindu.sct.ui.editor.preferences;
 
 import org.eclipse.jface.preference.ColorFieldEditor;
@@ -10,20 +20,25 @@ import org.yakindu.sct.ui.editor.DiagramActivator;
 public class StatechartAppearancePreferencePage extends
 		FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
+	public StatechartAppearancePreferencePage() {
+		super(GRID);
+		setDescription("Preference page for statechart editor colors");
+	}
+	
 	@Override
 	protected void createFieldEditors() {
 		addField(new ColorFieldEditor(
 				StatechartPreferenceConstants.PREF_REGION_BACKGROUND,
-				"Region background color", getFieldEditorParent()));
+				"Region background color:", getFieldEditorParent()));
 		addField(new ColorFieldEditor(
 				StatechartPreferenceConstants.PREF_REGION_LINE,
-				"Region line color", getFieldEditorParent()));
+				"Region line color:", getFieldEditorParent()));
 		addField(new ColorFieldEditor(
 				StatechartPreferenceConstants.PREF_STATE_BACKGROUND,
-				"State background color", getFieldEditorParent()));
+				"State background color:", getFieldEditorParent()));
 		addField(new ColorFieldEditor(
 				StatechartPreferenceConstants.PREF_STATE_LINE,
-				"State line color", getFieldEditorParent()));
+				"State line color:", getFieldEditorParent()));
 	}
 
 	public void init(IWorkbench workbench) {
