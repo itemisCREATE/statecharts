0e63490050e47db6e473b21c92c86b8fe344b8ae
markus.muehlbrandt@gmail.com <markus.muehlbrandt@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-06-21 15:27:08 +0000

    Generated java code uses FQN state names. Refactoring of SCTUnit java testcase generator. Refactoring of testcases.

---
+++ SCT2/trunk/examples/org.yakindu.sct.examples.trafficlight/src-gen/org/yakindu/sct/examples/trafficlight/cyclebased/trafficlightwaiting/ITrafficLightWaitingStatemachine.java
@@ -0,0 +1,47 @@
+package org.yakindu.sct.examples.trafficlight.cyclebased.trafficlightwaiting;
+
+import org.yakindu.sct.examples.trafficlight.cyclebased.IStatemachine;
+import org.yakindu.sct.examples.trafficlight.cyclebased.ITimedStatemachine;
+
+public interface ITrafficLightWaitingStatemachine
+		extends
+			ITimedStatemachine,
+			IStatemachine {
+
+	public interface SCITrafficLight {
+
+		public boolean getRed();
+		public void setRed(boolean value);
+		public boolean getYellow();
+		public void setYellow(boolean value);
+		public boolean getGreen();
+		public void setGreen(boolean value);
+
+	}
+
+	public interface SCIPedestrian {
+
+		public boolean getRequest();
+		public void setRequest(boolean value);
+		public boolean getRed();
+		public void setRed(boolean value);
+		public boolean getGreen();
+		public void setGreen(boolean value);
+
+	}
+
+	public interface SCIDefault {
+
+		public void raisePedestrianRequest();
+
+		public void raiseOnOff();
+
+	}
+
+	public SCITrafficLight getSCITrafficLight();
+
+	public SCIPedestrian getSCIPedestrian();
+
+	public SCIDefault getSCIDefault();
+
+}
