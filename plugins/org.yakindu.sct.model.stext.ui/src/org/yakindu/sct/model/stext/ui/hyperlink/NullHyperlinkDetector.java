49658ef395fc21cb4d39f872b5c1aae583366610
Andreas Mülder <A.Muelder@googlemail.com>
2012-08-14 13:20:24 +0000

    Disabled xtext hyperlinking for stext

---
+++ plugins/org.yakindu.sct.model.stext.ui/src/org/yakindu/sct/model/stext/ui/hyperlink/NullHyperlinkDetector.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.model.stext.ui.hyperlink;
+
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
+
+/**
+ * Null Impl for Hyperlink Detector
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public class NullHyperlinkDetector implements IHyperlinkDetector {
+
+	@Override
+	public IHyperlink[] detectHyperlinks(ITextViewer textViewer,
+			IRegion region, boolean canShowMultipleHyperlinks) {
+		return null;
+	}
+
+}
