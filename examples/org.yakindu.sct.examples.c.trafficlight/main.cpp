df5ee49e58e9ed6b84bb6209677e52d392bc65da
markus.muehlbrandt@gmail.com <markus.muehlbrandt@itemis.de>
2012-10-19 13:34:28 +0000

    Added comment header

--- examples/org.yakindu.sct.examples.c.trafficlight/main.cpp
+++ examples/org.yakindu.sct.examples.c.trafficlight/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	m.muehlbrandt - initial API and implementation
+ *
+ */
+
 #include "org_yakindu_sct_examples_c_trafficlight.h"
 
 #include <QtGui>
