737efdc0a403893cf3b70e85af34745c86d4f5fc
terfloth@itemis.de <terfloth@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-11-17 22:42:21 +0000

    fixed bug 728 : FlowOptimizer fails on histories without initial step (outgoing transition)

---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.java.test/src-gen/org/yakindu/scr/historywithoutinitialstep/HistoryWithoutInitialStepStatemachine.java
@@ -0,0 +1,266 @@
+package org.yakindu.scr.historywithoutinitialstep;
+
+public class HistoryWithoutInitialStepStatemachine
+		implements
+			IHistoryWithoutInitialStepStatemachine {
+
+	private final class SCIDefaultImpl implements SCIDefault {
+
+		private boolean e1;
+
+		public void raiseE1() {
+			e1 = true;
+		}
+
+		private boolean e2;
+
+		public void raiseE2() {
+			e2 = true;
+		}
+
+		public void clearEvents() {
+			e1 = false;
+			e2 = false;
+		}
+
+	}
+
+	private SCIDefaultImpl sCIDefault;
+
+	public enum State {
+		main_region_A, main_region_B, main_region_B_r1_C, main_region_B_r1_D, $NullState$
+	};
+
+	private State[] historyVector = new State[1];
+	private final State[] stateVector = new State[1];
+
+	private int nextStateIndex;
+
+	public HistoryWithoutInitialStepStatemachine() {
+
+		sCIDefault = new SCIDefaultImpl();
+
+	}
+
+	public void init() {
+		for (int i = 0; i < 1; i++) {
+			stateVector[i] = State.$NullState$;
+		}
+
+		for (int i = 0; i < 1; i++) {
+			historyVector[i] = State.$NullState$;
+		}
+		clearEvents();
+		clearOutEvents();
+
+	}
+
+	public void enter() {
+		entryAction();
+
+		nextStateIndex = 0;
+		stateVector[0] = State.main_region_A;
+	}
+
+	public void exit() {
+		switch (stateVector[0]) {
+			case main_region_A :
+				nextStateIndex = 0;
+				stateVector[0] = State.$NullState$;
+				break;
+
+			case main_region_B_r1_C :
+				historyVector[0] = stateVector[0];
+
+				nextStateIndex = 0;
+				stateVector[0] = State.$NullState$;
+				break;
+
+			case main_region_B_r1_D :
+				historyVector[0] = stateVector[0];
+
+				nextStateIndex = 0;
+				stateVector[0] = State.$NullState$;
+				break;
+
+			default :
+				break;
+		}
+
+		exitAction();
+	}
+
+	protected void clearEvents() {
+		sCIDefault.clearEvents();
+
+	}
+
+	protected void clearOutEvents() {
+	}
+
+	public boolean isStateActive(State state) {
+		switch (state) {
+			case main_region_A :
+				return stateVector[0] == State.main_region_A;
+			case main_region_B :
+				return stateVector[0].ordinal() >= State.main_region_B
+						.ordinal()
+						&& stateVector[0].ordinal() <= State.main_region_B_r1_D
+								.ordinal();
+			case main_region_B_r1_C :
+				return stateVector[0] == State.main_region_B_r1_C;
+			case main_region_B_r1_D :
+				return stateVector[0] == State.main_region_B_r1_D;
+			default :
+				return false;
+		}
+	}
+
+	public SCIDefault getSCIDefault() {
+		return sCIDefault;
+	}
+
+	public void raiseE1() {
+		sCIDefault.raiseE1();
+	}
+	public void raiseE2() {
+		sCIDefault.raiseE2();
+	}
+
+	/* Entry action for statechart 'HistoryWithoutInitialStep'. */
+	private void entryAction() {
+	}
+
+	/* Exit action for state 'HistoryWithoutInitialStep'. */
+	private void exitAction() {
+	}
+
+	/* shallow enterSequence with history in child r1 */
+	private void shallowEnterSequenceMain_region_B_r1() {
+		switch (historyVector[0]) {
+			case main_region_B_r1_C :
+				nextStateIndex = 0;
+				stateVector[0] = State.main_region_B_r1_C;
+				break;
+
+			case main_region_B_r1_D :
+				nextStateIndex = 0;
+				stateVector[0] = State.main_region_B_r1_D;
+				break;
+
+			default :
+				break;
+		}
+	}
+
+	/* The reactions of state A. */
+	private void reactMain_region_A() {
+		if (sCIDefault.e1) {
+			nextStateIndex = 0;
+			stateVector[0] = State.$NullState$;
+
+			nextStateIndex = 0;
+			stateVector[0] = State.main_region_B_r1_C;
+		} else {
+			if (sCIDefault.e2) {
+				nextStateIndex = 0;
+				stateVector[0] = State.$NullState$;
+
+				/* Enter the region with shallow history */
+				if (historyVector[0] != State.$NullState$) {
+					shallowEnterSequenceMain_region_B_r1();
+				}
+			}
+		}
+	}
+
+	/* The reactions of state C. */
+	private void reactMain_region_B_r1_C() {
+		if (sCIDefault.e1) {
+			historyVector[0] = stateVector[0];
+
+			switch (stateVector[0]) {
+				case main_region_B_r1_C :
+					nextStateIndex = 0;
+					stateVector[0] = State.$NullState$;
+					break;
+
+				case main_region_B_r1_D :
+					nextStateIndex = 0;
+					stateVector[0] = State.$NullState$;
+					break;
+
+				default :
+					break;
+			}
+
+			nextStateIndex = 0;
+			stateVector[0] = State.main_region_A;
+		} else {
+			if (sCIDefault.e2) {
+				nextStateIndex = 0;
+				stateVector[0] = State.$NullState$;
+
+				nextStateIndex = 0;
+				stateVector[0] = State.main_region_B_r1_D;
+			}
+		}
+	}
+
+	/* The reactions of state D. */
+	private void reactMain_region_B_r1_D() {
+		if (sCIDefault.e1) {
+			historyVector[0] = stateVector[0];
+
+			switch (stateVector[0]) {
+				case main_region_B_r1_C :
+					nextStateIndex = 0;
+					stateVector[0] = State.$NullState$;
+					break;
+
+				case main_region_B_r1_D :
+					nextStateIndex = 0;
+					stateVector[0] = State.$NullState$;
+					break;
+
+				default :
+					break;
+			}
+
+			nextStateIndex = 0;
+			stateVector[0] = State.main_region_A;
+		} else {
+			if (sCIDefault.e2) {
+				nextStateIndex = 0;
+				stateVector[0] = State.$NullState$;
+
+				nextStateIndex = 0;
+				stateVector[0] = State.main_region_B_r1_C;
+			}
+		}
+	}
+
+	public void runCycle() {
+
+		clearOutEvents();
+
+		for (nextStateIndex = 0; nextStateIndex < stateVector.length; nextStateIndex++) {
+
+			switch (stateVector[nextStateIndex]) {
+				case main_region_A :
+					reactMain_region_A();
+					break;
+				case main_region_B_r1_C :
+					reactMain_region_B_r1_C();
+					break;
+				case main_region_B_r1_D :
+					reactMain_region_B_r1_D();
+					break;
+				default :
+					// $NullState$
+			}
+		}
+
+		clearEvents();
+	}
+}
