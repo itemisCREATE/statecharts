a9ff189f3c922eee68c56e4d118ec4e1f5834571
malknet42@gmail.com <bohl@itemis.de>
2012-12-03 15:47:14 +0000

    [YAKHMI-700] Completed Test cases for using assignments in expressions in 'AssignmentAsExpression' SCTUnit-Test (SText.xtext - enum AssignmentOperator)

--- test-plugins/org.yakindu.sct.generator.java.test/src-gen/org/yakindu/scr/assignmentasexpression/IAssignmentAsExpressionStatemachine.java
+++ test-plugins/org.yakindu.sct.generator.java.test/src-gen/org/yakindu/scr/assignmentasexpression/IAssignmentAsExpressionStatemachine.java
@@ -8,6 +8,46 @@ public interface IAssignmentAsExpressionStatemachine extends IStatemachine {
 		public void setA(int value);
 		public int getB();
 		public void setB(int value);
+		public int getC();
+		public void setC(int value);
+		public int getD();
+		public void setD(int value);
+		public int getE();
+		public void setE(int value);
+		public int getF();
+		public void setF(int value);
+		public int getG();
+		public void setG(int value);
+		public int getH();
+		public void setH(int value);
+		public int getI();
+		public void setI(int value);
+		public int getJ();
+		public void setJ(int value);
+		public int getJ1();
+		public void setJ1(int value);
+		public int getK();
+		public void setK(int value);
+		public int getK1();
+		public void setK1(int value);
+		public int getL();
+		public void setL(int value);
+		public int getM();
+		public void setM(int value);
+		public int getN();
+		public void setN(int value);
+		public int getP();
+		public void setP(int value);
+		public int getR();
+		public void setR(int value);
+		public int getT();
+		public void setT(int value);
+		public int getU();
+		public void setU(int value);
+		public int getV();
+		public void setV(int value);
+		public int getW();
+		public void setW(int value);
 
 	}
 
