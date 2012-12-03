760669a77a70682c0e404c8865544fd1d5d229e9
Andreas Mülder <A.Muelder@googlemail.com>
2012-12-03 16:19:02 +0000

    Added Missing AssignmentAsExpressionTest

--- test-plugins/org.yakindu.sct.generator.java.test/src-gen/org/yakindu/sct/generator/java/test/AssignmentAsExpressionTest.java
+++ test-plugins/org.yakindu.sct.generator.java.test/src-gen/org/yakindu/sct/generator/java/test/AssignmentAsExpressionTest.java
@@ -38,8 +38,34 @@ public class AssignmentAsExpressionTest {
 	@Test
 	public void testsimpleAssignment() {
 		statemachine.enter();
-		assertTrue(statemachine.isStateActive(State.main_region_A));
+		assertTrue(statemachine.isStateActive(State.main_region_Add));
 		assertTrue(statemachine.getB() == 5);
 		assertTrue(statemachine.getA() == 9);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_Subtract));
+		assertTrue(statemachine.getD() == 6);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_Multiply));
+		assertTrue(statemachine.getE() == 15);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_Divide));
+		assertTrue(statemachine.getG() == 1);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_Modulo));
+		assertTrue(statemachine.getI() == 1);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_Shift));
+		assertTrue(statemachine.getJ() == 16);
+		assertTrue(statemachine.getK() == 4);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_boolean_And));
+		assertTrue(statemachine.getL() == 1);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_boolean_Or));
+		assertTrue(statemachine.getP() == 15);
+		statemachine.runCycle();
+		assertTrue(statemachine.isStateActive(State.main_region_boolean_Xor));
+		assertTrue(statemachine.getU() == 12);
+		statemachine.exit();
 	}
 }
