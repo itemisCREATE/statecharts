8a8aa72f64dd4e981d55ba3165f460306a37e8b9
Andreas Mülder <A.Muelder@googlemail.com>
2012-12-20 14:29:20 +0000

    Removed deprecated NamespaceLocalScopeResolver class

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/STextRuntimeModule.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/STextRuntimeModule.java
@@ -1,21 +1,25 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
 package org.yakindu.sct.model.stext;
 
 import org.eclipse.xtext.linking.ILinker;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
-import org.eclipse.xtext.scoping.IScopeProvider;
-import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
 import org.yakindu.base.types.ITypeSystemAccess;
 import org.yakindu.base.types.impl.BaseTypeSystemAccessImpl;
 import org.yakindu.sct.model.sgraph.resource.SCTLinker;
 import org.yakindu.sct.model.stext.conversion.StextValueConverterService;
 import org.yakindu.sct.model.stext.naming.StextNameProvider;
-import org.yakindu.sct.model.stext.scoping.NamespaceLocalScopeResolver;
 import org.yakindu.sct.model.stext.scoping.STextGlobalScopeProvider;
 import org.yakindu.sct.model.stext.validation.ITypeInferrer;
 import org.yakindu.sct.model.stext.validation.TypeInferrer;
-
-import com.google.inject.Binder;
-import com.google.inject.name.Names;
 
 /**
  * Use this class to register components to be used at runtime / without the
@@ -35,10 +39,6 @@ public class STextRuntimeModule extends
 	public Class<? extends ILinker> bindILinker() {
 		return SCTLinker.class;
 	}
-	
-	// public Class<? extends LazyURIEncoder> bindLazyURIEncoder() {
-	// return SCTLazyURIEncoder.class;
-	// }
 
 	@Override
 	public Class<? extends IQualifiedNameProvider> bindIQualifiedNameProvider() {
@@ -56,15 +56,6 @@ public class STextRuntimeModule extends
 
 	public Class<? extends ITypeInferrer> bindITypeInferrer() {
 		return TypeInferrer.class;
-	}
-
-	// contributed by
-	// org.eclipse.xtext.generator.scoping.AbstractScopingFragment
-	public void configureIScopeProviderDelegate(Binder binder) {
-		binder.bind(IScopeProvider.class)
-				.annotatedWith(
-						Names.named(AbstractDeclarativeScopeProvider.NAMED_DELEGATE))
-				.to(NamespaceLocalScopeResolver.class);
 	}
 
 }
