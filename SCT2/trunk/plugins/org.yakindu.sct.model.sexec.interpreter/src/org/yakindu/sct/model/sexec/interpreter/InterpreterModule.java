d977871d2054f5975c6ac743b2f5b21c8c2b72e6
alexander.nyssen@itemis.de <alexander.nyssen@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-02-17 18:33:23 +0000

    Ensure InterpreterModule extends AbstractRuntimeModule so subclasses can safely overwrite bindings.

--- SCT2/trunk/plugins/org.yakindu.sct.model.sexec.interpreter/src/org/yakindu/sct/model/sexec/interpreter/InterpreterModule.java
+++ SCT2/trunk/plugins/org.yakindu.sct.model.sexec.interpreter/src/org/yakindu/sct/model/sexec/interpreter/InterpreterModule.java
@@ -10,40 +10,73 @@
  */
 package org.yakindu.sct.model.sexec.interpreter;
 
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.service.AbstractGenericModule;
 import org.yakindu.base.types.ITypeSystem;
 import org.yakindu.sct.model.sexec.interpreter.impl.ExecutionFlowInterpreter;
 import org.yakindu.sct.model.sexec.interpreter.impl.TimerTaskTimingService;
 import org.yakindu.sct.model.sexec.interpreter.stext.StextStatementInterpreter;
 import org.yakindu.sct.model.sexec.transformation.SequencerModule;
+import org.yakindu.sct.model.stext.naming.StextNameProvider;
+import org.yakindu.sct.model.stext.types.ISTextTypeInferrer;
+import org.yakindu.sct.model.stext.types.ISTextTypeSystem;
 import org.yakindu.sct.model.stext.types.STextDefaulTypeSystem;
+import org.yakindu.sct.model.stext.types.STextDefaultTypeInferrer;
 import org.yakindu.sct.simulation.core.runtime.IExecutionContext;
 import org.yakindu.sct.simulation.core.runtime.impl.ExecutionContextImpl;
 
 import com.google.inject.Binder;
-import com.google.inject.Module;
 import com.google.inject.name.Names;
 
 /**
  * 
  * @author andreas muelder - Initial contribution and API
+ * @author Alexander Nyßen (alexander.nyssen@itemis.de) - Refactored to
+ *         AbstractGenericModule to allow subclasses to overwrite bindings
  * 
  */
-public class InterpreterModule implements Module {
+public class InterpreterModule extends AbstractGenericModule {
 
 	public static final String INTERPRETER_NAME = "InterpreterName";
 
 	public void configure(Binder binder) {
+		super.configure(binder);
 		binder.bind(Boolean.class)
 				.annotatedWith(Names.named(SequencerModule.ADD_TRACES))
 				.toInstance(Boolean.TRUE);
 		binder.bind(String.class).annotatedWith(Names.named(INTERPRETER_NAME))
 				.toInstance("SExecution Interpreter");
-		binder.bind(IStatementInterpreter.class).to(
-				StextStatementInterpreter.class);
-		binder.bind(IExecutionContext.class).to(ExecutionContextImpl.class);
-		binder.bind(ITimingService.class).to(TimerTaskTimingService.class);
-		binder.bind(IExecutionFlowInterpreter.class).to(
-				ExecutionFlowInterpreter.class);
-		binder.bind(ITypeSystem.class).to(STextDefaulTypeSystem.class);
+	}
+	
+	public Class<? extends ITimingService> bindITimingService(){
+		return TimerTaskTimingService.class;
+	}
+	
+	public Class<? extends IExecutionContext> bindIExecutionContext(){
+		return ExecutionContextImpl.class;
+	}
+	
+	public Class<? extends IStatementInterpreter> bindIStatementInterpreter(){
+		return StextStatementInterpreter.class;
+	}
+	
+	public Class<? extends IExecutionFlowInterpreter> bindIExecutionFlowInterpreter(){
+		return ExecutionFlowInterpreter.class;
+	}
+
+	public Class<? extends IQualifiedNameProvider> bindIQualifiedNameProvider() {
+		return StextNameProvider.class;
+	}
+
+	public Class<? extends ITypeSystem> bindITypeSystem() {
+		return STextDefaulTypeSystem.class;
+	}
+
+	public Class<? extends ISTextTypeSystem> bindISTextTypeSystem() {
+		return STextDefaulTypeSystem.class;
+	}
+	
+	public Class<? extends ISTextTypeInferrer> bindISTextTypeInferrer(){
+		return STextDefaultTypeInferrer.class;
 	}
 }
