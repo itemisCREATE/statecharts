1c62a55e51353dc2bb14a0696d2c9a2325f68c1d
alexander.nyssen@itemis.de <alexander.nyssen@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-02-17 19:40:33 +0000

    Ensure SequencerModule extends AbstractRuntimeModule so subclasses can safely overwrite bindings.

--- SCT2/trunk/plugins/org.yakindu.sct.model.sexec/src/org/yakindu/sct/model/sexec/transformation/SequencerModule.java
+++ SCT2/trunk/plugins/org.yakindu.sct.model.sexec/src/org/yakindu/sct/model/sexec/transformation/SequencerModule.java
@@ -1,29 +1,47 @@
 package org.yakindu.sct.model.sexec.transformation;
 
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.service.AbstractGenericModule;
+import org.yakindu.base.types.ITypeSystem;
 import org.yakindu.sct.model.sgraph.naming.SGraphNameProvider;
+import org.yakindu.sct.model.stext.types.ISTextTypeInferrer;
 import org.yakindu.sct.model.stext.types.ISTextTypeSystem;
 import org.yakindu.sct.model.stext.types.STextDefaulTypeSystem;
+import org.yakindu.sct.model.stext.types.STextDefaultTypeInferrer;
 
 import com.google.inject.Binder;
-import com.google.inject.Module;
 import com.google.inject.name.Names;
 
 /**
  * 
  * @author andreas muelder - Initial contribution and API
- * 
+ * @author Alexander Nyßen (alexander.nyssen@itemis.de) - Refactored to
+ *         AbstractGenericModule to allow subclasses to overwrite bindings
  */
-public class SequencerModule implements Module {
+public class SequencerModule extends AbstractGenericModule {
 
 	public static final String ADD_TRACES = "ADD_TRACES";
 
 	public void configure(Binder binder) {
-		binder.bind(IQualifiedNameProvider.class).to(SGraphNameProvider.class);
-		binder.bind(ISTextTypeSystem.class).to(STextDefaulTypeSystem.class);
+		super.configure(binder);
 		binder.bind(Boolean.class).annotatedWith(Names.named(ADD_TRACES))
 				.toInstance(Boolean.FALSE);
+	}
 
+	public Class<? extends IQualifiedNameProvider> bindIQualifiedNameProvider() {
+		return SGraphNameProvider.class;
+	}
+
+	public Class<? extends ITypeSystem> bindITypeSystem() {
+		return STextDefaulTypeSystem.class;
+	}
+
+	public Class<? extends ISTextTypeSystem> bindISTextTypeSystem() {
+		return STextDefaulTypeSystem.class;
+	}
+
+	public Class<? extends ISTextTypeInferrer> bindISTextTypeInferrer() {
+		return STextDefaultTypeInferrer.class;
 	}
 
 }
