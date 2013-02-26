6add04f0493cdcecf6b4bc5e95f3e53d24d444cc
Andreas Mülder <A.Muelder@googlemail.com>
2013-02-26 12:46:30 +0000

    resizing of compartments must not be smaller than its preferred size

---
+++ plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/policies/PreferredSizeCompartmentEditPolicy.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2013 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.ui.editor.policies;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.requests.ChangeBoundsRequest;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableEditPolicyEx;
+
+/**
+ * 
+ * Do not allow resizing smaller than the preferred size of the compartment
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public class PreferredSizeCompartmentEditPolicy extends ResizableEditPolicyEx {
+
+	@Override
+	public void showSourceFeedback(Request request) {
+		if (request instanceof ChangeBoundsRequest) {
+			adjustRequest((ChangeBoundsRequest) request);
+		}
+		super.showSourceFeedback(request);
+	}
+
+	@Override
+	protected Command getResizeCommand(ChangeBoundsRequest request) {
+		if (request instanceof ChangeBoundsRequest) {
+			adjustRequest((ChangeBoundsRequest) request);
+		}
+		return super.getResizeCommand(request);
+	}
+
+	protected void adjustRequest(ChangeBoundsRequest request) {
+		final IFigure figure = getHostFigure();
+		final Dimension prefSize = figure.getPreferredSize();
+		final Rectangle newBounds = request.getTransformedRectangle(figure.getBounds().getCopy());
+		final Dimension newSizeDelta = request.getSizeDelta().getCopy();
+		if (newBounds.width < prefSize.width) {
+			newSizeDelta.width = request.getSizeDelta().width + (prefSize.width - newBounds.width);
+		}
+		if (newBounds.height < prefSize.height) {
+			newSizeDelta.height = request.getSizeDelta().height + (prefSize.height - newBounds.height);
+		}
+		request.setSizeDelta(newSizeDelta);
+	}
+
+}
