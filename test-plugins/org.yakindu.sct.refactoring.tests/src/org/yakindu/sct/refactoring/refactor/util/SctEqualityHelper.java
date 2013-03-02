d2e2e55f2930a798b44dd2995e2e33c60ab9217f
tomqc86@googlemail.com <tomqc86@googlemail.com>
2013-03-02 14:21:34 +0000

    Suppress warning.

--- test-plugins/org.yakindu.sct.refactoring.tests/src/org/yakindu/sct/refactoring/refactor/util/SctEqualityHelper.java
+++ test-plugins/org.yakindu.sct.refactoring.tests/src/org/yakindu/sct/refactoring/refactor/util/SctEqualityHelper.java
@@ -14,6 +14,7 @@ import org.eclipse.emf.ecore.util.FeatureMapUtil;
  * @author thomas kutz - Initial contribution and API
  *
  */
+@SuppressWarnings("serial")
 public class SctEqualityHelper extends EqualityHelper{
 
 	@Override
