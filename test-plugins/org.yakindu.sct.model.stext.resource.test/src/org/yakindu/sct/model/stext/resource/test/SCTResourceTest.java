f5f4b74cef240b10614fbbcc10e24124a3bcf7d9
Andreas Mülder <A.Muelder@googlemail.com>
2013-03-04 09:35:24 +0000

    Fixed testStatechartSerializing1

--- test-plugins/org.yakindu.sct.model.stext.resource.test/src/org/yakindu/sct/model/stext/resource/test/SCTResourceTest.java
+++ test-plugins/org.yakindu.sct.model.stext.resource.test/src/org/yakindu/sct/model/stext/resource/test/SCTResourceTest.java
@@ -141,7 +141,7 @@ public class SCTResourceTest {
 		Scope internalScope = (InternalScope) statechart.getScopes().get(0);
 		Event event = internalScope.getEvents().get(0);
 		event.setName("Event2");
-		assertEquals("internal: event Event2", statechart.getSpecification());
+		assertEquals("internal: event Event2\n", statechart.getSpecification());
 		assertEquals("" + res.getSyntaxDiagnostics(), 0, res
 				.getSyntaxDiagnostics().size());
 	}
