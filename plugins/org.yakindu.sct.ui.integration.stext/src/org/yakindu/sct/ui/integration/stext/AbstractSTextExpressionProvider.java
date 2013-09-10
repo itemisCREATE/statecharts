1503d47af807bcf01ff2865e0eb334d129a49f88
Andreas Mülder <A.Muelder@googlemail.com>
2013-09-10 07:49:28 +0000

    Refactored Simulation core

--- plugins/org.yakindu.sct.ui.integration.stext/src/org/yakindu/sct/ui/integration/stext/AbstractSTextExpressionProvider.java
+++ plugins/org.yakindu.sct.ui.integration.stext/src/org/yakindu/sct/ui/integration/stext/AbstractSTextExpressionProvider.java
@@ -3,7 +3,6 @@ package org.yakindu.sct.ui.integration.stext;
 import org.eclipse.emf.ecore.EObject;
 import org.yakindu.sct.model.stext.STextRuntimeModule;
 import org.yakindu.sct.model.stext.ui.STextUiModule;
-import org.yakindu.sct.ui.editor.extensions.AbstractExpressionsProvider;
 import org.yakindu.sct.ui.editor.extensions.IExpressionLanguageProvider;
 import org.yakindu.sct.ui.integration.stext.modules.EntryRuleRuntimeModule;
 import org.yakindu.sct.ui.integration.stext.modules.EntryRuleUIModule;
