2e293c02996eb53927cacbe87e9bc04c726b823c
Menznerowski@gmail.com <Menznerowski@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-11-08 15:18:29 +0000

    remove unnecessary operation attribute

--- SCT2/trunk/plugins/org.yakindu.sct.simulation.ui.sexec/src/org/yakindu/sct/simulation/ui/sexec/launch/StatechartLaunchShortcut.java
+++ SCT2/trunk/plugins/org.yakindu.sct.simulation.ui.sexec/src/org/yakindu/sct/simulation/ui/sexec/launch/StatechartLaunchShortcut.java
@@ -75,7 +75,6 @@ public class StatechartLaunchShortcut implements ILaunchShortcut, ISCTLaunchConf
 					launchManager.generateLaunchConfigurationName(file.getName()));
 
 			newConfig.setAttribute(FILE_NAME, file.getFullPath().toString());
-			newConfig.setAttribute(OPERATION_CLASS, "org.yakindu.Operations");
 			return newConfig.doSave();
 
 		} catch (CoreException e) {
