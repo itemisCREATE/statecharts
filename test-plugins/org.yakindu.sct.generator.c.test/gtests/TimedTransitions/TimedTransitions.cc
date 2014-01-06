f2fd3dcc80374b9217d5220fe2bace52f6130864
malknet42 <bohl@itemis.de>
2014-01-06 15:10:37 +0000

    changed Name of TimeTrigger Tests, added DeepEntry and EntryChoice Test, updated AllTests

---
+++ test-plugins/org.yakindu.sct.generator.c.test/gtests/TimedTransitions/TimedTransitions.cc
@@ -0,0 +1,36 @@
+/**
+* Copyright (c) 2014 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "TimedTransitions.h"
+#include "TimedTransitionsRequired.h"
+
+
+TimedTransitions handle;
+
+TEST(StatemachineTest, Timer01) {
+	timedTransitions_init(&handle);
+	timedTransitions_enter(&handle);
+	EXPECT_TRUE(timedTransitions_isActive(&handle, TimedTransitions_main_region_Start));
+	// here should be waited 2030 Seconds
+	timedTransitions_runCycle(&handle);
+	EXPECT_TRUE(timedTransitions_isActive(&handle, TimedTransitions_main_region_End));
+}
+
+		
+void timedTransitions_setTimer(const sc_eventid evid, const sc_integer time_ms, const sc_boolean periodic){
+	// Mockup
+	timedTransitions_raiseTimeEvent(&handle, evid);
+}
+
+void timedTransitions_unsetTimer(const sc_eventid evid){
+	// Mockup
+}		
