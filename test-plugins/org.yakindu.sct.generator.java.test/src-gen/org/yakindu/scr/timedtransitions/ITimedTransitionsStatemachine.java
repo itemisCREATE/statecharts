64792b8fab032fda4e77cf97ad516295fd97812d
malknet42 <bohl@itemis.de>
2014-01-06 13:52:36 +0000

    changed Name of TimeTrigger Tests, added DeepEntry and EntryChoice Test, updated AllTests

---
+++ test-plugins/org.yakindu.sct.generator.java.test/src-gen/org/yakindu/scr/timedtransitions/ITimedTransitionsStatemachine.java
@@ -0,0 +1,10 @@
+package org.yakindu.scr.timedtransitions;
+import org.yakindu.scr.IStatemachine;
+import org.yakindu.scr.ITimerCallback;
+
+public interface ITimedTransitionsStatemachine
+		extends
+			ITimerCallback,
+			IStatemachine {
+
+}
