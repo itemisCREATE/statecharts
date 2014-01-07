f8a2b6ef1988377557c34f1b087bbdd97242fcf1
malknet42 <malknet42@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-01-07 12:18:02 +0000

    YAKHMI-1024, STatechart and Test for static/dynamic Choice Test

---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.cpp.test/gtests/EntryChoiceTest/EntryChoiceTest.cc
@@ -0,0 +1,28 @@
+/**
+* Copyright (c) 2014 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "EntryChoice.h"
+			
+
+EntryChoice* statechart;
+
+	
+TEST(StatemachineTest, EntryChoiceTest) {
+	statechart = new EntryChoice();				
+	statechart->init();
+	statechart->enter();
+	statechart->runCycle();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(EntryChoice::EntryChoice_main_region_A));
+	delete statechart;
+}
+
