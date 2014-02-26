e5e644b5229f3e02e074c79ed0ca64e596af458f
tomqc86@googlemail.com <tomqc86@googlemail.com>
2014-02-26 13:33:26 +0000

    Fixed global scope filter to only filter variables that are defined in other statecharts (but allowing references to variables defined in the resource kinds).

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/scoping/STextGlobalScopeProvider.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/scoping/STextGlobalScopeProvider.java
@@ -64,10 +64,14 @@ public class STextGlobalScopeProvider extends DefaultGlobalScopeProvider {
 				ContextElementAdapter.class);
 		if (provider != null) {
 			parentScope = new FilteringScope(parentScope, new Predicate<IEObjectDescription>() {
+				
 				public boolean apply(IEObjectDescription input) {
+					// only accept events, regular states and variables from own statechart
 					if (input.getEClass() == StextPackage.Literals.EVENT_DEFINITION
 							|| SGraphPackage.Literals.REGULAR_STATE.isSuperTypeOf(input.getEClass())
-							|| input.getEClass() == StextPackage.Literals.VARIABLE_DEFINITION) {
+							|| (input.getEClass() == StextPackage.Literals.VARIABLE_DEFINITION && 
+								input.getEObjectURI().fileExtension().equals("sct"))) {
+						
 						URI sourceURI = input.getEObjectURI().trimFragment();
 						return sourceURI.equals(provider.getElement().eResource().getURI());
 					}
