37c32ecb81ba8632e9b03d62809cb899eef57026
A.Muelder@googlemail.com <A.Muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-05-05 10:06:28 +0000

    made BuiltinEventSpec abstract

--- SCT2/trunk/plugins/org.yakindu.sct.model.stext.edit/src/org/yakindu/sct/model/stext/stext/provider/RegularEventSpecItemProvider.java
+++ SCT2/trunk/plugins/org.yakindu.sct.model.stext.edit/src/org/yakindu/sct/model/stext/stext/provider/RegularEventSpecItemProvider.java
@@ -41,222 +41,219 @@ public class RegularEventSpecItemProvider
     IItemPropertySource
 {
   /**
-   * This constructs an instance from a factory and a notifier.
-   * <!-- begin-user-doc -->
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   public RegularEventSpecItemProvider(AdapterFactory adapterFactory)
   {
-    super(adapterFactory);
-  }
+		super(adapterFactory);
+	}
 
   /**
-   * This returns the property descriptors for the adapted class.
-   * <!-- begin-user-doc -->
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   @Override
   public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object)
   {
-    if (itemPropertyDescriptors == null)
-    {
-      super.getPropertyDescriptors(object);
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
 
-    }
-    return itemPropertyDescriptors;
-  }
+		}
+		return itemPropertyDescriptors;
+	}
 
   /**
-   * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-   * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-   * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-   * <!-- begin-user-doc -->
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   @Override
   public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object)
   {
-    if (childrenFeatures == null)
-    {
-      super.getChildrenFeatures(object);
-      childrenFeatures.add(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT);
-    }
-    return childrenFeatures;
-  }
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT);
+		}
+		return childrenFeatures;
+	}
 
   /**
-   * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   @Override
   protected EStructuralFeature getChildFeature(Object object, Object child)
   {
-    // Check the type of the specified child object and return the proper feature to use for
-    // adding (see {@link AddCommand}) it as a child.
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
 
-    return super.getChildFeature(object, child);
-  }
+		return super.getChildFeature(object, child);
+	}
 
   /**
-   * This returns RegularEventSpec.gif.
-   * <!-- begin-user-doc -->
+	 * This returns RegularEventSpec.gif.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   @Override
   public Object getImage(Object object)
   {
-    return overlayImage(object, getResourceLocator().getImage("full/obj16/RegularEventSpec"));
-  }
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/RegularEventSpec"));
+	}
 
   /**
-   * This returns the label text for the adapted class.
-   * <!-- begin-user-doc -->
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   @Override
   public String getText(Object object)
   {
-    return getString("_UI_RegularEventSpec_type");
-  }
+		return getString("_UI_RegularEventSpec_type");
+	}
 
   /**
-   * This handles model notifications by calling {@link #updateChildren} to update any cached
-   * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-   * <!-- begin-user-doc -->
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   @Override
   public void notifyChanged(Notification notification)
   {
-    updateChildren(notification);
+		updateChildren(notification);
 
-    switch (notification.getFeatureID(RegularEventSpec.class))
-    {
-      case StextPackage.REGULAR_EVENT_SPEC__EVENT:
-        fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-        return;
-    }
-    super.notifyChanged(notification);
-  }
+		switch (notification.getFeatureID(RegularEventSpec.class)) {
+			case StextPackage.REGULAR_EVENT_SPEC__EVENT:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
 
   /**
-   * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-   * that can be created under this object.
-   * <!-- begin-user-doc -->
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @generated
-   */
+	 * @generated
+	 */
   @Override
   protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object)
   {
-    super.collectNewChildDescriptors(newChildDescriptors, object);
+		super.collectNewChildDescriptors(newChildDescriptors, object);
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         StextFactory.eINSTANCE.createEventRaisingExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 StextFactory.eINSTANCE.createEventRaisingExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         StextFactory.eINSTANCE.createEventValueReferenceExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 StextFactory.eINSTANCE.createEventValueReferenceExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         StextFactory.eINSTANCE.createActiveStateReferenceExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 StextFactory.eINSTANCE.createActiveStateReferenceExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createAssignmentExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createAssignmentExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createConditionalExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createConditionalExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createLogicalOrExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createLogicalOrExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createLogicalAndExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createLogicalAndExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createLogicalNotExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createLogicalNotExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createBitwiseXorExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createBitwiseXorExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createBitwiseOrExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createBitwiseOrExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createBitwiseAndExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createBitwiseAndExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createLogicalRelationExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createLogicalRelationExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createShiftExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createShiftExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createNumericalAddSubtractExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createNumericalAddSubtractExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createNumericalMultiplyDivideExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createNumericalMultiplyDivideExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createNumericalUnaryExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createNumericalUnaryExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createPrimitiveValueExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createPrimitiveValueExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createFeatureCall()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createFeatureCall()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createElementReferenceExpression()));
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createElementReferenceExpression()));
 
-    newChildDescriptors.add
-      (createChildParameter
-        (StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
-         ExpressionsFactory.eINSTANCE.createParenthesizedExpression()));
-  }
+		newChildDescriptors.add
+			(createChildParameter
+				(StextPackage.Literals.REGULAR_EVENT_SPEC__EVENT,
+				 ExpressionsFactory.eINSTANCE.createParenthesizedExpression()));
+	}
 
 }
