128f632b6d029c00b7172e69501db9a451789bd7
Flow.Antony@googlemail.com <Flow.Antony@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-07-15 15:58:47 +0000

    add syntax highlighting for deprecated FeatureType and FeatureParameter

--- SCT2/trunk/plugins/org.yakindu.sct.generator.genmodel.ui/src/org/yakindu/sct/generator/genmodel/ui/highlighting/SGenSemanticHighlightingCalculator.java
+++ SCT2/trunk/plugins/org.yakindu.sct.generator.genmodel.ui/src/org/yakindu/sct/generator/genmodel/ui/highlighting/SGenSemanticHighlightingCalculator.java
@@ -11,8 +11,10 @@ import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.yakindu.sct.model.sgen.DeprecatableElement;
 import org.yakindu.sct.model.sgen.GeneratorEntry;
 import org.yakindu.sct.model.sgen.SGenPackage;
+import org.yakindu.sct.model.sgen.impl.FeatureConfigurationImpl;
 
 public class SGenSemanticHighlightingCalculator implements
 		ISemanticHighlightingCalculator {
@@ -21,14 +23,16 @@ public class SGenSemanticHighlightingCalculator implements
 			IHighlightedPositionAcceptor acceptor) {
 		if (resource == null || resource.getParseResult() == null)
 			return;
-
 		TreeIterator<EObject> allContents = resource.getAllContents();
 		while (allContents.hasNext()) {
 			final EObject object = allContents.next();
 			if (object instanceof GeneratorEntry) {
 				GeneratorEntry entry = (GeneratorEntry) object;
+
 				List<INode> nodes = NodeModelUtils.findNodesForFeature(entry,
 						SGenPackage.Literals.GENERATOR_ENTRY__CONTENT_TYPE);
+				List<INode> features = NodeModelUtils.findNodesForFeature(
+						entry, SGenPackage.Literals.GENERATOR_ENTRY__FEATURES);
 				for (INode node : nodes) {
 					if (node instanceof LeafNode
 							&& !((LeafNode) node).isHidden()) {
@@ -36,6 +40,25 @@ public class SGenSemanticHighlightingCalculator implements
 								node.getTotalLength(),
 								DefaultHighlightingConfiguration.KEYWORD_ID);
 					}
+				}
+				for (INode node : features) {
+					if (node.getSemanticElement() instanceof FeatureConfigurationImpl) {
+						FeatureConfigurationImpl feature = (FeatureConfigurationImpl) node
+								.getSemanticElement();
+						if (feature.getType() instanceof DeprecatableElement) {
+
+							DeprecatableElement deprecatableElement = feature
+									.getType();
+							if (deprecatableElement.isDeprecated()) {
+								acceptor.addPosition(
+										node.getTotalOffset(),
+										node.getTotalLength(),
+										SGenHighlightingConfiguration.DEPRECATION);
+							}
+						}
+
+					}
+
 				}
 				allContents.prune();
 			}
