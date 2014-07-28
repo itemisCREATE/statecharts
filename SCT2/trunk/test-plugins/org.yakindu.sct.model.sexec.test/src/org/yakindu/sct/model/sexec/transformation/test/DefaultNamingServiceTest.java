9a1510b678b9ffd1d6473656c89a1aa8b78db46e
markus.muehlbrandt@gmail.com <markus.muehlbrandt@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-07-28 12:56:14 +0000

    Added test for DefaultNamingService implementation.

---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.model.sexec.test/src/org/yakindu/sct/model/sexec/transformation/test/DefaultNamingServiceTest.java
@@ -0,0 +1,61 @@
+package org.yakindu.sct.model.sexec.transformation.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+import org.yakindu.sct.model.sexec.ExecutionFlow;
+import org.yakindu.sct.model.sexec.ExecutionState;
+import org.yakindu.sct.model.sexec.naming.INamingService;
+import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.test.models.SCTUnitTestModels;
+
+import com.google.inject.Inject;
+
+public class DefaultNamingServiceTest extends ModelSequencerTest {
+
+	@Inject
+	protected SCTUnitTestModels testModels;
+
+	@Inject
+	protected INamingService statechartNamingService;
+
+	@Inject
+	protected INamingService flowNamingService;
+
+	@Test
+	public void testDefaultNamingServiceState_NameMatch() {
+
+		List<Statechart> statecharts = Collections.emptyList();
+
+		try {
+			statecharts = testModels.loadAllStatecharts();
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+
+		for (Statechart statechart : statecharts) {
+			// Transform statechart
+			ExecutionFlow flow = sequencer.transform(statechart);
+			if (statecharts.indexOf(statechart) == 4) {
+				System.out.println();
+			}
+			// Initialize naming services for statechart and ExecutionFlow
+			statechartNamingService.initializeNamingService(statechart);
+			flowNamingService.initializeNamingService(flow);
+
+			for (ExecutionState state : flow.getStates()) {
+				if (state.getSourceElement() instanceof State) {
+					assertEquals(flowNamingService.getShortName(state),
+							statechartNamingService
+									.getShortName((State) state
+											.getSourceElement()));
+				}
+			}
+		}
+	}
+}
