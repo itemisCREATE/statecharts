af435e799eb7f67aa82623a0593279d6930546d7
malknet42 <malknet42@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-08-07 12:02:38 +0000

    Added Operations-test and extended CustomTest

---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.java.test/src/org/yakindu/sct/generator/java/AllTestsTestCustom.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2014 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.generator.java;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({OperationsTestCustom.class})
+public class AllTestsTestCustom {
+}
