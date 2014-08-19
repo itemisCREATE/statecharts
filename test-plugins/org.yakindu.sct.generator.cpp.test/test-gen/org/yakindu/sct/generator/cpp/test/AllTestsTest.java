666e21d11b59fad239a5d155aefc1bc54800efca
malknet42 <bohl@itemis.de>
2014-08-19 13:19:44 +0000

    removed ZBridgeServerSync statechart and test from repo

--- test-plugins/org.yakindu.sct.generator.cpp.test/test-gen/org/yakindu/sct/generator/cpp/test/AllTestsTest.java
+++ test-plugins/org.yakindu.sct.generator.cpp.test/test-gen/org/yakindu/sct/generator/cpp/test/AllTestsTest.java
@@ -34,7 +34,6 @@ import org.junit.runners.Suite.SuiteClasses;
 		SimpleHierachyTest.class, StateIsActiveTest.class,
 		StaticChoiceTest.class, STextKeywordsInStatesAndRegionsTest.class,
 		StringExpressionsTest.class, SyncForkTest.class, SyncJoinTest.class,
-		TransitionWithoutConditionTest.class, ValuedEventsTest.class,
-		ZBridgeServerSyncTest.class})
+		TransitionWithoutConditionTest.class, ValuedEventsTest.class})
 public class AllTestsTest {
 }
