ffd2e5640ed57c1715656e17801a82747709457b
malknet42 <malknet42@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-08-20 11:23:16 +0000

    added CastExpressionTest

---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.java.test/test-gen/org/yakindu/sct/generator/java/test/CastExpressionsTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2014 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+
+package org.yakindu.sct.generator.java.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.yakindu.scr.castexpressions.CastExpressionsStatemachine;
+import org.yakindu.scr.castexpressions.CastExpressionsStatemachine.State;
+/**
+ *  Unit TestCase for CastExpressions
+ */
+@SuppressWarnings("all")
+public class CastExpressionsTest {
+
+	private CastExpressionsStatemachine statemachine;
+
+	@Before
+	public void setUp() {
+		statemachine = new CastExpressionsStatemachine();
+		statemachine.init();
+	}
+
+	@After
+	public void tearDown() {
+		statemachine = null;
+	}
+
+	@Test
+	public void testCastExpressionTest() {
+		statemachine.enter();
+		assertTrue(statemachine.getRealValue() == 5);
+		assertTrue(statemachine.getIntValue() == 5);
+		statemachine.runCycle();
+		assertTrue(statemachine.getRealValue() == 15);
+	}
+}
---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.simulation.core.sexec.test/test-gen/org/yakindu/sct/simulation/core/sexec/test/CastExpressionsTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2014 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.simulation.core.sexec.test;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yakindu.sct.model.sexec.ExecutionFlow;
+import org.yakindu.sct.model.sexec.interpreter.test.util.AbstractExecutionFlowTest;
+import org.yakindu.sct.model.sexec.interpreter.test.util.SExecInjectionProvider;
+import org.yakindu.sct.test.models.SCTUnitTestModels;
+import com.google.inject.Inject;
+import static org.junit.Assert.assertTrue;
+/**
+ *  Unit TestCase for CastExpressions
+ */
+@SuppressWarnings("all")
+@RunWith(XtextRunner.class)
+@InjectWith(SExecInjectionProvider.class)
+public class CastExpressionsTest extends AbstractExecutionFlowTest {
+	@Before
+	public void setup() throws Exception {
+		ExecutionFlow flow = models
+				.loadExecutionFlowFromResource("CastExpressions.sct");
+		initInterpreter(flow);
+	}
+	@Test
+	public void CastExpressionTest() throws Exception {
+		interpreter.enter();
+		assertTrue(getReal("realValue") == 5);
+		assertTrue(getInteger("intValue") == 5);
+		interpreter.runCycle();
+		assertTrue(getReal("realValue") == 15);
+	}
+}
