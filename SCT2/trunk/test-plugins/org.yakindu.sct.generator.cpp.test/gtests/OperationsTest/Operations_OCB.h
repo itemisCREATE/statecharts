9b1168fe4842b4908c334553b7d2b1b9ee7ecb41
malknet42 <malknet42@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-10-23 10:52:18 +0000

    customized customTest for Operations (because of statechart test enrichment)

--- SCT2/trunk/test-plugins/org.yakindu.sct.generator.cpp.test/gtests/OperationsTest/Operations_OCB.h
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.cpp.test/gtests/OperationsTest/Operations_OCB.h
@@ -7,6 +7,19 @@ class InternalCallBack : public Operations::InternalSCI_OCB {
 		void internalOperation1();
 
 		sc_boolean InternalOperation2(sc_integer param1);
+
+		sc_real internalOperation3();
+
+		sc_real internalOperation3a(sc_real param1);
+
+		sc_integer internalOperation4();
+
+		sc_integer internalOperation4a(sc_integer param1);
+
+		sc_string internalOperation5();
+
+		sc_string internalOperation5a(sc_string param1);
+
 };
 
 class Interface1CallBack : public Operations::SCI_Interface1_OCB {
@@ -15,6 +28,19 @@ class Interface1CallBack : public Operations::SCI_Interface1_OCB {
 		void interfaceOperation1();
 
 		sc_boolean InterfaceOperation2(sc_integer param1);
+
+		sc_real interfaceOperation3();
+
+		sc_real interfaceOperation3a(sc_real param1);
+
+		sc_integer interfaceOperation4();
+
+		sc_integer interfaceOperation4a(sc_integer param1);
+
+		sc_string interfaceOperation5();
+
+		sc_string interfaceOperation5a(sc_string param1);
+
 };
 
 class DefaultCallBack : public Operations::DefaultSCI_OCB {
@@ -25,6 +51,20 @@ class DefaultCallBack : public Operations::DefaultSCI_OCB {
 
 		sc_boolean UnnamedInterfaceOperation2(sc_integer param1);
 		
+		sc_boolean unnamedOperation2(sc_integer param1);
+
+		sc_real unnamedOperation3();
+
+		sc_real unnamedOperation3a(sc_real param1);
+
+		sc_integer unnamedOperation4();
+
+		sc_integer unnamedOperation4a(sc_integer param1);
+
+		sc_string unnamedOperation5();
+
+		sc_string unnamedOperation5a(sc_string param1);
+
 		sc_boolean alwaysTrue();
 };
 
