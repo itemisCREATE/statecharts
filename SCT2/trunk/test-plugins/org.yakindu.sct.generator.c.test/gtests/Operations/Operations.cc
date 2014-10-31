285c347b3735f0f9ae2ba343ceeab797a5e9cba8
terfloth@itemis.de <terfloth@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-10-31 13:04:16 +0000

    Added test cases for: Transition into substate does not recognize parents history context (Fixes issue 33)

---
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.c.test/gtests/Operations/Operations.cc
@@ -0,0 +1,23 @@
+/**
+* Copyright (c) 2014 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "Operations.h"
+
+
+Operations handle;
+
+TEST(StatemachineTest, OperationCallback) {
+	operations_init(&handle);
+	operations_enter(&handle);
+}
+
+		
