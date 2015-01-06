653c30349a9d40e83b10a200fc7dee24f444d2f9
A.Muelder@googlemail.com <A.Muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2015-01-06 11:02:29 +0000

    Added Domain Plugin

---
+++ SCT2/trunk/plugins/org.yakindu.sct.domain/src/org/yakindu/sct/domain/DomainActivator.java
@@ -0,0 +1,30 @@
+package org.yakindu.sct.domain;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class DomainActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		DomainActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		DomainActivator.context = null;
+	}
+
+}
---
+++ SCT2/trunk/plugins/org.yakindu.sct.domain/src/org/yakindu/sct/domain/extension/DomainRegistry.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2015 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.domain.extension;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Singleton;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+import org.osgi.framework.Bundle;
+
+/**
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+@Singleton
+public class DomainRegistry {
+
+	private static final String EXTENSION_POINT = "org.yakindu.sct.domain";
+	private static final String DOMAIN_ID = "domainID";
+	private static final String DESCRIPTION = "description";
+	private static final String IMAGE = "image";
+
+	private static List<DomainDescriptor> descriptors = null;
+
+	public static class DomainDescriptor {
+
+		private final IConfigurationElement configElement;
+
+		private Image image;
+
+		DomainDescriptor(IConfigurationElement configElement) {
+			this.configElement = configElement;
+		}
+
+		public String getDomainID() {
+			return configElement.getAttribute(DOMAIN_ID);
+		}
+
+		public String getDescription() {
+			return configElement.getAttribute(DESCRIPTION);
+		}
+
+		public Image getImage() {
+			if (image != null)
+				return image;
+			String path = configElement.getAttribute(IMAGE);
+			if (path == null)
+				return null;
+
+			Bundle extensionBundle = Platform.getBundle(configElement.getContributor().getName());
+			URL entry = extensionBundle.getEntry(path);
+			ImageDescriptor descriptor = ImageDescriptor.createFromURL(entry);
+			image = descriptor.createImage();
+			return image;
+		}
+
+		public IConfigurationElement getConfigElement() {
+			return configElement;
+		}
+
+		public void setImage(Image image) {
+			this.image = image;
+		}
+	}
+
+	public static List<DomainDescriptor> getDomainDescriptors() {
+		IConfigurationElement[] configurationElements = Platform.getExtensionRegistry().getConfigurationElementsFor(
+				EXTENSION_POINT);
+		if (descriptors == null) {
+			descriptors = new ArrayList<DomainDescriptor>();
+			for (IConfigurationElement iConfigurationElement : configurationElements) {
+				descriptors.add(new DomainDescriptor(iConfigurationElement));
+			}
+		}
+		return descriptors;
+	}
+
+}
