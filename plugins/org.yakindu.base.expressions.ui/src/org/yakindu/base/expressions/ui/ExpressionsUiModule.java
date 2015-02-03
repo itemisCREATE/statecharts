951e4508c8d0612dbcecbedb25a0ec60689b5fa5
Andreas Mülder <A.Muelder@googlemail.com>
2015-02-03 09:17:44 +0000

    Check if JST is available

--- plugins/org.yakindu.base.expressions.ui/src/org/yakindu/base/expressions/ui/ExpressionsUiModule.java
+++ plugins/org.yakindu.base.expressions.ui/src/org/yakindu/base/expressions/ui/ExpressionsUiModule.java
@@ -4,6 +4,13 @@
 package org.yakindu.base.expressions.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.model.IResourceForEditorInputFactory;
+import org.eclipse.xtext.ui.editor.model.JavaClassPathResourceForIEditorInputFactory;
+import org.eclipse.xtext.ui.editor.model.ResourceForIEditorInputFactory;
+import org.eclipse.xtext.ui.resource.IResourceSetProvider;
+import org.eclipse.xtext.ui.resource.SimpleResourceSetProvider;
+import org.eclipse.xtext.ui.resource.XtextResourceSetProvider;
+import org.eclipse.xtext.ui.shared.Access;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -11,5 +18,31 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class ExpressionsUiModule extends org.yakindu.base.expressions.ui.AbstractExpressionsUiModule {
 	public ExpressionsUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+
+	public com.google.inject.Provider<org.eclipse.xtext.resource.containers.IAllContainersState> provideIAllContainersState() {
+		if (Access.getJdtHelper().get().isJavaCoreAvailable()) {
+			return Access.getJavaProjectsState();
+		} else {
+			return Access.getWorkspaceProjectsState();
+		}
+	}
+
+	@Override
+	public Class<? extends IResourceSetProvider> bindIResourceSetProvider() {
+		if (Access.getJdtHelper().get().isJavaCoreAvailable()) {
+			return XtextResourceSetProvider.class;
+		} else {
+			return SimpleResourceSetProvider.class;
+		}
+	}
+
+	@Override
+	public Class<? extends IResourceForEditorInputFactory> bindIResourceForEditorInputFactory() {
+		if (Access.getJdtHelper().get().isJavaCoreAvailable()) {
+			return JavaClassPathResourceForIEditorInputFactory.class;
+		} else {
+			return ResourceForIEditorInputFactory.class;
+		}
 	}
 }
