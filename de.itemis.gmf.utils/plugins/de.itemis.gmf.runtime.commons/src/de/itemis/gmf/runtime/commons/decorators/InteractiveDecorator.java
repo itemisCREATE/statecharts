cb2ec0b488d3d7da23017f90ed130bfb7839cf89
Andreas Mülder <A.Muelder@googlemail.com>
2015-02-12 13:05:29 +0000

    added missing EPL header

--- de.itemis.gmf.utils/plugins/de.itemis.gmf.runtime.commons/src/de/itemis/gmf/runtime/commons/decorators/InteractiveDecorator.java
+++ de.itemis.gmf.utils/plugins/de.itemis.gmf.runtime.commons/src/de/itemis/gmf/runtime/commons/decorators/InteractiveDecorator.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
 package de.itemis.gmf.runtime.commons.decorators;
 
 import org.eclipse.draw2d.IFigure;
