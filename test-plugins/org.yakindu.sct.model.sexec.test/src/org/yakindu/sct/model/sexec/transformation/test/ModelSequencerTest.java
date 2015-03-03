1e7c4f0f6bf06009684ef1090c0754466df562cb
bohl@itemis.de <bohl@itemis.de>
2015-03-03 14:30:25 +0000

    added DefaultNamingService to Module

--- test-plugins/org.yakindu.sct.model.sexec.test/src/org/yakindu/sct/model/sexec/transformation/test/ModelSequencerTest.java
+++ test-plugins/org.yakindu.sct.model.sexec.test/src/org/yakindu/sct/model/sexec/transformation/test/ModelSequencerTest.java
@@ -2,13 +2,18 @@ package org.yakindu.sct.model.sexec.transformation.test;
 
 import org.junit.Before;
 import org.yakindu.sct.domain.default_.modules.DefaultSequencerModule;
+import org.yakindu.sct.model.sexec.naming.DefaultNamingService;
+import org.yakindu.sct.model.sexec.naming.INamingService;
 import org.yakindu.sct.model.sexec.transformation.BehaviorMapping;
 import org.yakindu.sct.model.sexec.transformation.IModelSequencer;
 import org.yakindu.sct.model.sexec.transformation.StructureMapping;
 
+import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.util.Modules;
 
 /**
  * 
@@ -28,7 +33,13 @@ public class ModelSequencerTest extends Assert {
 
 	@Before
 	public void setup() {
-		Injector injector = Guice.createInjector(new DefaultSequencerModule());
+		Module m = Modules.override(new DefaultSequencerModule()).with(new Module(){
+			@Override
+			public void configure(Binder binder) {
+				binder.bind(INamingService.class).to(DefaultNamingService.class);
+			}
+		});
+		Injector injector = Guice.createInjector(m);
 		injector.injectMembers(this);
 	}
 
