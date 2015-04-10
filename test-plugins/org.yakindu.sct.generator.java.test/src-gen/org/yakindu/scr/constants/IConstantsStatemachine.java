c144a80cb74eee78d756e663e9600e7c986a7172
Andreas Mülder <A.Muelder@googlemail.com>
2015-04-10 14:12:55 +0000

    Added constant support for java code generator

---
+++ test-plugins/org.yakindu.sct.generator.java.test/src-gen/org/yakindu/scr/constants/IConstantsStatemachine.java
@@ -0,0 +1,29 @@
+package org.yakindu.scr.constants;
+import org.yakindu.scr.IStatemachine;
+
+public interface IConstantsStatemachine extends IStatemachine {
+
+	public interface SCInterface {
+		public static final long x = 10;
+		public static final long y = SCInterface.x * 2;
+		public void raiseE();
+		public long getX();
+		public long getY();
+		public long getResult();
+		public void setResult(long value);
+
+	}
+
+	public SCInterface getSCInterface();
+
+	public interface SCINamed {
+		public static final String y = "Hello World";
+		public static final long two = 2;
+		public String getY();
+		public long getTwo();
+
+	}
+
+	public SCINamed getSCINamed();
+
+}
