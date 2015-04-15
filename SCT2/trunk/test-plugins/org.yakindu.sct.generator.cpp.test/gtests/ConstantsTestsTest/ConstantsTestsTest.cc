4ea124918d0912094a63b59f1cc3391a74e433d4
markus.muehlbrandt@gmail.com <markus.muehlbrandt@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2015-04-15 10:59:59 +0000

    Fixed pointer reference for logical expression in C++ SCTUnit generator. Regenerated C++ test cases.

--- SCT2/trunk/test-plugins/org.yakindu.sct.generator.cpp.test/gtests/ConstantsTestsTest/ConstantsTestsTest.cc
+++ SCT2/trunk/test-plugins/org.yakindu.sct.generator.cpp.test/gtests/ConstantsTestsTest/ConstantsTestsTest.cc
@@ -19,7 +19,7 @@ TEST(StatemachineTest, constantDefinition) {
 	EXPECT_TRUE(statechart->isActive(Constants::main_region_A));
 	EXPECT_TRUE(statechart->getDefaultSCI()->get_x()== 10l);
 	EXPECT_TRUE(statechart->getDefaultSCI()->get_y()== 20l);
-	EXPECT_TRUE(strcmp(statechart->getSCI_Named().get_y(), "Hello World") == 0);
+	EXPECT_TRUE(strcmp(statechart->getSCI_Named()->get_y(), "Hello World") == 0);
 	statechart->raise_e();
 	statechart->runCycle();
 	EXPECT_TRUE(statechart->getDefaultSCI()->get_result()== 20l);
