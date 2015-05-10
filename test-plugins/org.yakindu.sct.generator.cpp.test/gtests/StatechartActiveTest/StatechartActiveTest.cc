91711ad1c5d6ed93662f2b2bbf1deecd23597475
Axel Terfloth <terfloth@itemis.de>
2015-05-11 00:54:08 +0200

    Generated C++ tests for StatemachineActive tests

---
+++ test-plugins/org.yakindu.sct.generator.cpp.test/gtests/StatechartActiveTest/StatechartActiveTest.cc
@@ -0,0 +1,44 @@
+/**
+* Copyright (c) 2015 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "StatechartActive.h"
+
+TEST(StatemachineTest, inactiveBeforeEnter) {
+	StatechartActive* statechart = new StatechartActive();
+	statechart->init();
+	EXPECT_TRUE(false);
+	delete statechart;
+}
+TEST(StatemachineTest, activeAfterEnter) {
+	StatechartActive* statechart = new StatechartActive();
+	statechart->init();
+	statechart->enter();
+	EXPECT_TRUE(false);
+	delete statechart;
+}
+TEST(StatemachineTest, inactiveAfterExit) {
+	StatechartActive* statechart = new StatechartActive();
+	statechart->init();
+	statechart->enter();
+	statechart->exit();
+	EXPECT_TRUE(false);
+	delete statechart;
+}
+TEST(StatemachineTest, activeAfterReenter) {
+	StatechartActive* statechart = new StatechartActive();
+	statechart->init();
+	statechart->enter();
+	statechart->exit();
+	statechart->enter();
+	EXPECT_TRUE(false);
+	delete statechart;
+}
