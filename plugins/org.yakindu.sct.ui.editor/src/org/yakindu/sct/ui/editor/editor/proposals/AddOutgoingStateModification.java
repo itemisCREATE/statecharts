062154c8a11daaa366eb1f7080a1777dcfb414dc
Axel Terfloth <terfloth@itemis.de>
2015-11-19 10:21:19 +0100

    Added two optional parameters for transition specification and target state name to AddOutgoingStateModification

--- plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editor/proposals/AddOutgoingStateModification.java
+++ plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editor/proposals/AddOutgoingStateModification.java
@@ -18,6 +18,8 @@ import org.yakindu.sct.model.sgraph.Transition;
 import org.yakindu.sct.ui.editor.modifications.AbstractSemanticModification;
 
 /**
+ * This modification add a new outgoing transition to a new target state to a source state.
+ * This modification will set the {@link #transitionSpecification} and {@link #stateName} if it is specified.
  * 
  * @author andreas muelder - Initial contribution and API
  * @author terfloth - adapted to new hooks.
@@ -25,11 +27,40 @@ import org.yakindu.sct.ui.editor.modifications.AbstractSemanticModification;
  */
 public class AddOutgoingStateModification extends AbstractSemanticModification {
 	
+	
+	protected String transitionSpecification;
+	protected String stateName;
+	
+	
 	public AddOutgoingStateModification(View view) {
+		this(view, null, null);
+	}
+	public AddOutgoingStateModification(View view, String transitionSpecification, String stateName) {
 		super();
 		setTargetView(view);
+		this.transitionSpecification = transitionSpecification;
+		this.stateName = stateName;
 	}
 
+
+	
+	public String getTransitionSpecification() {
+		return transitionSpecification;
+	}
+	
+	public void setTransitionSpecification(String transitionSpecification) {
+		this.transitionSpecification = transitionSpecification;
+	}
+	
+	public String getStateName() {
+		return stateName;
+	}
+	
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	
+	
 	@Override
 	protected boolean check(EObject semanticObject, View view) {
 		return semanticObject instanceof State;
@@ -43,5 +74,8 @@ public class AddOutgoingStateModification extends AbstractSemanticModification {
 		state.getParentRegion().getVertices().add(newState);
 		transition.setSource(state);
 		transition.setTarget(newState);
+		
+		if (transitionSpecification != null) transition.setSpecification(transitionSpecification);
+		if (stateName != null) newState.setName(stateName);
 	}
 }
