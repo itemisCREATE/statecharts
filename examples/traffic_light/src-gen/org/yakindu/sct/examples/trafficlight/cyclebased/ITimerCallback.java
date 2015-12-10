fed91bb24847c368fa3c8dd6f8f8199de117486b
Axel Terfloth <terfloth@itemis.de>
2015-12-10 15:33:43 +0100

    renamed example org.yakindu.sct.examples.trafficlight to traffic_light

---
+++ examples/traffic_light/src-gen/org/yakindu/sct/examples/trafficlight/cyclebased/ITimerCallback.java
@@ -0,0 +1,25 @@
+/**
+Copyright (c) 2012 committers of YAKINDU and others.
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html
+
+Contributors:
+    committers of YAKINDU - initial API and implementation
+�*/
+package org.yakindu.sct.examples.trafficlight.cyclebased;
+
+/**
+* Interface for state machines which use timed event triggers.
+*/
+public interface ITimerCallback {
+	
+	/**
+	* Callback method if a time event occurred.
+	* 
+	* @param eventID
+	* 			:The id of the occurred event.
+	*/
+	public void timeElapsed(int eventID);
+}
