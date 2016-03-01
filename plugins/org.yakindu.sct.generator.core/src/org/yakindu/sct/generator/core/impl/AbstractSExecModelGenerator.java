4c0c5551a62b3cd88bb4ce3c56c307bc23c2631b
Johannes Dicks <dicks@itemis.de>
2016-03-01 17:46:00 +0100

    Revert "#473 : pass FSA to concrete IExecutionFlowGenerator"
    This reverts commit 80674c04250167dac3b3eb06ae68997cce4d045f.

--- plugins/org.yakindu.sct.generator.core/src/org/yakindu/sct/generator/core/impl/AbstractSExecModelGenerator.java
+++ plugins/org.yakindu.sct.generator.core/src/org/yakindu/sct/generator/core/impl/AbstractSExecModelGenerator.java
@@ -68,7 +68,7 @@ public abstract class AbstractSExecModelGenerator extends AbstractSGraphModelGen
 	protected void runGenerator(Statechart statechart, GeneratorEntry entry) {
 		if (this instanceof IExecutionFlowGenerator) {
 			IExecutionFlowGenerator flowGenerator = (IExecutionFlowGenerator) this;
-			flowGenerator.generate(createExecutionFlow(statechart, entry), entry, sctFsa.getIFileSystemAccess());
+			flowGenerator.generate(createExecutionFlow(statechart, entry), entry, null);
 		}
 		super.runGenerator(statechart, entry);
 	}
