dbf9da3d731a803b821091447ddb555899aadb20
Enste <Enste@Enste>
2016-06-06 17:42:23 +0200

    backup

--- examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/service/MockExampleService.java
+++ examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/service/MockExampleService.java
@@ -1,5 +1,6 @@
 package org.yakindu.sct.examples.ui.service;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +10,31 @@ public class MockExampleService implements IExampleService {
 	@Override
 	public List<ExampleData> getAllExamples() {
 		List<ExampleData> result = new ArrayList<>();
-		result.add(new ExampleData("my.id", "title", "description"));
-		result.add(new ExampleData("my.id2", "title2", "description2"));
-		result.add(new ExampleData("my.id3", "title3", "description3"));
-		
+		try {
+			URL[] url = {new URL("http://fab404.com/wp-content/uploads/2013/06/knockknockfactory404.jpg")};
+			ExampleData[] edGeneric = new ExampleData[10];
+			for(int i=0;i<edGeneric.length;i++) {
+				edGeneric[i] = new ExampleData(null,"Generic","my.id.nr"+i,"myTitle"+i,"super discription!",url);
+				result.add(edGeneric[i]);
+			}
+			ExampleData generic = createNode("Generic",edGeneric);
+			result.add(generic);
+			ExampleData[] edPro = new ExampleData[10];
+			for(int i=0;i<edPro.length;i++) {
+				edPro[i] = new ExampleData(null,"Pro","my.id.nr"+(10+i),"myTitlePro"+i,"super discribere thats latin",url);
+				result.add(edPro[i]);
+			}
+			ExampleData pro = createNode("Pro",edPro);
+			result.add(pro);
+		} catch(Exception e) {e.printStackTrace();}
+		return result;
+	}
+	
+	protected ExampleData createNode(String domain,ExampleData[] exdat) {
+		ExampleData result = new ExampleData(null,exdat,domain);
+		for(int i=0;i<exdat.length;i++) {
+			exdat[i].setParent(result);
+		}
 		return result;
 	}
 
--- examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/wizards/ExamplesWizardPage.java
+++ examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/wizards/ExamplesWizardPage.java
@@ -1,8 +1,10 @@
 package org.yakindu.sct.examples.ui.wizards;
 
+import java.io.File;
+
 import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -14,49 +16,54 @@ import org.yakindu.sct.examples.ui.service.MockExampleService;
 
 public class ExamplesWizardPage extends WizardPage {
 
+	public static final String WIZARD_TITLE = "Select an example";
+	public static final String WIZARD_DESCRIPTION = "This wizard creates statechart examples.";
+
 	// TODO guice
 	private IExampleService exampleService = new MockExampleService();
 
 	public ExamplesWizardPage(ISelection selection) {
-		super("wizardPage");
-		setTitle("Select an example");
-		setDescription("This wizard creates statechart examples.");
+		super(WIZARD_TITLE);
+		setTitle(WIZARD_TITLE);
+		setDescription(WIZARD_DESCRIPTION);
+		setPageComplete(false);
 	}
 
 	public void createControl(Composite parent) {
-		Composite container = new Composite(parent, SWT.NULL);
-		GridLayout layout = new GridLayout();
-		layout.numColumns = 2;
+		Composite container = new Composite(parent, SWT.NONE);
+		GridDataFactory.fillDefaults().applyTo(container);
+		GridLayout layout = new GridLayout(2, true);
 		container.setLayout(layout);
-		createExamplesViewer(container);
+		createTreeViewer(container);
 		createDetailsPane(container);
 		setControl(container);
 	}
 
-	protected void createDetailsPane(Composite container) {
-		Composite details = new Composite(container, SWT.NONE);
-		GridLayout layout = new GridLayout();
-		layout.numColumns = 1;
-		details.setLayout(layout);
-
-		// TODO: Show Image
-		Label title = new Label(container, SWT.NONE);
-		title.setText("Example Title here...");
-
-		GridDataFactory.fillDefaults().applyTo(title);
-
-		Label description = new Label(container, SWT.NONE);
-		description.setText("Description here...");
-		GridDataFactory.fillDefaults().applyTo(description);
-
-	}
-
-	protected void createExamplesViewer(Composite container) {
-		TableViewer viewer = new TableViewer(container);
+	protected void createTreeViewer(Composite container) {
+		TreeViewer viewer = new TreeViewer(container, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		GridDataFactory.fillDefaults().applyTo(viewer.getControl());
 		viewer.setContentProvider(new ExampleContentProvider());
 		viewer.setLabelProvider(new ExampleLabelProvider());
 		viewer.setInput(exampleService.getAllExamples());
 
 	}
+
+	protected void createDetailsPane(Composite parent) {
+		Composite container = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		container.setLayout(layout);
+
+		// TODO: Show Image
+		Label title = new Label(container, SWT.NONE);
+		title.setText("<title>");
+
+		GridDataFactory.fillDefaults().applyTo(title);
+
+		Label description = new Label(container, SWT.NONE);
+		description.setText("<description>");
+		GridDataFactory.fillDefaults().applyTo(description);
+
+	}
+
 }
