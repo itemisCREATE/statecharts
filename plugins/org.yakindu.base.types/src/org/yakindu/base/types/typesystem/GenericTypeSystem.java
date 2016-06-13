5e49606cccdc666ff79fb69dd491d8c3cffda794
Thomas Kutz <thomas.kutz@itemis.de>
2016-06-13 13:47:02 +0200

    Added array type as complex type in type system.

--- plugins/org.yakindu.base.types/src/org/yakindu/base/types/typesystem/GenericTypeSystem.java
+++ plugins/org.yakindu.base.types/src/org/yakindu/base/types/typesystem/GenericTypeSystem.java
@@ -10,6 +10,12 @@
  */
 package org.yakindu.base.types.typesystem;
 
+import org.yakindu.base.types.ComplexType;
+import org.yakindu.base.types.Operation;
+import org.yakindu.base.types.Parameter;
+import org.yakindu.base.types.TypeParameter;
+import org.yakindu.base.types.TypesFactory;
+
 import com.google.inject.Singleton;
 
 /**
@@ -30,7 +36,42 @@ public class GenericTypeSystem extends AbstractTypeSystem {
 		declarePrimitive(NULL);
 
 		declareSuperType(getType(INTEGER), getType(REAL));
-		
+
 		getType(NULL).setAbstract(true);
+		
+		declareArrayType();
+	}
+
+	protected void declareArrayType() {
+		ComplexType array = TypesFactory.eINSTANCE.createComplexType();
+		array.setName("array");
+		declareType(array, array.getName());
+		getResource().getContents().add(array);
+
+		TypeParameter baseType = TypesFactory.eINSTANCE.createTypeParameter();
+		baseType.setName("baseType");
+		array.getParameter().add(baseType);
+		// GET
+		Operation get = TypesFactory.eINSTANCE.createOperation();
+		get.setName("get");
+		Parameter index = TypesFactory.eINSTANCE.createParameter();
+		index.setType(getType(INTEGER));
+		index.setName("index");
+		get.getParameters().add(index);
+		get.setType(baseType);
+		array.getFeatures().add(get);
+		// ADD
+		Operation add = TypesFactory.eINSTANCE.createOperation();
+		add.setName("add");
+		Parameter element = TypesFactory.eINSTANCE.createParameter();
+		element.setName("element");
+		element.setType(baseType);
+		add.getParameters().add(element);
+		array.getFeatures().add(add);
+
+		// SIZE
+		Operation size = TypesFactory.eINSTANCE.createOperation();
+		size.setName("size");
+		array.getFeatures().add(size);
 	}
 }
