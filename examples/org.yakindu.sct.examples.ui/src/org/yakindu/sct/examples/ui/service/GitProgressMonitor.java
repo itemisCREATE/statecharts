99479c20a11b1b7f3414a571d513672e6c1292c0
Andreas Muelder <muelder@itemis.de>
2016-06-13 12:18:15 +0200

    Added copyright and author tags

--- examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/service/GitProgressMonitor.java
+++ examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/service/GitProgressMonitor.java
@@ -1,9 +1,24 @@
+/**
+ * Copyright (c) 2016 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
 package org.yakindu.sct.examples.ui.service;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.jgit.lib.BatchingProgressMonitor;
 
+/**
+ * 
+ * @author t00manysecretss
+ * 
+ */
 public class GitProgressMonitor extends BatchingProgressMonitor {
 
 	private SubMonitor monitor;
--- examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/wizards/provider/ExampleLabelProvider.java
+++ examples/org.yakindu.sct.examples.ui/src/org/yakindu/sct/examples/ui/wizards/provider/ExampleLabelProvider.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2016 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
 package org.yakindu.sct.examples.ui.wizards.provider;
 
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -9,6 +19,12 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.swt.graphics.Image;
 import org.yakindu.sct.examples.ui.service.IExampleService.ExampleData;
+
+/**
+ * 
+ * @author t00manysecretss
+ * 
+ */
 
 public class ExampleLabelProvider extends LabelProvider implements IStyledLabelProvider {
 
