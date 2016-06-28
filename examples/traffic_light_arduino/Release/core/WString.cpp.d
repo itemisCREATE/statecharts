5fa295819190555e4874f10ce9dc285a5649959c
Johannes Dicks <dicks@itemis.de>
2016-06-28 17:27:43 +0200

    #793 : remove traffic_light_arduino (moved to examplesy repository)
    * #793 : remove C application
    
    * #793 : remove traffic_light_arduino (moved to examplesy repository)

--- examples/traffic_light_arduino/Release/core/WString.cpp.d
+++
@@ -1,5 +0,0 @@
-core/WString.cpp.o: \
- /home/beckmann/Downloads/yakindu-sctpro/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/WString.cpp \
- /home/beckmann/Downloads/yakindu-sctpro/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/WString.h
-
-/home/beckmann/Downloads/yakindu-sctpro/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/WString.h:
