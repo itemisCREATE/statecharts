83bdb3d85712c2edd927e48467b081d70a2e74da
Andreas Muelder <muelder@itemis.de>
2016-07-06 19:46:47 +0200

    Removed examples from statecharts repository
    Mpved to examples repository

--- examples/traffic_light_arduino/Release/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-core/wiring_pulse.S.o: \
- /home/beckmann/Downloads/yakindu-sctpro/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/wiring_pulse.S
--- examples/traffic_light_arduino/Release/scutil/sc_timer_service.c.d
+++
@@ -1,6 +0,0 @@
-scutil/sc_timer_service.c.o: ../scutil/sc_timer_service.c \
- ../scutil/sc_timer_service.h ../scutil/../sc/sc_types.h
-
-../scutil/sc_timer_service.h:
-
-../scutil/../sc/sc_types.h:
