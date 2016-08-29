809af2eeaac597ece710c9b834e6a9495b3704f3
Andreas Muelder <muelder@itemis.de>
2016-08-29 10:03:14 +0200

    Only use XMIResource when no default domain is registered

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/resource/StextResourceFactory.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/resource/StextResourceFactory.java
@@ -13,7 +13,6 @@ package org.yakindu.sct.model.stext.resource;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
@@ -29,15 +28,11 @@ public class StextResourceFactory extends XMIResourceFactoryImpl {
 
 	@Override
 	public Resource createResource(URI uri) {
-
-		// if file is not existing, AND no generic/default domain is available,
-		// resource creation will fail
-		if (!URIConverter.INSTANCE.exists(uri, null)) {
+		String domainID = DomainRegistry.determineDomainID(uri);
+		if (domainID == null || DomainRegistry.getDomain(domainID) == null) {
 			return new XMIResourceImpl(uri);
 		}
-
-		String determinedDomainID = DomainRegistry.determineDomainID(uri);
-		IDomain domain = DomainRegistry.getDomain(determinedDomainID);
+		IDomain domain = DomainRegistry.getDomain(domainID);
 		Injector injector = domain.getInjector(IDomain.FEATURE_RESOURCE);
 		Resource resource = injector.getInstance(Resource.class);
 		ResourceSet set = new ResourceSetImpl();
