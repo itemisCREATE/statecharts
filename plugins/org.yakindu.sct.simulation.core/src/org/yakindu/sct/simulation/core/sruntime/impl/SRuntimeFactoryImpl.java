8fc21beeca3d4692d280ed4301e6b68fcd8e2758
Andreas Muelder <muelder@itemis.de>
2016-10-14 16:09:41 +0200

    Added Any Type to Base Runtime

--- plugins/org.yakindu.sct.simulation.core/src/org/yakindu/sct/simulation/core/sruntime/impl/SRuntimeFactoryImpl.java
+++ plugins/org.yakindu.sct.simulation.core/src/org/yakindu/sct/simulation/core/sruntime/impl/SRuntimeFactoryImpl.java
@@ -16,6 +16,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EFactoryImpl;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.yakindu.sct.simulation.core.sruntime.Any;
 import org.yakindu.sct.simulation.core.sruntime.CompositeSlot;
 import org.yakindu.sct.simulation.core.sruntime.EventDirection;
 import org.yakindu.sct.simulation.core.sruntime.ExecutionContext;
@@ -192,6 +193,8 @@ public class SRuntimeFactoryImpl extends EFactoryImpl implements SRuntimeFactory
 			return Double.parseDouble(value);
 		if (Integer.class.getName().equals(className))
 			return Integer.parseInt(value);
+		if(Any.class.getName().equals(className))
+			return new Any();
 		return super.createFromString(eDataType, initialValue);
 	}
 
