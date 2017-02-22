0f477a94857b22bb81cd3f97baed952ffb0d477b
René Beckmann <rene.bckmnn@gmail.com>
2017-02-22 15:55:27 +0100

    GMockHelper uses GMock for testing

---
+++ test-plugins/org.yakindu.sct.generator.c.test/src/org/yakindu/sct/generator/c/gtest/GMockHelper.java
@@ -0,0 +1,17 @@
+package org.yakindu.sct.generator.c.gtest;
+
+import java.util.List;
+
+public class GMockHelper extends GTestHelper {
+	public GMockHelper(Object owner) {
+		super(owner);
+	}
+	
+	@Override
+	protected List<String> createCommand() {
+		List<String> command = super.createCommand();
+		command.remove("-lgtest_main");
+		command.add("-lgmock_main");
+		return command;
+	}
+}
