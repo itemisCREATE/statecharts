cbfd4e2f9d2c4adba3fbd0cced45744fb75448f9
Andreas Muelder <muelder@itemis.de>
2017-04-27 20:03:02 +0200

    Added tests for validator

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/validation/STextValidationMessages.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/validation/STextValidationMessages.java
@@ -59,7 +59,8 @@ public interface STextValidationMessages {
 	public static final String VALUE_OF_REQUIRES_EVENT = "valueof() expression requires event as argument.";
 	public static final String VAR_ARGS_LAST_CODE = "VarArgsMustBeLast";
 	public static final String VAR_ARGS_LAST_MSG = "The variable argument type must be the last argument.";
-	public static final String WRONG_NUMBER_OF_OPERATIONARGUMENTS = "Wrong number of arguments, expected %s .";
+	public static final String WRONG_NUMBER_OF_ARGUMENTS_MSG = "Wrong number of arguments, expected %s .";
+	public static final String WRONG_NUMBER_OF_ARGUMENTS_CODE = "WrongNrOfArgs";
 
 
 }
