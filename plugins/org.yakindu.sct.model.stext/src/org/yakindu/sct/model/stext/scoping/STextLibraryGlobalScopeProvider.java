2deccb85e2a1a30909480486634ed0c292b4aa92
Andreas Muelder <muelder@itemis.de>
2017-04-28 13:38:32 +0200

    Added caching for library scopes

---
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/scoping/STextLibraryGlobalScopeProvider.java
@@ -0,0 +1,25 @@
+package org.yakindu.sct.model.stext.scoping;
+
+import java.util.Set;
+
+import org.eclipse.emf.common.util.URI;
+import org.yakindu.base.expressions.scoping.AbstractLibraryGlobalScopeProvider;
+
+import com.google.common.collect.Sets;
+import com.google.inject.Singleton;
+/**
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+@Singleton
+public class STextLibraryGlobalScopeProvider extends AbstractLibraryGlobalScopeProvider {
+
+	public static final URI STEXT_LIB = URI
+			.createURI("platform:/plugin/org.yakindu.sct.model.stext.lib/lib/STextLib.xmi");
+
+	@Override
+	public Set<URI> getLibraries() {
+		return Sets.newHashSet(STEXT_LIB);
+	}
+
+}
