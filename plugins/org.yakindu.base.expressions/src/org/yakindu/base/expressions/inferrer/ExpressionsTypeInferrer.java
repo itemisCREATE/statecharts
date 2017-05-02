20c5cb337d289724a3dbd5bdfc529736dec4ad02
Thomas Kutz <thomas.kutz@itemis.de>
2017-05-02 11:45:11 +0200

    Remove sysouts

--- plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/inferrer/ExpressionsTypeInferrer.java
+++ plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/inferrer/ExpressionsTypeInferrer.java
@@ -211,7 +211,6 @@ public class ExpressionsTypeInferrer extends AbstractTypeSystemInferrer implemen
 	}
 
 	public InferenceResult doInfer(FeatureCall e) {
-		System.out.println("ExpressionsTypeInferrer.doInfer(FeatureCall)");
 		// map to hold inference results for type parameters
 		Map<TypeParameter, InferenceResult> inferredTypeParameterTypes = Maps.newHashMap();
 		typeParameterInferrer.inferTypeParametersFromOwner(inferTypeDispatch(e.getOwner()), inferredTypeParameterTypes);
@@ -234,7 +233,6 @@ public class ExpressionsTypeInferrer extends AbstractTypeSystemInferrer implemen
 	}
 
 	public InferenceResult doInfer(ElementReferenceExpression e) {
-		System.out.println("ExpressionsTypeInferrer.doInfer(ElementReferenceExpression)");
 		if (e.isOperationCall()) {
 			if (!e.getReference().eIsProxy()) {
 				return inferOperation(e, (Operation) e.getReference(),
