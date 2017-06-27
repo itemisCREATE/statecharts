8571ff2e248c9d3d8fd20caa8849ba43b1b6a42e
Thomas Kutz <thomas.kutz@itemis.de>
2017-06-27 13:29:59 +0200

    For variadic operation parameter, arguments need to by type-assignable to parameter type (#1431) (#1458)
    * Operation argument can be of a subtype of the parameter type (#1431)
    
    * Adapt to switch in error message (expectation is named first)
    
    * For variadic operation parameter, arguments need to by type-assignable to parameter type (#1431)
    
    * Switch types in error message

--- test-plugins/org.yakindu.sct.model.stext.test/src/org/yakindu/sct/model/stext/test/validation/STextJavaValidatorTest.java
+++ test-plugins/org.yakindu.sct.model.stext.test/src/org/yakindu/sct/model/stext/test/validation/STextJavaValidatorTest.java
@@ -198,7 +198,7 @@ public class STextJavaValidatorTest extends AbstractSTextValidationTest implemen
 
 		model = super.parseExpression("myOperation2('','')", Expression.class.getSimpleName(), scope);
 		validationResult = tester.validate(model);
-		validationResult.assertErrorContains("Incompatible types integer and string.");
+		validationResult.assertErrorContains("Incompatible types string and integer.");
 
 		scope = "internal: operation myOperation(param1... : integer, param2...: integer)";
 		model = super.parseExpression(scope, InternalScope.class.getSimpleName());
