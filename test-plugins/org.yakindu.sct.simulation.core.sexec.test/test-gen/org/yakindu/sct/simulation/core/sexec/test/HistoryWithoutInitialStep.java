81dda07980a5d012878139ce6fda4b1c045187b7
jthoene <jonathan@fam-thoene.de>
2017-06-27 13:29:23 +0200

    Newly generated and updated InterpreterTests (#1457)

---
+++ test-plugins/org.yakindu.sct.simulation.core.sexec.test/test-gen/org/yakindu/sct/simulation/core/sexec/test/HistoryWithoutInitialStep.java
@@ -0,0 +1,73 @@
+/**
+* Copyright (c) 2017 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+package org.yakindu.sct.simulation.core.sexec.test;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yakindu.sct.model.sexec.ExecutionFlow;
+import org.yakindu.sct.model.sexec.interpreter.test.util.AbstractExecutionFlowTest;
+import org.yakindu.sct.model.sexec.interpreter.test.util.SExecInjectionProvider;
+import org.yakindu.sct.test.models.SCTUnitTestModels;
+import com.google.inject.Inject;
+import static org.junit.Assert.assertTrue;
+/**
+ * Unit TestCase for HistoryWithoutInitialStep
+ */
+@SuppressWarnings("all")
+@RunWith(XtextRunner.class)
+@InjectWith(SExecInjectionProvider.class)
+public class HistoryWithoutInitialStep extends AbstractExecutionFlowTest {
+	@Before
+	public void setup() throws Exception{
+		ExecutionFlow flow = models.loadExecutionFlowFromResource("HistoryWithoutInitialStep.sct");
+		initInterpreter(flow);
+	}
+	@Test
+	public void enterThroughInitialEntry() throws Exception {
+		init();
+		assertTrue(isStateActive("C"));
+		raiseEvent("next");
+		interpreter.runCycle();
+		assertTrue(isStateActive("D"));
+	}
+	@Test
+	public void enterCThroughHistory() throws Exception {
+		init();
+		assertTrue(isStateActive("C"));
+		raiseEvent("toA");
+		interpreter.runCycle();
+		assertTrue(isStateActive("A"));
+		raiseEvent("toHistory");
+		interpreter.runCycle();
+		assertTrue(isStateActive("C"));
+	}
+	@Test
+	public void enterDThroughHistory() throws Exception {
+		init();
+		raiseEvent("next");
+		interpreter.runCycle();
+		assertTrue(isStateActive("D"));
+		raiseEvent("toA");
+		interpreter.runCycle();
+		assertTrue(isStateActive("A"));
+		raiseEvent("toHistory");
+		interpreter.runCycle();
+		assertTrue(isStateActive("D"));
+	}
+	public void init() throws Exception {
+		interpreter.enter();
+		assertTrue(isStateActive("A"));
+		raiseEvent("toB");
+		interpreter.runCycle();
+	}
+}
