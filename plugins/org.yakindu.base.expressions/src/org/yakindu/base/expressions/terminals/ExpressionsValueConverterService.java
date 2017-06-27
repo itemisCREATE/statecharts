24429540a89f061dd35c7da7d01cb03bf7bd0d39
jthoene <jthoene@192.168.3.210>
2017-06-27 11:44:04 +0200

    Added the Converter rule for the QID Rule

--- plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/terminals/ExpressionsValueConverterService.java
+++ plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/terminals/ExpressionsValueConverterService.java
@@ -13,6 +13,9 @@ package org.yakindu.base.expressions.terminals;
 import org.eclipse.xtext.common.services.DefaultTerminalConverters;
 import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverter;
+import org.eclipse.xtext.conversion.impl.AbstractIDValueConverter;
+
+import com.google.inject.Inject;
 
 /**
  * Contributes stext specific value converters
@@ -20,6 +23,9 @@ import org.eclipse.xtext.conversion.ValueConverter;
  * @author axel terfloth
  */
 public class ExpressionsValueConverterService extends DefaultTerminalConverters {
+	
+	@Inject
+	private AbstractIDValueConverter idValueConverter;
 	
 	protected BoolValueConverter boolConverter = new BoolValueConverter();
 	protected HexValueConverter hexConverter = new HexValueConverter();
@@ -38,6 +44,11 @@ public class ExpressionsValueConverterService extends DefaultTerminalConverters 
 	@ValueConverter(rule = "BINARY")
 	public IValueConverter<Integer> BINARY() {
 		return binaryConverter;
+	}
+	
+	@ValueConverter(rule = "QID")
+	public IValueConverter<String> QID() {
+		return  idValueConverter;
 	}
 
 
