9fdb7526c26f533328fb0629af5c5492242c8e44
jthoene <jthoene@192.168.3.210>
2017-06-29 17:00:22 +0200

    Using QualifiedName instead of String splitting

--- plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/terminals/QIDValueConverter.java
+++ plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/terminals/QIDValueConverter.java
@@ -11,9 +11,12 @@
 */
 package org.yakindu.base.expressions.terminals;
 
+import java.util.Arrays;
+
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.conversion.impl.AbstractIDValueConverter;
 import org.eclipse.xtext.conversion.impl.AbstractValueConverter;
+import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.nodemodel.INode;
 
 import com.google.inject.Inject;
@@ -31,38 +34,29 @@ public class QIDValueConverter extends AbstractValueConverter<String> {
 
 	@Override
 	public String toValue(String string, INode node) throws ValueConverterException {
-		String result = "";
-		if (string.split(".").length > 0) {
-			for (String str : string.split(".")) {
-				String partResult = idValueConverter.toValue(str, node);
-				result = reassembleString(string, result, str, partResult);
+		if(string.split(".").length > 0){
+			QualifiedName name = QualifiedName.create(Arrays.asList(string.split(".")));
+			QualifiedName result = QualifiedName.EMPTY;
+			for(String str : name.getSegments()){
+				result.append(idValueConverter.toValue(str, node));
 			}
-		} else {
-			result = idValueConverter.toValue(string, node);
+			return result.toString();
+		}else{
+			return idValueConverter.toValue(string, node);
 		}
-		return result;
 	}
 
 	@Override
 	public String toString(String value) throws ValueConverterException {
-		String result = "";
-		if (value.split(".").length > 0) {
-			for (String str : value.split(".")) {
-				String partResult = idValueConverter.toString(str);
-				result = reassembleString(value, result, str, partResult);
+		if(value.split(".").length > 0){
+			QualifiedName name = QualifiedName.create(Arrays.asList(value.split(".")));
+			QualifiedName result = QualifiedName.EMPTY;
+			for(String str : name.getSegments()){
+				result.append(idValueConverter.toString(str));
 			}
-		} else {
-			result = idValueConverter.toString(value);
+			return result.toString();
+		}else{
+			return idValueConverter.toString(value);
 		}
-		return result;
-	}
-
-	private String reassembleString(String value, String result, String str, String partResult) {
-		if (value.split("")[0].equals(str)) {
-			result += partResult;
-		} else {
-			result += ("." + partResult);
-		}
-		return result;
 	}
 }
