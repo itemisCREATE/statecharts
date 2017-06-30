42ca22aa1765a4fc252d864be276218753557b55
Johannes Dicks <dicks@itemis.de>
2017-06-30 13:13:39 +0200

    #1429 : test plugin should not be a fragment
    *try solve injection problems (no resource bound)

--- test-plugins/org.yakindu.sct.model.resource.test/src/org/yakindu/sct/model/resource/test/SCTTestInjectorProvider.java
+++ test-plugins/org.yakindu.sct.model.resource.test/src/org/yakindu/sct/model/resource/test/SCTTestInjectorProvider.java
@@ -1,27 +1,28 @@
 package org.yakindu.sct.model.resource.test;
 
 import static com.google.inject.Guice.createInjector;
-import static com.google.inject.util.Modules.override;
 
 import org.eclipse.xtext.junit4.IInjectorProvider;
-import org.yakindu.sct.model.resource.SCTXtextIntegrationModule;
+import org.eclipse.xtext.ui.shared.SharedStateModule;
 import org.yakindu.sct.model.stext.STextRuntimeModule;
 import org.yakindu.sct.model.stext.ui.STextUiModule;
 import org.yakindu.sct.model.stext.ui.internal.STextActivator;
 
 import com.google.inject.Injector;
 import com.google.inject.Module;
+import com.google.inject.util.Modules;
 
 public class SCTTestInjectorProvider implements IInjectorProvider {
 
 
 	public Injector getInjector() {
-		return createInjector(override(override(override(new STextRuntimeModule()).with(getSharedStateModule()))
-				.with(new STextUiModule(STextActivator.getInstance()))).with(new SCTXtextIntegrationModule()));
-	}
 
-	protected Module getSharedStateModule() {
-		return new org.eclipse.xtext.ui.shared.SharedStateModule();
+
+		Module with2 = Modules.override(new STextRuntimeModule()).with(new SharedStateModule());
+
+		Module with3 = Modules.override(with2).with((Module) new STextUiModule(STextActivator.getInstance()));
+
+		return createInjector(with3);
 	}
 
 }
