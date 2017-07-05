8ffddf3c84c67d6182ab2dceb38d50d116a81617
Johannes Dicks <dicks@itemis.de>
2017-07-05 14:14:20 +0200

    trivial
    * copyright header

--- test-plugins/org.yakindu.sct.model.resource.test/src/org/yakindu/sct/model/resource/test/SCTTestInjectorProvider.java
+++ test-plugins/org.yakindu.sct.model.resource.test/src/org/yakindu/sct/model/resource/test/SCTTestInjectorProvider.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2017 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ */
 package org.yakindu.sct.model.resource.test;
 
 import static com.google.inject.Guice.createInjector;
@@ -11,7 +20,9 @@ import org.yakindu.sct.model.stext.ui.internal.STextActivator;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.util.Modules;
-
+/**
+ * @author Johannes Dicks - Initial contribution and API
+ */
 public class SCTTestInjectorProvider implements IInjectorProvider {
 
 
