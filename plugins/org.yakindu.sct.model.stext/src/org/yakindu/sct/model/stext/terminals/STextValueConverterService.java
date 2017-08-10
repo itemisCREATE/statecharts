26858a814bf39eaac1e453e6490ffb527d1c0e6c
jthoene <jonathan@fam-thoene.de>
2017-08-10 14:08:45 +0200

    Refactoring

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/terminals/STextValueConverterService.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/terminals/STextValueConverterService.java
@@ -4,26 +4,18 @@ import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverter;
 import org.yakindu.base.expressions.terminals.ExpressionsValueConverterService;
 
-import com.google.inject.Inject;
-
 public class STextValueConverterService extends ExpressionsValueConverterService{
 	
 	public static final String XID = "XID";
 	public static final String FQN = "FQN";
 	
-	@Inject
-	protected FQNValueConverter fqnConverter;
-	
-	@Inject
-	protected XIDValueConverter xidConverter;
-	
 	@ValueConverter(rule = XID)
 	public IValueConverter<String> XID() {
-		return xidConverter;
+		return qidConverter;
 	}
 	
 	@ValueConverter(rule = FQN)
 	public IValueConverter<String> FQN() {
-		return fqnConverter;
+		return qidConverter;
 	}
 }
