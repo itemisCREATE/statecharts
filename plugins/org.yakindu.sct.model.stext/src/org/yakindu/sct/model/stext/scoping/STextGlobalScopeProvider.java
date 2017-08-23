f683d113ac7eb4ac68854c2cc605135dfd0cf7d0
jthoene <jonathan@fam-thoene.de>
2017-08-23 14:29:57 +0200

    Filtering Annotation Properties in STextScopeProvider

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/scoping/STextGlobalScopeProvider.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/scoping/STextGlobalScopeProvider.java
@@ -107,7 +107,6 @@ public class STextGlobalScopeProvider extends ImportUriGlobalScopeProvider {
 			}
 		});
 		result = filterAnnotations(reference, result);
-		result = filterAnnotationArgument(result);
 		return result;
 	}
 
@@ -120,15 +119,6 @@ public class STextGlobalScopeProvider extends ImportUriGlobalScopeProvider {
 				}
 			});
 		return result;
-	}
-	
-	protected IScope filterAnnotationArgument(IScope result) {
-		return new FilteringScope(result, new Predicate<IEObjectDescription>() {
-			@Override
-			public boolean apply(IEObjectDescription input) {
-				return input.getEClass() != TypesPackage.Literals.PROPERTY;
-			}
-		});
 	}
 
 	@Override
