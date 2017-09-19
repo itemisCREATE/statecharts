d5688f208ebb20415be927b1799edfb834692419
Andreas Muelder <muelder@itemis.de>
2017-09-19 17:43:35 +0200

    added implicit imports for active scoping (#1294)
    added navigation to states from active statement
    removed states from global scope

--- plugins/org.yakindu.sct.model.stext.ui/src/org/yakindu/sct/model/stext/ui/contentassist/STextProposalProvider.java
+++ plugins/org.yakindu.sct.model.stext.ui/src/org/yakindu/sct/model/stext/ui/contentassist/STextProposalProvider.java
@@ -36,6 +36,7 @@ import org.yakindu.base.expressions.expressions.ElementReferenceExpression;
 import org.yakindu.base.expressions.expressions.FeatureCall;
 import org.yakindu.base.types.Operation;
 import org.yakindu.base.types.Type;
+import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.stext.services.STextGrammarAccess;
 import org.yakindu.sct.model.stext.stext.InterfaceScope;
 import org.yakindu.sct.model.stext.stext.InternalScope;
@@ -242,6 +243,11 @@ public class STextProposalProvider extends AbstractSTextProposalProvider {
 		if (element instanceof Type) {
 			return super.getDisplayString(element, qualifiedNameAsString, shortName);
 		}
+		if (element instanceof State) {
+			qualifiedNameAsString = getQualifiedNameConverter()
+					.toString(getQualifiedNameConverter().toQualifiedName(qualifiedNameAsString).skipFirst(1));
+			return super.getDisplayString(element, qualifiedNameAsString, shortName);
+		}
 
 		if (element == null || element.eIsProxy()) {
 			return qualifiedNameAsString;
@@ -302,6 +308,11 @@ public class STextProposalProvider extends AbstractSTextProposalProvider {
 		priorityOptimizer.accept(proposal);
 	}
 
+	public void completeActiveStateReferenceExpression_Value(EObject model, Assignment assignment,
+			ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+		lookupCrossReference(((CrossReference) assignment.getTerminal()), context, acceptor);
+	}
+
 	private void alterPriority(ICompletionProposal proposal, int delta) {
 		if (proposal == null || !(proposal instanceof ConfigurableCompletionProposal))
 			return;
@@ -310,8 +321,8 @@ public class STextProposalProvider extends AbstractSTextProposalProvider {
 	}
 
 	/**
-	 * The acceptor delegate creates a Dummy EObject of type Keyword for the
-	 * User Help Hover integration
+	 * The acceptor delegate creates a Dummy EObject of type Keyword for the User
+	 * Help Hover integration
 	 * 
 	 */
 	public class AcceptorDelegate implements ICompletionProposalAcceptor {
--- plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editor/SCTFileEditorOpener.java
+++ plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editor/SCTFileEditorOpener.java
@@ -10,15 +10,24 @@
 */
 package org.yakindu.sct.ui.editor.editor;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.xtext.ui.editor.IURIEditorOpener;
+import org.yakindu.sct.ui.editor.partitioning.DiagramPartitioningUtil;
 
 /**
  * Opens <code>.sct</code> files in corresponding statechart diagramm editor.
@@ -33,8 +42,31 @@ public class SCTFileEditorOpener implements IURIEditorOpener {
 		try {
 			IFile fileToOpen = toFile(uri);
 			if (fileToOpen != null) {
-				return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
 						.openEditor(new FileEditorInput(fileToOpen), StatechartDiagramEditor.ID);
+
+				if (editor instanceof DiagramEditor) {
+					DiagramEditor diagramEditor = (DiagramEditor) editor;
+					EObject targetElement = (EObject) diagramEditor.getDiagram().eResource().getEObject(uri.fragment());
+					if (targetElement == null) {
+						return editor;
+					}
+					View view = DiagramPartitioningUtil.findNotationView(targetElement);
+					if (view == null) {
+						return editor;
+					}
+					Diagram element = DiagramPartitioningUtil.getDiagramContaining(view.getElement());
+					diagramEditor = (DiagramEditor) DiagramPartitioningUtil.openEditor(element);
+					@SuppressWarnings("rawtypes")
+					Map editPartRegistry = diagramEditor.getDiagramGraphicalViewer().getEditPartRegistry();
+					EditPart targetEditPart = (EditPart) editPartRegistry.get(view);
+					if (targetEditPart != null) {
+						DiagramPartitioningUtil.selectElementsInDiagram(diagramEditor,
+								Arrays.asList(new EditPart[] { targetEditPart }));
+					}
+
+				}
+				return editor;
 			}
 		} catch (PartInitException e) {
 			e.printStackTrace();
@@ -46,7 +78,7 @@ public class SCTFileEditorOpener implements IURIEditorOpener {
 	public IEditorPart open(URI referenceOwnerURI, EReference reference, int indexInList, boolean select) {
 		return open(referenceOwnerURI, select);
 	}
-	
+
 	public IFile toFile(URI uri) {
 		if (uri.isPlatformResource()) {
 			String platformString = uri.toPlatformString(true);
