6cbd9bb89480df4957cbbfda84379ba7a3c15524
René Beckmann <rene.bckmnn@gmail.com>
2017-09-28 13:56:43 +0200

    New generator commons project

---
+++ plugins/org.yakindu.sct.generator.common/src/org/yakindu/sct/generator/common/IModifierOwner.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2017 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	rbeckmann - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.generator.common;
+
+import java.util.List;
+
+/**
+ * @author rbeckmann
+ *
+ */
+public interface IModifierOwner extends ICodePart {
+	public List<IModifier> getModifiers();
+	public void addModifier(IModifier part);
+	public void addModifiers(List<IModifier> part);
+}
