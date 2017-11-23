76a8a8b65205edfab76fc289ab0ee767919b0562
Robert Rudi <rudi.robert@itemis.de>
2017-11-23 16:06:53 +0100

    Changed serialization of internal scope of statechart specification. The serialization process now remembers the order and location of the contents.

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/resource/StextResource.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/resource/StextResource.java
@@ -11,9 +11,17 @@
 
 package org.yakindu.sct.model.stext.resource;
 
+import java.util.Map;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.TerminalRule;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.ILeafNode;
+import org.eclipse.xtext.nodemodel.impl.HiddenLeafNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.parser.IParseResult;
 import org.yakindu.base.types.Annotation;
 import org.yakindu.base.types.Declaration;
@@ -31,12 +39,19 @@ import org.yakindu.sct.model.stext.stext.StatechartSpecification;
 import org.yakindu.sct.model.stext.stext.TransitionReaction;
 import org.yakindu.sct.model.stext.stext.TransitionSpecification;
 
+import com.google.common.collect.Maps;
+
 /**
  * 
  * @author andreas muelder - Initial contribution and API
  * 
  */
 public class StextResource extends AbstractSCTResource {
+
+	private static final String SINGLELINE_COMMENT_TERMINAL = "SL";
+	private static final String MULTILINE_COMMENT_TERMINAL = "ML";
+	private static final String WHITESPACE_TERMINAL = "WS";
+	private static final String NEW_LINE = System.lineSeparator();
 
 	public StextResource() {
 		this(null);
@@ -51,15 +66,15 @@ public class StextResource extends AbstractSCTResource {
 		StringBuilder builder = new StringBuilder();
 		if (statechart.getNamespace() != null) {
 			builder.append("namespace " + statechart.getNamespace());
-			builder.append("\n");
+			builder.append(NEW_LINE);
 		}
 		for (Annotation annotation : statechart.getAnnotations()) {
 			builder.append(serialize(annotation));
-			builder.append("\n");
+			builder.append(NEW_LINE);
 		}
 		for (Scope scope : statechart.getScopes()) {
 			builder.append(serializeScope(scope));
-			builder.append("\n");
+			builder.append(NEW_LINE);
 		}
 		statechart.setSpecification(builder.toString());
 	}
@@ -78,18 +93,84 @@ public class StextResource extends AbstractSCTResource {
 		StringBuilder builder = new StringBuilder();
 		InternalScope copiedScope = copyAndStripInternalScope(internalScope);
 		builder.append(serialize(copiedScope));
-		builder.append("\n");
-
-		for (Declaration declaration : internalScope.getDeclarations()) {	
-			builder.append(serialize(declaration));
-			builder.append("\n");
+		Map<Integer, EObject> specificationElements = Maps.newTreeMap();
+		for (Declaration declaration : internalScope.getDeclarations()) {
+			int lineNumber = getNodeLineNumber(declaration);
+			specificationElements.put(lineNumber, declaration);
 		}
-		builder.append("\n");
 		for (Reaction reaction : internalScope.getReactions()) {
-			builder.append(serializeReaction(reaction));
-			builder.append("\n");
+			int lineNumber = getNodeLineNumber(reaction);
+			specificationElements.put(lineNumber, reaction);
+		}
+
+		for (Integer lineNumber : specificationElements.keySet()) {
+			EObject node = specificationElements.get(lineNumber);
+			Iterable<ILeafNode> leafNodes = NodeModelUtils.findActualNodeFor(node).getLeafNodes();
+			for (ILeafNode iLeafNode : leafNodes) {
+				if (iLeafNode instanceof HiddenLeafNode) {
+					HiddenLeafNode hiddenLeafNode = (HiddenLeafNode) iLeafNode;
+					if (hiddenLeafNode.getGrammarElement() instanceof TerminalRule) {
+						TerminalRule terminalRule = (TerminalRule) hiddenLeafNode.getGrammarElement();
+						int startLine = hiddenLeafNode.getStartLine();
+						int endLine = hiddenLeafNode.getEndLine();
+
+						if (!terminalRule.getName().contains(SINGLELINE_COMMENT_TERMINAL)) {
+							if (terminalRule.getName().contains(MULTILINE_COMMENT_TERMINAL)) {
+								if (endLine - startLine == 0) { // Multiline comment in single line
+									builder.append(NEW_LINE); // add linebreak between contiguous comments
+								}
+								break;
+							}
+						} else {
+							if (nextNodesAreWhitespace(hiddenLeafNode)) {
+								builder.append(NEW_LINE); // add linebreak if there are contiguous whitespaces
+								break;
+							}
+							endLine--; // reduces new lines if single line comment is present
+						}
+						for (int i = startLine; i < endLine; i++)
+							builder.append(NEW_LINE);
+
+					} else
+						break; // break on first non-hidden leaf node
+				}
+			}
+			builder.append(serializeInteralScopeContents(node));
 		}
 		return builder.toString();
+	}
+
+	private boolean nextNodesAreWhitespace(HiddenLeafNode hiddenLeafNode) {
+		if (hiddenLeafNode.getNextSibling() != null && hiddenLeafNode.getNextSibling().getNextSibling() != null) {
+			EObject next = hiddenLeafNode.getNextSibling().getGrammarElement();
+			EObject secondNext = hiddenLeafNode.getNextSibling().getNextSibling().getGrammarElement();
+			if (next != null && next instanceof TerminalRule && secondNext != null
+					&& secondNext instanceof TerminalRule) {
+				TerminalRule nextRule = (TerminalRule) next;
+				TerminalRule secondNextRule = (TerminalRule) secondNext;
+				if (nextRule.getName().contains(WHITESPACE_TERMINAL)
+						&& secondNextRule.getName().contains(WHITESPACE_TERMINAL)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private String serializeInteralScopeContents(EObject node) {
+		StringBuilder builder = new StringBuilder();
+		if (node instanceof Reaction) {
+			builder.append(serializeReaction((Reaction) node).trim());
+		} else {
+			builder.append(serialize(node));
+		}
+		return builder.toString();
+	}
+
+	protected int getNodeLineNumber(EObject object) {
+		ICompositeNode node = NodeModelUtils.getNode(object);
+		int lineNumber = node.getStartLine();
+		return lineNumber;
 	}
 
 	/**
@@ -117,7 +198,7 @@ public class StextResource extends AbstractSCTResource {
 		StringBuilder builder = new StringBuilder();
 		for (Reaction reaction : reactions) {
 			if (builder.length() > 0)
-				builder.append("\n");
+				builder.append(NEW_LINE);
 			builder.append(serializeReaction(reaction));
 		}
 		state.setSpecification(builder.toString());
