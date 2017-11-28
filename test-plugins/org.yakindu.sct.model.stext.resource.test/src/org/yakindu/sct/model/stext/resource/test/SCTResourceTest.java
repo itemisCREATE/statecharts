6eee70db60ead3e4954ad2293be05cd44d5b3373
Robert Rudi <rudi.robert@itemis.de>
2017-11-28 09:21:05 +0100

    Due to changes for the serialization of statecharts, the SCTResourceTests had to be changed.
    Added a whitespace between the "internal" and ":" terminal

--- test-plugins/org.yakindu.sct.model.stext.resource.test/src/org/yakindu/sct/model/stext/resource/test/SCTResourceTest.java
+++ test-plugins/org.yakindu.sct.model.stext.resource.test/src/org/yakindu/sct/model/stext/resource/test/SCTResourceTest.java
@@ -164,7 +164,7 @@ public class SCTResourceTest {
 		Scope internalScope = (InternalScope) statechart.getScopes().get(0);
 		Event event = internalScope.getEvents().get(0);
 		event.setName("Event2");
-		assertEquals("internal:event Event2", statechart.getSpecification().trim());
+		assertEquals("internal: event Event2", statechart.getSpecification().trim());
 		assertEquals("" + res.getSyntaxDiagnostics(), 0, res.getSyntaxDiagnostics().size());
 	}
 
