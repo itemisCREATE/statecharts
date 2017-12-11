1164a731a55ea53689da42a2acb58c057112c8ac
svenjawendler <wendler@itemis.de>
2017-12-11 12:49:11 +0100

    issue1726 version increment script (#1730)
    * Initial commit of script to increase version qualifiers
    
    not completed yet
    only works for feature.xml
    Techniques: Xpath, java.nio
    
    * Updated / finished for statecharts
    
    * Refactored UpdateVersion

---
+++ releng/org.yakindu.sct.renamescript/src/org/yakindu/sct/renamescript/FeatureXMLGetVersionVisitor.java
@@ -0,0 +1,81 @@
+package org.yakindu.sct.renamescript;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+
+public class FeatureXMLGetVersionVisitor implements FileVisitor<Path> {
+
+	private String version;
+	private String filePattern = "feature.xml";
+
+	public static String getVersion(Path startDir) throws IOException {
+		FeatureXMLGetVersionVisitor fileVisitor = new FeatureXMLGetVersionVisitor();
+		Files.walkFileTree(startDir, fileVisitor);
+
+		return fileVisitor.getVersion();
+	}
+
+	private String getVersion() {
+		return this.version;
+	}
+
+	@Override
+	public FileVisitResult postVisitDirectory(Path path, IOException arg1) throws IOException {
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes arg1) throws IOException {
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path path, BasicFileAttributes arg1) throws IOException {
+		FileSystem fileSystem = FileSystems.getDefault();
+		PathMatcher pathMatcher = fileSystem.getPathMatcher("glob:" + filePattern);
+
+		if (pathMatcher.matches(path.getFileName())) {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			try {
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document doc = builder.parse(path.toFile());
+
+				XPathFactory xpathfactory = XPathFactory.newInstance();
+				XPath xpath = xpathfactory.newXPath();
+
+				XPathExpression expr = xpath.compile("string(//feature[1]/@version)");
+				Object result = expr.evaluate(doc, XPathConstants.STRING);
+				if (result instanceof String) {
+					this.version = result.toString();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return FileVisitResult.TERMINATE;
+		}
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFileFailed(Path path, IOException arg1) throws IOException {
+		return FileVisitResult.CONTINUE;
+	}
+
+}
---
+++ releng/org.yakindu.sct.renamescript/src/org/yakindu/sct/renamescript/VersionUpdateVisitor.java
@@ -0,0 +1,93 @@
+package org.yakindu.sct.renamescript;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VersionUpdateVisitor implements FileVisitor<Path> {
+
+	private int fileCount = 0;
+	private int dirCount = 0;
+	private String searchPattern = "";
+	private List<Path> searchList;
+	private String replacePattern = "";
+	private String filePattern = "";
+
+	/**
+	 * Pattern examples: "abc*.*" ;  "abc?fg?.j*"
+	 */
+	public static List<Path> searchFor(Path startDir, String filePattern, String searchPattern, String replacePattern)
+			throws IOException {
+
+		VersionUpdateVisitor fileVisitor = new VersionUpdateVisitor(filePattern, searchPattern, replacePattern);
+		Files.walkFileTree(startDir, fileVisitor);
+
+		return fileVisitor.getResultList();
+	}
+
+	private VersionUpdateVisitor(String filePattern, String searchPattern, String newValue) {
+		this.filePattern = filePattern;
+		this.searchPattern = searchPattern;
+		this.replacePattern = newValue;
+		this.searchList = new ArrayList<>();
+	}
+
+	@Override
+	public FileVisitResult postVisitDirectory(Path path, IOException arg1) throws IOException {
+		// System.out.println("postVisitDirectory: " + path + " Exception = " + arg1);
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes arg1) throws IOException {
+		// System.out.println("preVisitDirectory: " + path + " size = " + arg1.size() +
+		// " bytes");
+		dirCount++;
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path path, BasicFileAttributes arg1) throws IOException {
+		FileSystem fileSystem = FileSystems.getDefault();
+		PathMatcher pathMatcher = fileSystem.getPathMatcher("glob:" + filePattern);
+
+		if (pathMatcher.matches(path.getFileName())) {
+			searchList.add(path);
+			String charset = Charset.defaultCharset().name();
+			//System.out.println(charset);
+			String content = new String(Files.readAllBytes(path), charset);
+			content = content.replaceAll(this.searchPattern, this.replacePattern);
+			Files.write(path, content.getBytes(charset));
+			fileCount++;
+		}
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFileFailed(Path path, IOException arg1) throws IOException {
+		// System.out.println("visitFileFailed " + " Exception = " + arg1);
+		return FileVisitResult.CONTINUE;
+	}
+
+	public int getFileCount() {
+		return fileCount;
+	}
+
+	public int getDirCount() {
+		return dirCount;
+	}
+
+	public List<Path> getResultList() {
+		return searchList;
+	}
+
+}
