4ff8fd874b52926fb2ecbb770bb064e79c10c03e
Johannes Dicks <dicks@itemis.de>
2018-01-02 21:36:50 +0100

    #1830 : sync editor title on element name change

--- plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/partitioning/DiagramPartitioningEditor.java
+++ plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/partitioning/DiagramPartitioningEditor.java
@@ -404,6 +404,7 @@ public abstract class DiagramPartitioningEditor extends DiagramDocumentEditor
 				if (Notification.SET == notification.getEventType()) {
 					Object feature = notification.getFeature();
 					if (feature != null && feature.equals(BasePackage.Literals.NAMED_ELEMENT__NAME)) {
+						initializeTitle(getDiagram());
 						viewer.refresh();
 					}
 				}
