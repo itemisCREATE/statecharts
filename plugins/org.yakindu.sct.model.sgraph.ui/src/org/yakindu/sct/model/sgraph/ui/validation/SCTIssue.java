2ec96df485628f8393d2e1fc2f418aa9d1dccc43
René Beckmann <rene.bckmnn@gmail.com>
2018-02-15 14:47:18 +0100

    Add EObject field to SCTIssue

--- plugins/org.yakindu.sct.model.sgraph.ui/src/org/yakindu/sct/model/sgraph/ui/validation/SCTIssue.java
+++ plugins/org.yakindu.sct.model.sgraph.ui/src/org/yakindu/sct/model/sgraph/ui/validation/SCTIssue.java
@@ -6,20 +6,21 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * Contributors:
  * 	committers of YAKINDU - initial API and implementation
- * 
+ *
  */
 package org.yakindu.sct.model.sgraph.ui.validation;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.validation.CheckType;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.validation.Issue.IssueImpl;
 
 /**
- * 
+ *
  * @author andreas muelder - Initial contribution and API
- * 
+ *
  */
 public class SCTIssue extends IssueImpl {
 
@@ -27,55 +28,76 @@ public class SCTIssue extends IssueImpl {
 
 	private final String semanticURI;
 
-	public SCTIssue(final Issue delegate, String semanticURI) {
+	private final EObject eObject;
+
+	public SCTIssue(final Issue delegate, EObject eObject, String semanticURI) {
 		this.delegate = delegate;
+		this.eObject = eObject;
 		this.semanticURI = semanticURI;
+	}
+
+	public SCTIssue(final Issue delegate, String semanticURI) {
+		this(delegate, null, semanticURI);
 	}
 
 	public String getSemanticURI() {
 		return semanticURI;
 	}
 
+	public EObject getEObject() {
+		return this.eObject;
+	}
+
+	@Override
 	public Severity getSeverity() {
 		return delegate.getSeverity();
 	}
 
+	@Override
 	public String getMessage() {
 		return delegate.getMessage();
 	}
 
+	@Override
 	public String getCode() {
 		return delegate.getCode();
 	}
 
+	@Override
 	public CheckType getType() {
 		return delegate.getType();
 	}
 
+	@Override
 	public URI getUriToProblem() {
 		return delegate.getUriToProblem();
 	}
 
+	@Override
 	public Integer getLineNumber() {
 		return delegate.getLineNumber();
 	}
 
+	@Override
 	public Integer getOffset() {
 		return delegate.getOffset();
 	}
 
+	@Override
 	public Integer getLength() {
 		return delegate.getLength();
 	}
 
+	@Override
 	public boolean isSyntaxError() {
 		return delegate.isSyntaxError();
 	}
 
+	@Override
 	public String[] getData() {
 		return delegate.getData();
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
