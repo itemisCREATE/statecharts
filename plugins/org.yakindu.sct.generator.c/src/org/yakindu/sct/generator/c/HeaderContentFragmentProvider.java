5848e9db99d4cc6af3f096f1798ea370201da4d2
René Beckmann <rene.bckmnn@gmail.com>
2018-05-23 11:28:23 +0200

    Extract abstract superclass

--- plugins/org.yakindu.sct.generator.c/src/org/yakindu/sct/generator/c/HeaderContentFragmentProvider.java
+++ plugins/org.yakindu.sct.generator.c/src/org/yakindu/sct/generator/c/HeaderContentFragmentProvider.java
@@ -10,45 +10,24 @@
  */
 package org.yakindu.sct.generator.c;
 
-import static org.yakindu.sct.model.stext.lib.StatechartAnnotations.EVENT_DRIVEN_ANNOTATION;
-
-import org.yakindu.base.types.Annotation;
 import org.yakindu.sct.generator.c.files.StatemachineHeader;
 import org.yakindu.sct.generator.c.submodules.StatemachineHeaderContentFragment;
 import org.yakindu.sct.generator.c.submodules.eventdriven.EventDrivenStatemachineHeaderFragment;
-import org.yakindu.sct.model.sgen.GeneratorEntry;
-import org.yakindu.sct.model.sgraph.Statechart;
-
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-import com.google.inject.Provider;
 
 /**
  * @author rbeckmann
  *
  */
-public class HeaderContentFragmentProvider implements Provider<StatemachineHeader> {
-	@Inject
-	Injector injector;
-
+public class HeaderContentFragmentProvider extends AbstractContentFragmentProvider<StatemachineHeader> {
 	@Override
 	public StatemachineHeader get() {
 		StatemachineHeader header = injector.getInstance(StatemachineHeader.class);
 
-		GeneratorEntry entry = injector.getInstance(GeneratorEntry.class);
-
-		if (isEventDriven(entry)) {
+		if (isEventDriven()) {
 			header.getContentProviders().add(injector.getInstance(EventDrivenStatemachineHeaderFragment.class));
 		}
 
 		header.getContentProviders().add(injector.getInstance(StatemachineHeaderContentFragment.class));
 		return header;
 	}
-
-	protected boolean isEventDriven(GeneratorEntry entry) {
-		Statechart statechart = (Statechart) entry.getElementRef();
-		Annotation eventDrivenAnnotation = statechart.getAnnotationOfType(EVENT_DRIVEN_ANNOTATION);
-		return eventDrivenAnnotation != null;
-	}
-
 }
--- plugins/org.yakindu.sct.generator.c/src/org/yakindu/sct/generator/c/SourceContentFragmentProvider.java
+++ plugins/org.yakindu.sct.generator.c/src/org/yakindu/sct/generator/c/SourceContentFragmentProvider.java
@@ -10,44 +10,24 @@
  */
 package org.yakindu.sct.generator.c;
 
-import static org.yakindu.sct.model.stext.lib.StatechartAnnotations.EVENT_DRIVEN_ANNOTATION;
-
-import org.yakindu.base.types.Annotation;
 import org.yakindu.sct.generator.c.files.StatemachineSource;
 import org.yakindu.sct.generator.c.submodules.StatemachineSourceContentFragment;
 import org.yakindu.sct.generator.c.submodules.eventdriven.EventDrivenStatemachineSourceContentProvider;
-import org.yakindu.sct.model.sgen.GeneratorEntry;
-import org.yakindu.sct.model.sgraph.Statechart;
-
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-import com.google.inject.Provider;
 
 /**
  * @author rbeckmann
  *
  */
-public class SourceContentFragmentProvider implements Provider<StatemachineSource> {
-	@Inject
-	Injector injector;
-
+public class SourceContentFragmentProvider extends AbstractContentFragmentProvider<StatemachineSource> {
 	@Override
 	public StatemachineSource get() {
 		StatemachineSource source = injector.getInstance(StatemachineSource.class);
 
-		GeneratorEntry entry = injector.getInstance(GeneratorEntry.class);
 
 		source.getContentProviders().add(injector.getInstance(StatemachineSourceContentFragment.class));
-		if (isEventDriven(entry)) {
+		if (isEventDriven()) {
 			source.getContentProviders().add(injector.getInstance(EventDrivenStatemachineSourceContentProvider.class));
 		}
 		return source;
 	}
-
-	protected boolean isEventDriven(GeneratorEntry entry) {
-		Statechart statechart = (Statechart) entry.getElementRef();
-		Annotation eventDrivenAnnotation = statechart.getAnnotationOfType(EVENT_DRIVEN_ANNOTATION);
-		return eventDrivenAnnotation != null;
-	}
-
 }
