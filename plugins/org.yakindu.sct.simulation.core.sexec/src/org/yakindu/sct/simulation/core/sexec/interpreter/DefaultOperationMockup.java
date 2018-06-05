fc842e6b7a0e315cd06374419e9ced6c6a19110b
jthoene <jonathan@fam-thoene.de>
2018-06-05 08:30:26 +0200

    Added copyright and author tag and removed TODOs

--- plugins/org.yakindu.sct.simulation.core.sexec/src/org/yakindu/sct/simulation/core/sexec/interpreter/DefaultOperationMockup.java
+++ plugins/org.yakindu.sct.simulation.core.sexec/src/org/yakindu/sct/simulation/core/sexec/interpreter/DefaultOperationMockup.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2018 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
 package org.yakindu.sct.simulation.core.sexec.interpreter;
 
 import static java.util.Arrays.asList;
@@ -13,6 +23,11 @@ import org.yakindu.base.types.Operation;
 
 import com.google.inject.Singleton;
 
+/**
+ * 
+ * @author jonathan thoene - Initial contribution and API
+ * 
+ */
 @Singleton
 public class DefaultOperationMockup implements IOperationMockup {
 	
