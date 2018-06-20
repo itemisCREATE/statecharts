7017c6e170c3f940635e73377a05054b5e836035
René Beckmann <rene.bckmnn@gmail.com>
2018-06-20 11:26:50 +0200

    Extract 'isEventDriven' into helper extension

--- plugins/org.yakindu.sct.generator.c/src/org/yakindu/sct/generator/c/AbstractContentFragmentProvider.java
+++ plugins/org.yakindu.sct.generator.c/src/org/yakindu/sct/generator/c/AbstractContentFragmentProvider.java
@@ -1,10 +1,7 @@
 package org.yakindu.sct.generator.c;
 
-import static org.yakindu.sct.model.stext.lib.StatechartAnnotations.EVENT_DRIVEN_ANNOTATION;
-
-import org.yakindu.base.types.Annotation;
+import org.yakindu.sct.generator.core.extensions.AnnotationExtensions;
 import org.yakindu.sct.model.sgen.GeneratorEntry;
-import org.yakindu.sct.model.sgraph.Statechart;
 
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -13,11 +10,11 @@ import com.google.inject.Provider;
 public abstract class AbstractContentFragmentProvider<T> implements Provider<T> {
 	@Inject
 	protected Injector injector;
+	@Inject
+	protected AnnotationExtensions annotationExtensions;
 
 	protected boolean isEventDriven() {
 		GeneratorEntry entry = injector.getInstance(GeneratorEntry.class);
-		Statechart statechart = (Statechart) entry.getElementRef();
-		Annotation eventDrivenAnnotation = statechart.getAnnotationOfType(EVENT_DRIVEN_ANNOTATION);
-		return eventDrivenAnnotation != null;
+		return annotationExtensions.isEventDriven(entry);
 	}
 }
