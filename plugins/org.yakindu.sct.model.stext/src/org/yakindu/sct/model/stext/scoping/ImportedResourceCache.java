b944c7ae97939a02e1c80dab07a8f78e79671c8a
andreas muelder <muelder@itemis.de>
2018-07-03 20:16:37 +0200

    this fixes that files are not updated after deletion / recreation
    - If a proxy can not be resolved, the resource is loaded into the
    resoure set with an error and loaded = true.
    
    An new Workspace Listener is added that unloaded this stale resource.
    
    - cache here is removed since the EObjectDescription Lookup has a cache
    itself.

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/scoping/ImportedResourceCache.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/scoping/ImportedResourceCache.java
@@ -10,23 +10,18 @@
  */
 package org.yakindu.sct.model.stext.scoping;
 
-import java.util.Optional;
-
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.transaction.RunnableWithResult;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.workspace.WorkspaceEditingDomainFactory;
-import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.resource.IResourceDescription.Manager;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
-import org.eclipse.xtext.util.IResourceScopeCache;
 
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import static org.yakindu.sct.model.stext.scoping.SharedEditingDomainFactory.*;
 
 /**
  * 
@@ -36,22 +31,10 @@ import com.google.inject.Singleton;
 @Singleton
 public class ImportedResourceCache {
 
-	public static final String DOMAIN_ID = "org.yakindu.sct.domain.resources";
-
 	@Inject
 	private IResourceServiceProvider.Registry serviceProviderRegistry;
-	@Inject
-	private IResourceScopeCache cache;
 
-	protected static synchronized TransactionalEditingDomain getEditingDomain() {
-		TransactionalEditingDomain editingDomain = TransactionalEditingDomain.Registry.INSTANCE
-				.getEditingDomain(DOMAIN_ID);
-		if (editingDomain == null) {
-			TransactionalEditingDomain resourceDomain = WorkspaceEditingDomainFactory.INSTANCE.createEditingDomain();
-			resourceDomain.setID(DOMAIN_ID);
-			TransactionalEditingDomain.Registry.INSTANCE.add(DOMAIN_ID, resourceDomain);
-			new WorkspaceSynchronizer(resourceDomain);
-		}
+	protected TransactionalEditingDomain getEditingDomain() {
 		return TransactionalEditingDomain.Registry.INSTANCE.getEditingDomain(DOMAIN_ID);
 	}
 
@@ -64,26 +47,17 @@ public class ImportedResourceCache {
 							final ResourceSet set = getResourceSet();
 							final Resource resource = set.getResource(uri, true);
 							if (resource != null) {
-								Optional<IResourceDescription> optional = cache.get(ImportedResourceCache.class,
-										resource, new Provider<Optional<IResourceDescription>>() {
-											@Override
-											public Optional<IResourceDescription> get() {
-												IResourceServiceProvider serviceProvider = serviceProviderRegistry
-														.getResourceServiceProvider(uri);
-												if (serviceProvider == null)
-													return Optional.empty();
-												final Manager resourceDescriptionManager = serviceProvider
-														.getResourceDescriptionManager();
-												if (resourceDescriptionManager == null)
-													return Optional.empty();
-												IResourceDescription result = resourceDescriptionManager
-														.getResourceDescription(resource);
-												return Optional.of(result);
-											}
-										});
-								if (optional.isPresent()) {
-									setResult(optional.get());
-								}
+								IResourceServiceProvider serviceProvider = serviceProviderRegistry
+										.getResourceServiceProvider(uri);
+								if (serviceProvider == null)
+									return;
+								final Manager resourceDescriptionManager = serviceProvider
+										.getResourceDescriptionManager();
+								if (resourceDescriptionManager == null)
+									return;
+								IResourceDescription result = resourceDescriptionManager
+										.getResourceDescription(resource);
+								setResult(result);
 							}
 						}
 					});
