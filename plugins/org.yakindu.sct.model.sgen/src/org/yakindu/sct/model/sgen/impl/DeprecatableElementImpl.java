4783bb2c8963cf63fe9178261a76847163a58c2a
rherrmannr <Robin.Herrmann@itemis.com>
2018-09-04 11:34:42 +0200

    generated sgen

--- plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/impl/DeprecatableElementImpl.java
+++ plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/impl/DeprecatableElementImpl.java
@@ -213,7 +213,7 @@ public class DeprecatableElementImpl extends EObjectImpl implements Deprecatable
 	public String toString() {
 		if (eIsProxy()) return super.toString();
 
-		StringBuffer result = new StringBuffer(super.toString());
+		StringBuilder result = new StringBuilder(super.toString());
 		result.append(" (deprecated: ");
 		result.append(deprecated);
 		result.append(", comment: ");
--- plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/impl/GeneratorModelImpl.java
+++ plugins/org.yakindu.sct.model.sgen/src/org/yakindu/sct/model/sgen/impl/GeneratorModelImpl.java
@@ -251,7 +251,7 @@ public class GeneratorModelImpl extends EObjectImpl implements GeneratorModel {
 	public String toString() {
 		if (eIsProxy()) return super.toString();
 
-		StringBuffer result = new StringBuffer(super.toString());
+		StringBuilder result = new StringBuilder(super.toString());
 		result.append(" (generatorId: ");
 		result.append(generatorId);
 		result.append(')');
