36af02c9dccf19a2fdc0e7d841d9cd8d084bb92e
René Beckmann <rene.bckmnn@gmail.com>
2018-09-06 15:19:06 +0200

    Add copyright header and URI helper func

--- plugins/org.yakindu.sct.commons/src/org/yakindu/sct/commons/EMFHelper.java
+++ plugins/org.yakindu.sct.commons/src/org/yakindu/sct/commons/EMFHelper.java
@@ -1,8 +1,23 @@
+/**
+ * Copyright (c) 2018 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ *
+ */
 package org.yakindu.sct.commons;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 
 public class EMFHelper {
@@ -31,5 +46,9 @@ public class EMFHelper {
 		for (EClass superType : superTypes) {
 			getSuperClassTree(superClasses, superType, depth + 1);
 		}
+	}
+
+	public static IFile getIFileFromEMFUri(URI uri) {
+		return ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uri.toPlatformString(true)));
 	}
 }
