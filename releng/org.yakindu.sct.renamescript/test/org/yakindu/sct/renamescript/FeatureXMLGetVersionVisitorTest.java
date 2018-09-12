91c3963a7096ad4d466c919114b8a61408db7d4f
Thomas Kutz <thomas.kutz@itemis.de>
2018-09-12 10:16:48 +0200

    Make update version script OS independent (#2244)

--- releng/org.yakindu.sct.renamescript/test/org/yakindu/sct/renamescript/FeatureXMLGetVersionVisitorTest.java
+++ releng/org.yakindu.sct.renamescript/test/org/yakindu/sct/renamescript/FeatureXMLGetVersionVisitorTest.java
@@ -1,10 +1,9 @@
 package org.yakindu.sct.renamescript;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +12,7 @@ class FeatureXMLGetVersionVisitorTest {
 	@Test
 	void test() throws IOException
 	{
-		Path f = Paths.get(System.getProperty("user.dir")+"\\..\\..\\");
+		Path f = UpdateVersion.getSearchPath();
 		String currentVersion = "error";
 		currentVersion = FeatureXMLGetVersionVisitor.getVersion(f);
 		assertEquals("3.1.0.qualifier",currentVersion);
