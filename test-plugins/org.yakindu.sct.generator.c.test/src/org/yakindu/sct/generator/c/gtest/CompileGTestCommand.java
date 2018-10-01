c549cb6fda0ff2d9113119839e1dcb8aa1220882
René Beckmann <rene.bckmnn@gmail.com>
2018-10-01 16:10:27 +0200

    Make GTest compiler pedantic C++98

--- test-plugins/org.yakindu.sct.generator.c.test/src/org/yakindu/sct/generator/c/gtest/CompileGTestCommand.java
+++ test-plugins/org.yakindu.sct.generator.c.test/src/org/yakindu/sct/generator/c/gtest/CompileGTestCommand.java
@@ -24,38 +24,40 @@ public class CompileGTestCommand {
 	private List<String> sources = Lists.newArrayList();
 	private String compiler;
 	private String program;
-	
+
 	public CompileGTestCommand directory(String dir) {
 		this.dir = dir;
 		return this;
 	}
-	
+
 	public CompileGTestCommand includes(List<String> includes) {
 		this.includes = includes;
 		return this;
 	}
-	
+
 	public CompileGTestCommand sources(List<String> sources) {
 		this.sources  = sources;
 		return this;
 	}
-	
+
 	public CompileGTestCommand compiler(String compiler) {
 		this.compiler = compiler;
 		return this;
 	}
-	
+
 	public CompileGTestCommand program(String program) {
 		this.program = program;
 		return this;
 	}
-	
+
 	public List<String> build() {
-		List<String> command = new ArrayList<String>();
+		List<String> command = new ArrayList<>();
 		command.add(compiler);
 		command.add("-o");
 		command.add(getFileName(program));
 		command.add("-O2");
+		command.add("-std=c++98");
+		command.add("-pedantic-errors");
 		if (dir != null)
 			command.add("-I" + dir + "/include");
 		for (String include : includes) {
@@ -72,11 +74,11 @@ public class CompileGTestCommand {
 		command.add("-lstdc++");
 		command.add("-pthread");
 		// command.add("-pg");
-		
+
 		System.out.println(command);
 		return command;
 	}
-	
+
 	protected String getFileName(String path) {
 		return new Path(path).lastSegment();
 	}
