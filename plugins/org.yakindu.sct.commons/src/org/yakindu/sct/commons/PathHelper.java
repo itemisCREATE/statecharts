df044abf261e2b2e73b4727f83976ef86e3c3836
René Beckmann <rene.bckmnn@gmail.com>
2018-10-02 14:39:23 +0200

    Improve naming and let null argument throw NPE

--- plugins/org.yakindu.sct.commons/src/org/yakindu/sct/commons/PathHelper.java
+++ plugins/org.yakindu.sct.commons/src/org/yakindu/sct/commons/PathHelper.java
@@ -10,10 +10,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.URI;
 
 public class PathHelper {
-	public String getPathString(File file) {
-		if (file == null) {
-			return "";
-		}
+	public String toPathString(File file) {
 		try {
 			return file.getCanonicalPath();
 		} catch (IOException e) {
@@ -21,7 +18,7 @@ public class PathHelper {
 			return file.getAbsolutePath();
 		}
 	}
-
+	
 	public Path toPath(String pathString) {
 		if (pathString == null) {
 			return null;
@@ -34,21 +31,21 @@ public class PathHelper {
 			return path;
 		}
 	}
-	
+
 	public Path toPath(URI uri) {
 		return toPath(uri.toFileString());
 	}
-
+	
 	public Path toPath(IFile file) {
 		return toPath(file.getLocation().toOSString());
 	}
-
+	
 	public boolean pathsEqual(String p1, String p2) {
 		Path path1 = toPath(p1);
 		Path path2 = toPath(p2);
 		return pathsEqual(path1, path2);
 	}
-
+	
 	public boolean pathsEqual(Path p1, Path p2) {
 		return Objects.equals(p1, p2);
 	}
