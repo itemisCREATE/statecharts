57e3ebc1b94f95452f5c34990c72960ea7a97166
Tobias Angele <tangele@itemis.de>
2018-10-25 15:58:18 +0200

    add null check for unresolved proxy message

--- plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/linking/LinkingDiagnosticMessageProvider.java
+++ plugins/org.yakindu.base.expressions/src/org/yakindu/base/expressions/linking/LinkingDiagnosticMessageProvider.java
@@ -3,11 +3,19 @@ package org.yakindu.base.expressions.linking;
 import org.eclipse.xtext.diagnostics.Diagnostic;
 import org.eclipse.xtext.diagnostics.DiagnosticMessage;
 import org.eclipse.xtext.diagnostics.Severity;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 
 public class LinkingDiagnosticMessageProvider extends org.eclipse.xtext.linking.impl.LinkingDiagnosticMessageProvider {
 	@Override
 	public DiagnosticMessage getUnresolvedProxyMessage(ILinkingDiagnosticContext context) {
-		String message = "Could not find declaration of '" + context.getLinkText() + "'";
-		return new DiagnosticMessage(message, Severity.ERROR, Diagnostic.LINKING_DIAGNOSTIC);
+		String message;
+		if (context.getLinkText() == null) {
+			ICompositeNode node = NodeModelUtils.getNode(context.getContext());
+			message = node.getText();
+		} else {
+			message = context.getLinkText();
+		}
+		return new DiagnosticMessage("Could not find declaration of '" + message + "'", Severity.ERROR, Diagnostic.LINKING_DIAGNOSTIC);
 	}
 }
