771d8db37336150066dec0091883ad6ae0fb7a41
René Beckmann <rene.bckmnn@gmail.com>
2018-11-06 15:02:33 +0100

    Cleanup output+

--- test-plugins/org.yakindu.sct.generator.c.test/src/org/yakindu/sct/generator/c/gtest/GTestHelper.java
+++ test-plugins/org.yakindu.sct.generator.c.test/src/org/yakindu/sct/generator/c/gtest/GTestHelper.java
@@ -42,105 +42,102 @@ import org.yakindu.sct.model.sgraph.Statechart;
  *
  */
 public class GTestHelper {
-
+	
 	/**
 	 * @author rbeckmann
 	 *
 	 */
 	public enum Compiler {
 		GCC("gcc"), GPLUSPLUS("g++");
-
+		
 		protected String command;
-
+		
 		Compiler(String command) {
 			this.command = command;
 		}
-
+		
 		public String getCommand() {
 			return this.command;
 		}
 	}
-
+	
 	private final Object owner;
 	protected Compiler compiler;
 	protected TestFileCopier copier;
-
+	
 	public GTestHelper(Object owner) {
 		this(owner, Compiler.GCC);
 	}
-
+	
 	public GTestHelper(Object owner, Compiler compiler) {
 		this.owner = owner;
 		this.compiler = compiler;
-		
+
 		this.copier = new TestFileCopier((p, m) -> {
 			p.create(m);
 			return p;
 		});
 	}
-
+	
 	public void compile() {
 		copyFilesFromBundleToFolder();
 		IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(getTargetPath());
 		File directory = resource.getLocation().toFile();
 		List<String> command = createCommand();
-
+		
 		long t0 = System.currentTimeMillis();
 		getCommandExecutor().execute(command, directory, false);
 		long t1 = System.currentTimeMillis();
-		System.out.println("Compiling " + directory.getName() + "took " + (t1 - t0) + "ms");
+		System.out.println("Compiling " + directory.getName() + " took " + (t1 - t0) + "ms");
 	}
-
+	
 	public void generate() {
-		long t0 = System.currentTimeMillis();
 		IPath targetPath = getTargetPath();
-
+		
 		// copy model to JUnit workspace
 		copier.copyFileFromBundleToFolder(getModelBundle(), getModelPath(), targetPath);
-
+		
 		String sgenFileName = getSgenFileName(getTestProgram());
 		copier.copyFileFromBundleToFolder(getTestBundle(), sgenFileName, targetPath);
-
+		
 		GeneratorModel model = getGeneratorModel(sgenFileName);
 		model.getEntries().get(0).setElementRef(getStatechart());
-
+		
 		performFullBuild();
-
+		
 		getGeneratorExecutorLookup().execute(model);
-		long t1 = System.currentTimeMillis();
-		System.out.println("Generating took " + (t1 - t0) + "ms");
 	}
-
+	
 	protected GeneratorModel getGeneratorModel(String sgenFileName) {
 		IPath path = new Path(sgenFileName);
 		Resource sgenResource = loadResource(getWorkspaceFileFor(path));
 		GeneratorModel model = (GeneratorModel) sgenResource.getContents().get(0);
 		return model;
 	}
-
+	
 	protected List<String> getFilesToCopy() {
 		return new ArrayList<>(
 				Arrays.asList(owner.getClass().getAnnotation(GTest.class).additionalFilesToCopy()));
 	}
-
+	
 	protected List<String> getFilesToCompile() {
 		return new ArrayList<>(
 				Arrays.asList(owner.getClass().getAnnotation(GTest.class).additionalFilesToCompile()));
 	}
-
+	
 	protected GeneratorExecutorLookup getGeneratorExecutorLookup() {
 		return new EclipseContextGeneratorExecutorLookup();
 	}
-
+	
 	protected String getSgenFileName(String testProgram) {
 		String sgenFileName = testProgram + ".sgen";
 		return sgenFileName;
 	}
-
+	
 	protected GCCCommandExecutor getCommandExecutor() {
 		return new GCCCommandExecutor();
 	}
-
+	
 	protected void performFullBuild() {
 		try {
 			ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, null);
@@ -148,29 +145,29 @@ public class GTestHelper {
 			throw new RuntimeException(e);
 		}
 	}
-
+	
 	protected IFile getWorkspaceFileFor(IPath filePath) {
 		return ResourcesPlugin.getWorkspace().getRoot().getFile(getTargetProjectPath().append(filePath));
 	}
-
+	
 	protected Statechart getStatechart() {
 		IPath path = new Path(getTargetPath().toString() + "/" + getModelPath().lastSegment());
 		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
 		Resource resource = loadResource(file);
 		return (Statechart) resource.getContents().get(0);
 	}
-
+	
 	protected Resource loadResource(IFile file) {
 		URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
 		Resource resource = new ResourceSetImpl().getResource(uri, true);
 		return resource;
 	}
-
+	
 	protected Bundle getModelBundle() {
 		String bundle = getStatechartBundleAnnotation();
 		return Platform.getBundle(bundle);
 	}
-
+	
 	private void copyFilesFromBundleToFolder() {
 		IPath targetPath = getTargetPath();
 		List<String> testDataFiles = getFilesToCopy();
@@ -179,18 +176,18 @@ public class GTestHelper {
 			copier.copyFileFromBundleToFolder(getTestBundle(), file, targetPath);
 		}
 	}
-
+	
 	protected List<String> createCommand() {
 		String gTestDirectory = getGTestDirectory();
-
+		
 		List<String> includes = new ArrayList<>();
 		getIncludes(includes);
-
+		
 		List<String> sourceFiles = getFilesToCompile();
 		getSourceFiles(sourceFiles);
-		
-		File targetPath = ResourcesPlugin.getWorkspace().getRoot().findMember(getTargetPath()).getLocation().toFile();
 
+		File targetPath = ResourcesPlugin.getWorkspace().getRoot().findMember(getTargetPath()).getLocation().toFile();
+		
 		return new CompileGTestCommand()
 				.compiler(getCompilerCommand())
 				.program(getFileName(getTestProgram()))
@@ -201,18 +198,18 @@ public class GTestHelper {
 				.mainLib(getMainLib())
 				.build();
 	}
-
+	
 	/**
 	 * @return
 	 */
 	protected String getCompilerCommand() {
 		return this.compiler.getCommand();
 	}
-
+	
 	protected String getMainLib() {
 		return "gtest_main";
 	}
-
+	
 	/**
 	 * @return
 	 */
@@ -220,55 +217,55 @@ public class GTestHelper {
 		String gTestDirectory = System.getenv("GTEST_DIR");
 		return gTestDirectory;
 	}
-
+	
 	protected String getFileName(String path) {
 		return new Path(path).lastSegment();
 	}
-
+	
 	protected IPath getTargetPath() {
 		return getTargetProjectPath().append(new Path(getTestProgram()).removeLastSegments(1));
 	}
-
+	
 	protected IPath getModelPath() {
 		return new Path(getModelAnnotation());
 	}
-
+	
 	protected void getIncludes(Collection<String> includes) {
 	}
-
+	
 	protected void getSourceFiles(Collection<String> files) {
 		files.add(getFileName(getTestSourceFile()));
 	}
-
+	
 	protected String getTestSourceFile() {
 		return owner.getClass().getAnnotation(GTest.class).sourceFile();
 	}
-
+	
 	protected void getTestDataFiles(Collection<String> files) {
 		files.add(getTestSourceFile());
 	}
-
+	
 	protected String getTestProgram() {
 		return owner.getClass().getAnnotation(GTest.class).program();
 	}
-
+	
 	protected String getModelAnnotation() {
 		return owner.getClass().getAnnotation(GTest.class).model();
 	}
-
+	
 	protected String getTestBundleAnnotation() {
 		return owner.getClass().getAnnotation(GTest.class).testBundle();
 	}
-
+	
 	protected String getStatechartBundleAnnotation() {
 		return owner.getClass().getAnnotation(GTest.class).statechartBundle();
 	}
-
+	
 	protected IPath getTargetProjectPath() {
 		return new Path(getTestBundleAnnotation());
 	}
-	
 
+	
 	protected Bundle getTestBundle() {
 		Bundle bundle = getAnnotatedTestBundle();
 		if (bundle == null) {
@@ -276,7 +273,7 @@ public class GTestHelper {
 		}
 		return bundle;
 	}
-
+	
 	protected Bundle getAnnotatedTestBundle() {
 		String testProject = getTestBundleAnnotation();
 		if (!testProject.isEmpty()) {
@@ -287,7 +284,7 @@ public class GTestHelper {
 		}
 		return null;
 	}
-
 	
 
+	
 }
