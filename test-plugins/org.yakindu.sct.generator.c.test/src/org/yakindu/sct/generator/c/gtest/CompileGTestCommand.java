fdfa7f897f5a42f4be30b5bc1066d56cdecf7db3
Rene Beckmann <rene.bckmnn@gmail.com>
2018-11-07 10:19:34 +0100

    Include debug symbols when compiling tests (#2392)

--- test-plugins/org.yakindu.sct.generator.c.test/src/org/yakindu/sct/generator/c/gtest/CompileGTestCommand.java
+++ test-plugins/org.yakindu.sct.generator.c.test/src/org/yakindu/sct/generator/c/gtest/CompileGTestCommand.java
@@ -18,41 +18,42 @@ import org.eclipse.core.runtime.Path;
 import com.google.common.collect.Lists;
 
 public class CompileGTestCommand {
-
+	
 	private String dir;
 	private List<String> includes = Lists.newArrayList();
 	private List<String> sources = Lists.newArrayList();
 	private String compiler;
 	private String program;
-	
+
 	public CompileGTestCommand directory(String dir) {
 		this.dir = dir;
 		return this;
 	}
-	
+
 	public CompileGTestCommand includes(List<String> includes) {
 		this.includes = includes;
 		return this;
 	}
-	
+
 	public CompileGTestCommand sources(List<String> sources) {
 		this.sources  = sources;
 		return this;
 	}
-	
+
 	public CompileGTestCommand compiler(String compiler) {
 		this.compiler = compiler;
 		return this;
 	}
-	
+
 	public CompileGTestCommand program(String program) {
 		this.program = program;
 		return this;
 	}
-	
+
 	public List<String> build() {
-		List<String> command = new ArrayList<String>();
+		List<String> command = new ArrayList<>();
 		command.add(compiler);
+		command.add("-g");
 		command.add("-o");
 		command.add(getFileName(program));
 		command.add("-O2");
@@ -72,11 +73,11 @@ public class CompileGTestCommand {
 		command.add("-lstdc++");
 		command.add("-pthread");
 		// command.add("-pg");
-		
+
 		System.out.println(command);
 		return command;
 	}
-	
+
 	protected String getFileName(String path) {
 		return new Path(path).lastSegment();
 	}
