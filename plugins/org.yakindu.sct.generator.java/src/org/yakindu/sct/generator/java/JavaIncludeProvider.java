5e40c3d1b58da61f36c10dab125e4864587b1e7e
René Beckmann <rene.bckmnn@gmail.com>
2018-11-13 16:30:04 +0100

    Add return type to Iterable<String>

--- plugins/org.yakindu.sct.generator.java/src/org/yakindu/sct/generator/java/JavaIncludeProvider.java
+++ plugins/org.yakindu.sct.generator.java/src/org/yakindu/sct/generator/java/JavaIncludeProvider.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import org.yakindu.sct.model.sexec.ExecutionFlow;
 
 public class JavaIncludeProvider {
-	public Iterable<CharSequence> getImports(ExecutionFlow it) {
+	public Iterable<String> getImports(ExecutionFlow it) {
 		return Collections.emptyList();
 	}
 }
