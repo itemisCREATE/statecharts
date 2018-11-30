e39bd730d3a93e0956b7fac58ff4cb58d34e45ff
Robin Herrmann <Robin.Herrmann@itemis.com>
2018-11-30 15:38:36 +0100

    method to gather valid domains into one string

--- plugins/org.yakindu.sct.generator.genmodel/src/org/yakindu/sct/generator/genmodel/validation/SGenJavaValidator.java
+++ plugins/org.yakindu.sct.generator.genmodel/src/org/yakindu/sct/generator/genmodel/validation/SGenJavaValidator.java
@@ -275,18 +275,22 @@ public class SGenJavaValidator extends AbstractSGenJavaValidator {
 			if (reference instanceof DomainElement) {
 				String domainID = ((DomainElement) reference).getDomainID();
 				if (!validDomains.isEmpty() && !validDomains.contains(domainID)) {
-					String issueData = null;
-					for (String validDomain : validDomains) {
-						if(issueData == null) {
-							issueData = validDomain;
-						} else {
-							issueData = issueData + ";" + validDomain;
-						}
-					}
-					error(String.format(INVALID_DOMAIN_ID, domainID, Arrays.toString(validDomains.toArray())), generatorEntry, SGenPackage.Literals.GENERATOR_ENTRY__ELEMENT_REF, CODE_REQUIRED_DOMAIN, issueData);
+					error(String.format(INVALID_DOMAIN_ID, domainID, Arrays.toString(validDomains.toArray())), generatorEntry, SGenPackage.Literals.GENERATOR_ENTRY__ELEMENT_REF, CODE_REQUIRED_DOMAIN, comineDomainsToString(validDomains));
 				}
 			}
 		}
+	}
+
+	private String comineDomainsToString(Set<String> validDomains) {
+		String issueData = null;
+		for (String validDomain : validDomains) {
+			if(issueData == null) {
+				issueData = validDomain;
+			} else {
+				issueData = issueData + ";" + validDomain;
+			}
+		}
+		return issueData;
 	}
 	@Check
 	public void checkDeprecatedParameters(FeatureParameterValue value) {
