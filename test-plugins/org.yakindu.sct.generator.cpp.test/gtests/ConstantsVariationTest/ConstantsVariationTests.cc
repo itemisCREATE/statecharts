85513850a66689ca5a10d7348468c951f52d8e3f
Robin Herrmann <Robin.Herrmann@itemis.com>
2018-12-03 15:11:52 +0100

    added tests for constants

---
+++ test-plugins/org.yakindu.sct.generator.cpp.test/gtests/ConstantsVariationTest/ConstantsVariationTests.cc
@@ -0,0 +1,64 @@
+#include <string>
+#include "gtest/gtest.h"
+#include "ConstantsVariation.h"
+#include "sc_runner.h"
+#include "sc_types.h"
+
+namespace  {
+
+ConstantsVariation* statechart;
+
+
+
+//! The timers are managed by a timer service. */
+static SctUnitRunner * runner;
+
+class ConstantsVariationTests : public ::testing::Test{
+	protected:
+	virtual void SetUp() {
+		statechart = new ConstantsVariation();
+		statechart->init();
+		runner = new SctUnitRunner(
+			statechart,
+			false,
+			200
+		);
+	}
+	virtual void TearDown() {
+		delete statechart;
+		delete runner;
+	}
+};
+
+
+TEST_F(ConstantsVariationTests, constantDefinition) {
+	
+	statechart->enter();
+	
+	EXPECT_TRUE(statechart->isStateActive(ConstantsVariation::main_region_StateA));
+	
+	EXPECT_TRUE(statechart->getDefaultSCI()->get_b2()== true);
+	
+	EXPECT_TRUE(statechart->getSCI_Named()->get_b()== true);
+	
+	EXPECT_TRUE(statechart->getDefaultSCI()->get_i2()== 5l);
+	
+	EXPECT_TRUE(statechart->getSCI_Named()->get_i()== 5l);
+	
+	EXPECT_TRUE(statechart->getDefaultSCI()->get_r2()== 5l);
+	
+	EXPECT_TRUE(statechart->getSCI_Named()->get_r()== 5l);
+	
+	EXPECT_TRUE(strcmp(statechart->getDefaultSCI()->get_str2(), "String") == 0);
+	
+	EXPECT_TRUE(strcmp(statechart->getSCI_Named()->get_str(), "String") == 0);
+	
+	runner->proceed_cycles(1);
+	
+	EXPECT_TRUE(statechart->isFinal());
+	
+	
+}
+
+	}
+
