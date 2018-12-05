86f7ae1b301d172041d4d2ad83aae429ace8217f
Robin Herrmann <Robin.Herrmann@itemis.com>
2018-12-05 09:31:38 +0100

    expand test case with multiple trace listener

--- test-plugins/org.yakindu.sct.generator.java.test/src/org/yakindu/sct/generator/java/test/TracingTest.java
+++ test-plugins/org.yakindu.sct.generator.java.test/src/org/yakindu/sct/generator/java/test/TracingTest.java
@@ -11,13 +11,21 @@ public class TracingTest {
 	@Test
 	public void testTracing() {
 		TracingStatemachine sm = new TracingStatemachine();
-		TracingImpl<State> ifaceTraceObserver = new TracingImpl<State>();
-		sm.addIfaceTraceObserver(ifaceTraceObserver);
+		TracingImpl<State> ifaceTraceObserver1 = new TracingImpl<State>();
+		TracingImpl<State> ifaceTraceObserver2 = new TracingImpl<State>();
+		TracingImpl<State> ifaceTraceObserver3 = new TracingImpl<State>();
+		sm.addIfaceTraceObserver(ifaceTraceObserver1);
+		sm.addIfaceTraceObserver(ifaceTraceObserver2);
+		sm.addIfaceTraceObserver(ifaceTraceObserver3);
 		sm.init();
 		sm.enter();
-		assertTrue(ifaceTraceObserver.isEntered());
+		assertTrue(ifaceTraceObserver1.isEntered());
+		assertTrue(ifaceTraceObserver2.isEntered());
+		assertTrue(ifaceTraceObserver3.isEntered());
+		sm.removeIfaceTraceObserver(ifaceTraceObserver2);
 		sm.runCycle();
-		assertTrue(ifaceTraceObserver.isExited());
+		assertTrue(ifaceTraceObserver1.isExited());
+		assertTrue(ifaceTraceObserver3.isExited());
 		assertTrue(sm.isFinal());
 	}
 }
