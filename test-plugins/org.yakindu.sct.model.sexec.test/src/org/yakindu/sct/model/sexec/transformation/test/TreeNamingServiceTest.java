c5c74aa7dde5d8363087301e3155286d7ce58a01
Robin Herrmann <Robin.Herrmann@itemis.com>
2019-01-07 11:00:23 +0100

    changed names for testing

--- test-plugins/org.yakindu.sct.model.sexec.test/src/org/yakindu/sct/model/sexec/transformation/test/TreeNamingServiceTest.java
+++ test-plugins/org.yakindu.sct.model.sexec.test/src/org/yakindu/sct/model/sexec/transformation/test/TreeNamingServiceTest.java
@@ -194,8 +194,7 @@ public class TreeNamingServiceTest extends ModelSequencerTest {
 //		List<String> expectedNames = new ArrayList<>(Arrays.asList("mrgn_StA", "mrgn_StteB", "s_SA", "t_SA", "t_SA_AR_SA",
 //				"t_SA_AR_StB", "s_SA_AR_SA", "s_SA_AR_StB"));
 		
-		List<String> expectedNames = new ArrayList<>(Arrays.asList("mnrgon_SttA", "mnrgon_SttB", "sn_SA, sn_SA_ARgn_SA",
-				"sn_SA_ARgn_SB", "tn_SA, tn_SA_ARgn_SA", "tn_SA_ARgn_SB"));
+		List<String> expectedNames = new ArrayList<>(Arrays.asList("mnrgon_SttA", "mnrgon_SttB", "sn_SA", "sn_SA_ARgn_SA", "sn_SA_ARgn_SB", "tn_SA", "tn_SA_ARgn_SA", "tn_SA_ARgn_SB"));
 
 		ExecutionFlow flow = optimizer.transform(sequencer.transform(toTest));
 
