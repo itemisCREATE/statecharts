312eab954b00198f88644f27bf4cc4896c986408
Tobias Angele <tangele@itemis.de>
2019-10-17 15:50:59 +0200

    copyright

--- plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editor/DisableButtonsByPerspectiveListener.java
+++ plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editor/DisableButtonsByPerspectiveListener.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2010 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
 package org.yakindu.sct.ui.editor.editor;
 
 import static org.yakindu.sct.ui.perspectives.IYakinduSctPerspectives.ID_PERSPECTIVE_SCT_SIMULATION;
@@ -9,6 +19,9 @@ import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IWorkbenchPage;
 
+/**
+ * @author tobias angele - Initial contribution and API
+ */
 public class DisableButtonsByPerspectiveListener implements IPerspectiveListener {
 
 	public static final String TESTING_PERSPECTIVE = "com.yakindu.sct.ui.TestingPerspective";
