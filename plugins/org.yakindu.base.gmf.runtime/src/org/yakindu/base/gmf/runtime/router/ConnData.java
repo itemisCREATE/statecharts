72d4d6d074a905fa5b1d96fa92cf41d43e26fb90
mwienand <matthias.wienand@itemis.de>
2019-10-21 18:29:31 +0200

    Adopt latest rubber_band changes.

--- plugins/org.yakindu.base.gmf.runtime/src/org/yakindu/base/gmf/runtime/router/ConnData.java
+++ plugins/org.yakindu.base.gmf.runtime/src/org/yakindu/base/gmf/runtime/router/ConnData.java
@@ -8,20 +8,62 @@ import org.eclipse.draw2d.RelativeBendpoint;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.PrecisionPoint;
+import org.eclipse.draw2d.geometry.Rectangle;
 
 public class ConnData {
 	public Connection conn;
 	public List<PrecisionPoint> initialVisualPoints = new ArrayList<>();
 	public List<PrecisionPoint> initialBendpointLocations = new ArrayList<>();
 	public List<PrecisionPoint> finalVisualPoints = new ArrayList<>();
+	public boolean isSource;
+	public boolean isTarget;
+	public boolean isReflexive;
+	public int sourceAnchorIndex = -1;
+	public int sourceNeighborIndex = -1;
+	public int targetAnchorIndex = -1;
+	public int targetNeighborIndex = -1;
+	public boolean isSourceVertical = false;
+	public boolean isTargetVertical = false;
+	public int sourceSideIndex = -1;
+	public int targetSideIndex = -1;
 
-	public ConnData(Connection conn) {
+	public double SideDistance = 10d;
+
+	public ConnData() {
+		// FIXME: remove, refactor printPoints() etc.
+	}
+
+	public ConnData(Connection conn, boolean isSource, boolean isTarget) {
 		this.conn = conn;
+		this.isSource = isSource;
+		this.isTarget = isTarget;
+		this.isReflexive = isSource && isTarget;
+
+		// initial points
 		PointList pl = conn.getPoints();
 		for (int i = 0; i < pl.size(); i++) {
 			Point p = pl.getPoint(i);
 			initialVisualPoints.add(new PrecisionPoint(p));
 		}
+
+		// anchored segment
+		int initialPointsSize = initialVisualPoints.size();
+		if (initialPointsSize > 1) {
+			if (isSource) {
+				sourceAnchorIndex = 0;
+				sourceNeighborIndex = 1;
+				isSourceVertical = initialVisualPoints.get(sourceAnchorIndex).preciseX() == initialVisualPoints
+						.get(sourceNeighborIndex).preciseX();
+			}
+			if (isTarget) {
+				targetAnchorIndex = initialPointsSize - 1;
+				targetNeighborIndex = initialPointsSize - 2;
+				isTargetVertical = initialVisualPoints.get(targetAnchorIndex).preciseX() == initialVisualPoints
+						.get(targetNeighborIndex).preciseX();
+			}
+		}
+
+		// initial bendpoints
 		Object routingConstraint = conn.getRoutingConstraint();
 		if (routingConstraint instanceof List) {
 			List<?> bendpointList = (List<?>) routingConstraint;
@@ -38,40 +80,90 @@ public class ConnData {
 			System.out.println("[ERR] unknown routing constraint " + routingConstraint);
 		}
 	}
-	
-	public ConnData() {
-		// TODO Auto-generated constructor stub
+
+	public List<PrecisionPoint> getInitialVisualPointsCopy() {
+		List<PrecisionPoint> copy = new ArrayList<>();
+		for (PrecisionPoint p : initialVisualPoints) {
+			copy.add(new PrecisionPoint(p));
+		}
+		return copy;
 	}
 
-	public void printInitialLocations() {
-		System.out.println("Initial bendpoint locations for " + conn);
-		for (Point point : initialBendpointLocations) {
-			System.out.println("- " + point);
+	private MaxMoveDelta getMaxMoveDelta(Rectangle relBounds, boolean isVertical, PrecisionPoint anchorPoint) {
+		if (isVertical) {
+			// move in X
+			double x = anchorPoint.preciseX();
+			double bx = relBounds.preciseX() + SideDistance;
+			double bx2 = (bx + relBounds.preciseWidth()) - SideDistance - SideDistance;
+			if (x < bx) {
+				x = bx;
+			}
+			if (x > bx2) {
+				x = bx2;
+			}
+			return new MaxMoveDelta(true, x - bx2, x - bx);
+		} else {
+			// move in Y
+			double y = anchorPoint.preciseY();
+			double by = relBounds.preciseY() + SideDistance;
+			double by2 = (by + relBounds.preciseHeight()) - SideDistance - SideDistance;
+			if (y < by) {
+				y = by;
+			}
+			if (y > by2) {
+				y = by2;
+			}
+			return new MaxMoveDelta(false, y - by2, y - by);
 		}
 	}
-	
-	public void printPoints(List<? extends Point> points) {
-		for (Point point : points) {
-			System.out.println("- " + point);
+
+	public MaxMoveDelta[] getMaxMoveDeltas(Rectangle relBounds) {
+		MaxMoveDelta[] mmds = new MaxMoveDelta[] { new MaxMoveDelta(true), new MaxMoveDelta(), new MaxMoveDelta(true),
+				new MaxMoveDelta() };
+		if (initialVisualPoints.size() < 2) {
+			// need two points for max-move-deltas
+			return mmds;
 		}
-	}
-	
-	public void printFinalLocations() {
-		System.out.println("Final bendpoint locations for " + conn);
-		for (Point point : finalVisualPoints) {
-			System.out.println("- " + point);
+		// merge in deltas for source and/or target
+		if (isSource) {
+			boolean isVertical = isSourceVertical;
+			PrecisionPoint anchorPoint = initialVisualPoints.get(sourceAnchorIndex);
+			MaxMoveDelta mmd = getMaxMoveDelta(relBounds, isVertical, anchorPoint);
+			sourceSideIndex = getSideIndex(relBounds, isVertical, anchorPoint);
+			mmds[sourceSideIndex].merge(mmd);
 		}
-	}
-	
-	public void printVisualPoints() {
-		System.out.println("Visual Points for " + conn);
-		PointList pointList = conn.getPoints();
-		for (int i = 0; i < pointList.size(); i++) {
-			Point p = pointList.getPoint(i);
-			System.out.println("- " + p);
+		if (isTarget) {
+			boolean isVertical = isTargetVertical;
+			PrecisionPoint anchorPoint = initialVisualPoints.get(targetAnchorIndex);
+			MaxMoveDelta mmd = getMaxMoveDelta(relBounds, isVertical, anchorPoint);
+			targetSideIndex = getSideIndex(relBounds, isVertical, anchorPoint);
+			mmds[targetSideIndex].merge(mmd);
 		}
+		return mmds;
 	}
-	
+
+	private int getSideIndex(Rectangle relBounds, boolean isVertical, PrecisionPoint anchorPoint) {
+		int index;
+		if (isVertical) {
+			if (relBounds.getTop().getDistance(anchorPoint) < relBounds.getBottom().getDistance(anchorPoint)) {
+				// top
+				index = 0;
+			} else {
+				// bottom
+				index = 2;
+			}
+		} else {
+			if (relBounds.getLeft().getDistance(anchorPoint) < relBounds.getRight().getDistance(anchorPoint)) {
+				// left
+				index = 1;
+			} else {
+				// right
+				index = 3;
+			}
+		}
+		return index;
+	}
+
 	public List<PrecisionPoint> getVisualPoints() {
 		List<PrecisionPoint> list = new ArrayList<>();
 		PointList pointList = conn.getPoints();
@@ -80,7 +172,7 @@ public class ConnData {
 		}
 		return list;
 	}
-	
+
 	public void printBendpointLocations() {
 		System.out.println("Bendpoints for " + conn);
 		Object routingConstraint = conn.getRoutingConstraint();
@@ -100,11 +192,32 @@ public class ConnData {
 		}
 	}
 
-	public List<PrecisionPoint> getInitialVisualPointsCopy() {
-		List<PrecisionPoint> copy = new ArrayList<>();
-		for (PrecisionPoint p : initialVisualPoints) { 
-			copy.add(new PrecisionPoint(p));
+	public void printFinalLocations() {
+		System.out.println("Final bendpoint locations for " + conn);
+		for (Point point : finalVisualPoints) {
+			System.out.println("- " + point);
 		}
-		return copy;
+	}
+
+	public void printInitialLocations() {
+		System.out.println("Initial bendpoint locations for " + conn);
+		for (Point point : initialBendpointLocations) {
+			System.out.println("- " + point);
+		}
+	}
+
+	public void printPoints(List<? extends Point> points) {
+		for (Point point : points) {
+			System.out.println("- " + point);
+		}
+	}
+
+	public void printVisualPoints() {
+		System.out.println("Visual Points for " + conn);
+		PointList pointList = conn.getPoints();
+		for (int i = 0; i < pointList.size(); i++) {
+			Point p = pointList.getPoint(i);
+			System.out.println("- " + p);
+		}
 	}
 }
