7fcf225dfe9de6eb1f54cad24516b9fd86e656c0
mwienand <matthias.wienand@itemis.de>
2019-10-22 20:15:42 +0200

    Prevent label center fallback to anchor point in special case.

--- plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/commands/SetConnectionBendpointsAndLabelCommmand.java
+++ plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/commands/SetConnectionBendpointsAndLabelCommmand.java
@@ -32,70 +32,66 @@ import org.yakindu.base.xtext.utils.gmf.commands.SetLabelsOffsetOperation;
 @SuppressWarnings("restriction")
 public class SetConnectionBendpointsAndLabelCommmand extends SetConnectionBendpointsCommand {
 
-    /** Operation to delegate the job concerning the labels. */
-    private SetLabelsOffsetOperation setLabelsOperation;
+	/** Operation to delegate the job concerning the labels. */
+	private SetLabelsOffsetOperation setLabelsOperation;
 
-    /**
-     * Default constructor.
-     *
-     * @param editingDomain
-     *            the editing domain through which model changes are made
-     */
-    public SetConnectionBendpointsAndLabelCommmand(TransactionalEditingDomain editingDomain) {
-        super(editingDomain);
-        setLabelsOperation = new SetLabelsOffsetOperation();
-    }
+	/**
+	 * Default constructor.
+	 *
+	 * @param editingDomain the editing domain through which model changes are made
+	 */
+	public SetConnectionBendpointsAndLabelCommmand(TransactionalEditingDomain editingDomain) {
+		super(editingDomain);
+		setLabelsOperation = new SetLabelsOffsetOperation();
+	}
 
-    @Override
-    protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
-        setLabelsOperation.updateGMFLabelsOffset();
-        return super.doExecute(monitor, info);
-    }
+	@Override
+	protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		setLabelsOperation.updateGMFLabelsOffset();
+		return super.doExecute(monitor, info);
+	}
 
-    /**
-     * Set labels to update according to a connectionEditPart (all labels of
-     * this connection will be update). <BR>
-     * This method must be called after having called the
-     * {@link #setNewPointList(PointList, org.eclipse.draw2d.ConnectionAnchor, org.eclipse.draw2d.ConnectionAnchor)}
-     * of {@link #setNewPointList(PointList, Point, Point)} method.
-     *
-     * @param connectionEditPart
-     *            The connection from which to get the potential three labels to
-     *            update
-     */
-    public void setLabelsToUpdate(ConnectionEditPart connectionEditPart) {
-        setLabelsOperation.setLabelsToUpdate(connectionEditPart);
+	/**
+	 * Set labels to update according to a connectionEditPart (all labels of this
+	 * connection will be update). <BR>
+	 * This method must be called after having called the
+	 * {@link #setNewPointList(PointList, org.eclipse.draw2d.ConnectionAnchor, org.eclipse.draw2d.ConnectionAnchor)}
+	 * of {@link #setNewPointList(PointList, Point, Point)} method.
+	 *
+	 * @param connectionEditPart The connection from which to get the potential
+	 *                           three labels to update
+	 */
+	public void setLabelsToUpdate(ConnectionEditPart connectionEditPart) {
+		setLabelsOperation.setLabelsToUpdate(connectionEditPart);
+		setLabelsOperation.setNewPointList(connectionEditPart.getConnectionFigure().getPoints());
+	}
 
-    }
+	/**
+	 * Set labels to update according to a connectionEditPart (all labels of this
+	 * connection will be update). This method must be used if the edge figure is
+	 * updated (through feedback) during the move. Indeed, in this case, we can not
+	 * use the figure to retrieve the old points.<BR>
+	 * This method must be called after having called the
+	 * {@link #setNewPointList(PointList, org.eclipse.draw2d.ConnectionAnchor, org.eclipse.draw2d.ConnectionAnchor)}
+	 * of {@link #setNewPointList(PointList, Point, Point)} method.
+	 *
+	 * @param connectionEditPart The connection from which to get the potential
+	 *                           three labels to update
+	 * @param originalPoints     The points of the edge before the move.
+	 */
+	public void setLabelsToUpdate(ConnectionEditPart connectionEditPart, PointList originalPoints) {
+		setLabelsOperation.setLabelsToUpdate(connectionEditPart, originalPoints);
+	}
 
-    /**
-     * Set labels to update according to a connectionEditPart (all labels of
-     * this connection will be update). This method must be used if the edge
-     * figure is updated (through feedback) during the move. Indeed, in this
-     * case, we can not use the figure to retrieve the old points.<BR>
-     * This method must be called after having called the
-     * {@link #setNewPointList(PointList, org.eclipse.draw2d.ConnectionAnchor, org.eclipse.draw2d.ConnectionAnchor)}
-     * of {@link #setNewPointList(PointList, Point, Point)} method.
-     *
-     * @param connectionEditPart
-     *            The connection from which to get the potential three labels to
-     *            update
-     * @param originalPoints
-     *            The points of the edge before the move.
-     */
-    public void setLabelsToUpdate(ConnectionEditPart connectionEditPart, PointList originalPoints) {
-        setLabelsOperation.setLabelsToUpdate(connectionEditPart, originalPoints);
-    }
+	@Override
+	public void setNewPointList(PointList newPointList, ConnectionAnchor sourceAnchor, ConnectionAnchor targetAnchor) {
+		super.setNewPointList(newPointList, sourceAnchor, targetAnchor);
+		setLabelsOperation.setNewPointList(newPointList);
+	}
 
-    @Override
-    public void setNewPointList(PointList newPointList, Point sourceRefPoint, Point targetRefPoint) {
-        super.setNewPointList(newPointList, sourceRefPoint, targetRefPoint);
-        setLabelsOperation.setNewPointList(newPointList);
-    }
-
-    @Override
-    public void setNewPointList(PointList newPointList, ConnectionAnchor sourceAnchor, ConnectionAnchor targetAnchor) {
-        super.setNewPointList(newPointList, sourceAnchor, targetAnchor);
-        setLabelsOperation.setNewPointList(newPointList);
-    }
+	@Override
+	public void setNewPointList(PointList newPointList, Point sourceRefPoint, Point targetRefPoint) {
+		super.setNewPointList(newPointList, sourceRefPoint, targetRefPoint);
+		setLabelsOperation.setNewPointList(newPointList);
+	}
 }
