e52f26a69e95bd45ff6545e39f60de7cb6cc6b4d
Thomas Kutz <thomas.kutz@itemis.de>
2020-07-10 09:30:44 +0200

    Add hint to change domain when imports cannot be resolved

--- plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/validation/STextValidationMessages.java
+++ plugins/org.yakindu.sct.model.stext/src/org/yakindu/sct/model/stext/validation/STextValidationMessages.java
@@ -53,7 +53,7 @@ public interface STextValidationMessages {
 	public static final String REFERENCE_CONSTANT_BEFORE_DEFINED = "Cannot reference a constant from different scope or before it is defined.";
 	public static final String INTERNAL_DECLARATION_UNUSED = "Internal declaration is not used in statechart.";
 	public static final String VALUE_OF_REQUIRES_EVENT = "valueof() expression requires event as argument.";
-	public static final String IMPORT_NOT_RESOLVED_MSG = "Import '%s' cannot be resolved.";
+	public static final String IMPORT_NOT_RESOLVED_MSG = "Import '%s' cannot be resolved. Please check the statechart domain in the statechart properties, e.g. set the C/C++ domain to enable header file imports.";
 	public static final String IMPORT_NOT_RESOLVED_CODE = "ImportNotResolved";
 	public static final String DUPLICATE_IMPORT = "Duplicate import '%s'.";
 	public static final String CONTRADICTORY_ANNOTATIONS = "Some annotations (%s) have contradictory effects.";
